9d4e5d282a7f5056bb2020bdb99e4d6279480f4d
nick-wilson95 <nick.xue15@gmail.com>
2019-10-11 09:09:55 +0100

    Added effect class flesh

--- Danki2/Assets/Scripts/Effects/Effect.cs
+++ Danki2/Assets/Scripts/Effects/Effect.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Effects
 {
-    class Effect
+    public abstract class Effect
     {
+        private float remainingDuration;
+        public Effect(float duration)
+        {
+            remainingDuration = duration;
+        }
+
+        protected abstract void UpdateAction(Actor actor);
+
+        protected abstract void FinishAction(Actor actor);
+
+        public bool Update(Actor actor)
+        {
+            remainingDuration -= Time.deltaTime;
+            this.UpdateAction(actor);
+            if (remainingDuration < 0f)
+            {
+                this.FinishAction(actor);
+                return true;
+            }
+            return false;
+        }
     }
 }
