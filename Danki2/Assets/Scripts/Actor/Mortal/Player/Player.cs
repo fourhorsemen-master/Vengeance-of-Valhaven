cd0e29505eb6ece9e5cf78abef6b066b04e41182
George <g.w1.armstrong@googlemail.com>
2019-10-16 19:40:25 +0100

    Adds WASD movement to player.

--- Danki2/Assets/Scripts/Actor/Mortal/Player/Player.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Player/Player.cs
@@ -3,10 +3,34 @@ using UnityEngine;
 
 public class Player : Mortal
 {
+
+    [SerializeField, Range(1f, 100f), Tooltip("Recommended value: 16.")]
+    private float moveSpeed = 1f; //This will be inherited in the future. Keeping here for now as to avoid changing other files.
+
+    private float moveSpeedMultiplier = 100f; //Allows for nice moveSpeed values in the inspector.
+    private Vector3 vecToMove = new Vector3();
+    private KeyBindings keyBinds;
+
     public override void Act()
     {
-        var bindings = KeyMapper.Mapper.Bindings;
+        Move();
+    }
 
-        // Move etc. according to bindings
+    private void Move()
+    {
+        if (keyBinds == null) keyBinds = KeyMapper.Mapper.Bindings;
+
+        vecToMove = Vector3.zero;
+
+        if (Input.GetKey(keyBinds.Right)) vecToMove.x += 1f;
+        if (Input.GetKey(keyBinds.Left)) vecToMove.x -= 1f;
+        if (Input.GetKey(keyBinds.Up)) vecToMove.y += 1f;
+        if (Input.GetKey(keyBinds.Down)) vecToMove.y -= 1f;
+
+        //Handles diagonal speed reduction.
+        vecToMove.Normalize();
+        vecToMove *= moveSpeed/moveSpeedMultiplier;
+
+        transform.Translate(vecToMove);
     }
 }
