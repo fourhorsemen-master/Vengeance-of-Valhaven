cbba8af1e6194734f943ac456b2ce805ddcf88d1
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-10-23 21:45:40 +0100

    Adds some of the required editor scripts, having trouble getting to work with prefab

---
+++ Danki2/Assets/Scripts/Actor/Editor/ActorEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class ActorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Actor actor = (Actor)target;
+
+        EditorGUILayout.LabelField("Stats", EditorStyles.boldLabel);
+        EditorGUI.indentLevel++;
+        foreach (Stat stat in Enum.GetValues(typeof(Stat)))
+        {
+            if (!actor.stats.ContainsKey(stat))
+            {
+                actor.stats.Add(stat, 0);
+            }
+            actor.stats[stat] = EditorGUILayout.IntSlider(stat.ToString(), actor.stats[stat], 0, 100);
+        }
+        EditorGUI.indentLevel--;
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
---
+++ Danki2/Assets/Scripts/Actor/Mortal/Player/Editor/PlayerEditor.cs
@@ -0,0 +1,4 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(Player))]
+public class PlayerEditor : MortalEditor {}
