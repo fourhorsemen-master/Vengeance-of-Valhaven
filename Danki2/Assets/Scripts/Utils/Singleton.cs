9349a962ee4363ce1b08734b4239f365525a9d45
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-11-30 17:02:23 +0000

    Adds abstract singleton class

---
+++ Danki2/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+        }
+        else
+        {
+            Debug.LogError($"Tried to instantiate more than one instance of the singleton: {typeof(T).Name}");
+            Destroy(gameObject);
+        }
+    }
+}
