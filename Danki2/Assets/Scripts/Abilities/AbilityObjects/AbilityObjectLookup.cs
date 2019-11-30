4f54efa77fab9baa8e86b4df2ca7633298e30b2d
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-11-30 17:25:27 +0000

    Adds fireball to lookup

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/AbilityObjectLookup.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/AbilityObjectLookup.cs
@@ -1,3 +1,9 @@
-﻿public class AbilityObjectLookup : Singleton<AbilityObjectLookup>
+﻿using UnityEngine;
+
+public class AbilityObjectLookup : Singleton<AbilityObjectLookup>
 {
+    [SerializeField]
+    private FireballObject _fireballPrefab;
+
+    public FireballObject FireballPrefab { get => _fireballPrefab; }
 }
