a5458b5d651bc24e96116b0f6c207f8df7749149
nick-wilson95 <nick.xue15@gmail.com>
2019-12-15 15:02:51 +0000

    Added rigidbody to fireballObject and turned on the sphere collider, added OnCollisionEnter method that doestroys it except for on collisions with caster

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/FireballObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/FireballObject.cs
@@ -3,16 +3,23 @@
 public class FireballObject : MonoBehaviour
 {
     private float _speed;
+    private Actor _caster;
 
-    public static void Fire(Vector3 position, Quaternion rotation, float speed)
+    public static void Fire(Actor caster, Vector3 position, Quaternion rotation, float speed)
     {
         FireballObject prefab = AbilityObjectPrefabLookup.Instance.FireballObjectPrefab;
         FireballObject fireballObject = Instantiate(prefab, position, rotation);
         fireballObject._speed = speed;
+        fireballObject._caster = caster;
     }
 
     private void Update()
     {
         transform.position += transform.forward * _speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject != _caster.gameObject) Destroy(gameObject);
     }
 }
--- Danki2/Assets/Scripts/Abilities/InstantCast/Fireball.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Fireball.cs
@@ -3,9 +3,11 @@
 public class Fireball : InstantCast
 {
     private static readonly float FIREBALL_SPEED = 5;
+    private readonly AbilityContext context;
 
     public Fireball(AbilityContext context) : base(context)
     {
+        this.context = context;
     }
 
     public override void Cast()
@@ -13,6 +15,6 @@ public class Fireball : InstantCast
         Vector3 position = Context.Owner.transform.position;
         Vector3 target = Context.TargetPosition;
         Quaternion rotation = Quaternion.LookRotation(target - position);
-        FireballObject.Fire(position, rotation, FIREBALL_SPEED);
+        FireballObject.Fire(context.Owner, position, rotation, FIREBALL_SPEED);
     }
 }
