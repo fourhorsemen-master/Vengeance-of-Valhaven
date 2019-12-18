79575ce40b60edaa99dbad058a25373f12275d82
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-12-18 17:41:08 +0000

    Adds FollowPlayerAtDistance behaviour

--- Danki2/Assets/Scripts/Actor/Mortal/Enemy/TargetDummy.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Enemy/TargetDummy.cs
@@ -12,7 +12,7 @@ public class TargetDummy : Enemy
         IPlanner<TargetDummy> planner = new AlwaysAdvance<TargetDummy>();
         Personality<TargetDummy> personality = new Personality<TargetDummy>
         {
-            { AIAction.Advance, new FollowPlayer<TargetDummy>() }
+            { AIAction.Advance, new FollowPlayerAtDistance<TargetDummy>() }
         };
 
         _ai = new AI<TargetDummy>(
