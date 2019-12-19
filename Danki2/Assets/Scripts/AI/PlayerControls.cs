22908f08c9f9b53fd8b9d78ddc2dea5a0a4e3e99
pipkin94 <andrewgp94@hotmail.co.uk>
2019-12-19 20:34:25 +0000

    WIP handleabilities moved over but is being called on playercontrols update

--- Danki2/Assets/Scripts/AI/PlayerControls.cs
+++ Danki2/Assets/Scripts/AI/PlayerControls.cs
@@ -7,64 +7,40 @@ public class PlayerControls : MonoBehaviour
     [SerializeField]
     private Player _player;
 
+    private ActionControlState _previousActionControlState = ActionControlState.None;
 
     private void Update()
     {
         Move();
-
-       /* HandleAbilities();*/
+        HandleAbilities();
     }
 
     private void Move()
     {
-        var _moveDirection = Vector3.zero;
+        var moveDirection = Vector3.zero;
 
-        if (Input.GetAxis("Horizontal") > 0) _moveDirection.x += 1f;
-        if (Input.GetAxis("Horizontal") < 0) _moveDirection.x -= 1f;
-        if (Input.GetAxis("Vertical") > 0) _moveDirection.z += 1f;
-        if (Input.GetAxis("Vertical") < 0) _moveDirection.z -= 1f;
+        if (Input.GetAxis("Horizontal") > 0) moveDirection.x += 1f;
+        if (Input.GetAxis("Horizontal") < 0) moveDirection.x -= 1f;
+        if (Input.GetAxis("Vertical") > 0) moveDirection.z += 1f;
+        if (Input.GetAxis("Vertical") < 0) moveDirection.z -= 1f;
 
         if (Input.GetAxis("Dash") > 0)
         {
-            _player.Dash(_moveDirection);
+            _player.Dash(moveDirection);
         }
         else
         {
-            _player.MoveAlong(_moveDirection);
+            _player.MoveAlong(moveDirection);
         }
     }
 
-   /* private void HandleAbilities()
+    private void HandleAbilities()
     {
-        var currentControlState = this.GetCurrentActionControlState();
+        var currentControlState = GetCurrentActionControlState();
 
-        var castingCommand = ControlMatrix.GetCastingCommand(_castingStatus, _previousActionControlState, currentControlState);
+        _player.HandleAbilities(currentControlState, _previousActionControlState);
 
-        switch (castingCommand)
-        {
-            case CastingCommand.ContinueChannel:
-                // Handle case where channel has ended naturally.
-                if (!_channelService.Active)
-                {
-                    _castingStatus = _remainingCooldown <= 0f ? CastingStatus.Ready : CastingStatus.Cooldown;
-                }
-                break;
-            case CastingCommand.CancelChannel:
-                _channelService.Cancel();
-                _castingStatus = _remainingCooldown <= 0f ? CastingStatus.Ready : CastingStatus.Cooldown;
-
-                // Ability whiffed, reset tree. TODO: Make a method out of this including feedback for player. 
-                _abilityTree.Reset();
-                break;
-            case CastingCommand.CastLeft:
-                this.BranchAndCast(Direction.Left);
-                break;
-            case CastingCommand.CastRight:
-                this.BranchAndCast(Direction.Right);
-                break;
-        }
-
-        this._previousActionControlState = currentControlState;
+        _previousActionControlState = currentControlState;
     }
 
     private ActionControlState GetCurrentActionControlState()
@@ -76,36 +52,4 @@ public class PlayerControls : MonoBehaviour
             ? (right ? ActionControlState.Both : ActionControlState.Left)
             : (right ? ActionControlState.Right : ActionControlState.None);
     }
-
-    private void BranchAndCast(Direction direction)
-    {
-        if (!_abilityTree.CanWalk(direction))
-        {
-            _abilityTree.Reset();
-        }
-
-        var abilityReference = _abilityTree.Walk(direction);
-
-        var abilityContext = new AbilityContext(this, MouseGamePositionFinder.Instance.GetMouseGamePosition());
-
-        if (Ability.TryGetAsInstantCastBuilder(abilityReference, out var instantCastbuilder))
-        {
-            var instantCast = instantCastbuilder.Invoke(abilityContext);
-            instantCast.Cast();
-
-            _castingStatus = CastingStatus.Cooldown;
-        }
-
-        if (Ability.TryGetAsChannelBuilder(abilityReference, out var channelBuilder))
-        {
-            var channel = channelBuilder.Invoke(abilityContext);
-            _channelService.Start(channel);
-
-            _castingStatus = direction == Direction.Left
-                ? CastingStatus.ChannelingLeft
-                : CastingStatus.ChannelingRight;
-        }
-
-        _remainingCooldown = abilityCooldown;
-    }*/
 }
--- Danki2/Assets/Scripts/Actor/Mortal/Player/Player.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Player/Player.cs
@@ -38,7 +38,6 @@ public class Player : Mortal
 
     private float _remainingDashCooldown = 0f;
     private CastingStatus _castingStatus = CastingStatus.Ready;
-    private ActionControlState _previousActionControlState = ActionControlState.None;
     private AbilityTree _abilityTree;
     private ChannelService _channelService;
     private float _remainingCooldown = 0f;
@@ -94,6 +93,67 @@ public class Player : Mortal
             LockMovement(dashDuration, GetStat(Stat.Speed) * dashSpeedMultiplier, direction);
             _remainingDashCooldown = totalDashCooldown;
         }
+    }
+
+    public void HandleAbilities(ActionControlState current, ActionControlState previous)
+    {
+        var castingCommand = ControlMatrix.GetCastingCommand(_castingStatus, previous, current);
+
+        switch (castingCommand)
+        {
+            case CastingCommand.ContinueChannel:
+                // Handle case where channel has ended naturally.
+                if (!_channelService.Active)
+                {
+                    _castingStatus = _remainingCooldown <= 0f ? CastingStatus.Ready : CastingStatus.Cooldown;
+                }
+                break;
+            case CastingCommand.CancelChannel:
+                _channelService.Cancel();
+                _castingStatus = _remainingCooldown <= 0f ? CastingStatus.Ready : CastingStatus.Cooldown;
+
+                // Ability whiffed, reset tree. TODO: Make a method out of this including feedback for player. 
+                _abilityTree.Reset();
+                break;
+            case CastingCommand.CastLeft:
+                this.BranchAndCast(Direction.Left);
+                break;
+            case CastingCommand.CastRight:
+                this.BranchAndCast(Direction.Right);
+                break;
+        }
+    }
+
+    private void BranchAndCast(Direction direction)
+    {
+        if (!_abilityTree.CanWalk(direction))
+        {
+            _abilityTree.Reset();
+        }
+
+        var abilityReference = _abilityTree.Walk(direction);
+
+        var abilityContext = new AbilityContext(this, MouseGamePositionFinder.Instance.GetMouseGamePosition());
+
+        if (Ability.TryGetAsInstantCastBuilder(abilityReference, out var instantCastbuilder))
+        {
+            var instantCast = instantCastbuilder.Invoke(abilityContext);
+            instantCast.Cast();
+
+            _castingStatus = CastingStatus.Cooldown;
+        }
+
+        if (Ability.TryGetAsChannelBuilder(abilityReference, out var channelBuilder))
+        {
+            var channel = channelBuilder.Invoke(abilityContext);
+            _channelService.Start(channel);
+
+            _castingStatus = direction == Direction.Left
+                ? CastingStatus.ChannelingLeft
+                : CastingStatus.ChannelingRight;
+        }
+
+        _remainingCooldown = abilityCooldown;
     }
 
     protected override void OnDeath()
