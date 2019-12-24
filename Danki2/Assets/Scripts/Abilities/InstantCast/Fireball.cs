3b2acff25ca1e92590782f489654ba3af7731eb5
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-12-24 12:40:57 +0000

    Adds player mesh and fixes fireball spawning position

--- Danki2/Assets/Scripts/Abilities/InstantCast/Fireball.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Fireball.cs
@@ -2,27 +2,27 @@
 
 public class Fireball : InstantCast
 {
-    private static readonly float FIREBALL_SPEED = 5;
-    private readonly AbilityContext _context;
+    private static readonly float SPEED = 5;
+    private static readonly Vector3 POSITION_TRANSFORM = new Vector3(0, 1.25f, 0);
 
     public Fireball(AbilityContext context) : base(context)
     {
-        _context = context;
+
     }
 
     public override void Cast()
     {
-        Vector3 position = Context.Owner.transform.position;
+        Vector3 position = Context.Owner.transform.position + POSITION_TRANSFORM;
         Vector3 target = Context.TargetPosition;
         Quaternion rotation = Quaternion.LookRotation(target - position);
-        FireballObject.Fire(_context.Owner, OnCollision, FIREBALL_SPEED, position, rotation);
+        FireballObject.Fire(Context.Owner, OnCollision, SPEED, position, rotation);
     }
 
     public void OnCollision(GameObject gameObject)
     {
         if (gameObject.TryGetComponent<Mortal>(out var mortal))
         {
-            var strength = _context.Owner.GetStat(Stat.Strength);
+            var strength = Context.Owner.GetStat(Stat.Strength);
             mortal.ModifyHealth(-strength);
         }
     }
