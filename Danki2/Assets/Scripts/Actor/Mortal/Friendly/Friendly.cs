d39c4da18dc36a3663ce960d1e6da7b2d92cf49a
Anthony Murphy <am13571@my.bristol.ac.uk>
2019-12-27 17:51:31 +0000

    Removes unused AI classes

--- Danki2/Assets/Scripts/Actor/Mortal/Friendly/Friendly.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Friendly/Friendly.cs
@@ -1,11 +1,3 @@
 ﻿public abstract class Friendly : Mortal
 {
-    public abstract AI AI { get; }
-
-    protected override void Update()
-    {
-        base.Update();
-
-        AI.Act();
-    }
 }
