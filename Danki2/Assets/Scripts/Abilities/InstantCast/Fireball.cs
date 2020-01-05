5fb9680af7abec0b5f63205a010b74de1de5f1ba
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-01-05 16:58:55 +0000

    Changes format of static fields

--- Danki2/Assets/Scripts/Abilities/InstantCast/Fireball.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Fireball.cs
@@ -2,8 +2,8 @@
 
 public class Fireball : InstantCast
 {
-    private static readonly float SPEED = 5;
-    private static readonly Vector3 POSITION_TRANSFORM = new Vector3(0, 1.25f, 0);
+    private static readonly float _fireballSpeed = 5;
+    private static readonly Vector3 _positionTransform = new Vector3(0, 1.25f, 0);
 
     public Fireball(AbilityContext context) : base(context)
     {
@@ -12,10 +12,10 @@ public class Fireball : InstantCast
 
     public override void Cast()
     {
-        Vector3 position = Context.Owner.transform.position + POSITION_TRANSFORM;
+        Vector3 position = Context.Owner.transform.position + _positionTransform;
         Vector3 target = Context.TargetPosition;
         Quaternion rotation = Quaternion.LookRotation(target - position);
-        FireballObject.Fire(Context.Owner, OnCollision, SPEED, position, rotation);
+        FireballObject.Fire(Context.Owner, OnCollision, _fireballSpeed, position, rotation);
     }
 
     public void OnCollision(GameObject gameObject)
