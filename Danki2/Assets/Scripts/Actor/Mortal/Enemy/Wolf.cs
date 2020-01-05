f669cbfbbaab70604bfda696bf2f9c928f352067
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-01-05 22:01:41 +0000

    Removes redundant AI from Wolf

--- Danki2/Assets/Scripts/Actor/Mortal/Enemy/Wolf.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Enemy/Wolf.cs
@@ -2,21 +2,6 @@
 
 public class Wolf : Enemy
 {
-    private AI<Wolf> _ai;
-
-    protected override void Awake()
-    {
-        base.Awake();
-
-        _ai = new AI<Wolf>(
-            this,
-            (a, b) => new Agenda(),
-            new Personality<Wolf>()
-        );
-    }
-
-    public override AI AI => _ai;
-
     protected override void OnDeath()
     {
         Debug.Log("Wolf died");
