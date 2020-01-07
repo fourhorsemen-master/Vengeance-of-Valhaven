d526cb0128f4a4b3ae6aa7b654ee7980d7dad31c
pipkin94 <andrewgp94@hotmail.co.uk>
2020-01-07 21:21:42 +0000

    most the way

--- Danki2/Assets/Scripts/Actor/Mortal/Mortal.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Mortal.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Mortal : Actor
 {
     public int Health { get; private set; }
+
+    public Image HealthBar;
 
     public bool Dead { get; private set; }
 
@@ -25,11 +28,33 @@ public abstract class Mortal : Actor
     protected override void Update()
     {
         base.Update();
+        UpdateHealthBar();
 
         if (Health <= 0 && !Dead)
         {
             OnDeath();
             Dead = true;
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (HealthBar == null || HealthBar.canvas == null)
+        {
+            return;
+        }
+
+        if (Health > 0)
+        {
+            var lookAtPosition = 2 * HealthBar.canvas.transform.position - Camera.main.transform.position;
+            lookAtPosition.x = HealthBar.canvas.transform.position.x;
+            HealthBar.canvas.transform.LookAt(lookAtPosition);
+            float maxHealth = GetStat(Stat.MaxHealth);
+            HealthBar.transform.localScale = new Vector3(Health / maxHealth, 1f, 1f);
+        }
+        else
+        {
+            HealthBar.canvas.enabled = false;
         }
     }
 
