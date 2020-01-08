fd7802bc87b30cab191e70acd39dc9c100bd1663
nick-wilson95 <nick.xue15@gmail.com>
2020-01-08 21:19:19 +0000

    Ant's comments

---
+++ Danki2/Assets/Scripts/Actor/Mortal/Components/HealthBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image _healthBar;
+    public Mortal _mortal;
+
+    void Update()
+    {
+        if (_healthBar == null || _healthBar.canvas == null)
+        {
+            return;
+        }
+
+        if (_mortal.Health <= 0)
+        {
+            _healthBar.canvas.enabled = false;
+            return;
+        }
+
+        FaceCamera();
+
+        // Update the remaining health display
+        float maxHealth = _mortal.GetStat(Stat.MaxHealth);
+        _healthBar.transform.localScale = new Vector3(_mortal.Health / maxHealth, 1f, 1f);
+    }
+
+    /// <summary>
+    /// Rotate the health bar to face the camera, but lock the y-rotation
+    /// </summary>
+    private void FaceCamera()
+    {
+        var healthBarPosition = _healthBar.canvas.transform.position;
+        var cameraPosition = Camera.main.transform.position;
+        var lookAtPosition = 2 * healthBarPosition - cameraPosition;
+        lookAtPosition.x = healthBarPosition.x;
+        _healthBar.canvas.transform.LookAt(lookAtPosition);
+    }
+}
--- Danki2/Assets/Scripts/Actor/Mortal/Mortal.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Mortal.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public abstract class Mortal : Actor
 {
     public int Health { get; private set; }
-
-    public Image HealthBar;
 
     public bool Dead { get; private set; }
 
@@ -28,38 +25,12 @@ public abstract class Mortal : Actor
     protected override void Update()
     {
         base.Update();
-        UpdateHealthBar();
 
         if (Health <= 0 && !Dead)
         {
             OnDeath();
             Dead = true;
         }
-    }
-
-    private void UpdateHealthBar()
-    {
-        if (HealthBar == null || HealthBar.canvas == null)
-        {
-            return;
-        }
-
-        if (Health <= 0)
-        {
-            HealthBar.canvas.enabled = false;
-            return;
-        }
-
-        // Rotate the health bar to face the camera, but lock the y-rotation
-        var healthBarPosition = HealthBar.canvas.transform.position;
-        var cameraPosition = Camera.main.transform.position;
-        var lookAtPosition = 2 * healthBarPosition - cameraPosition;
-        lookAtPosition.x = healthBarPosition.x;
-        HealthBar.canvas.transform.LookAt(lookAtPosition);
-
-        // Update the remaining health display
-        float maxHealth = GetStat(Stat.MaxHealth);
-        HealthBar.transform.localScale = new Vector3(Health / maxHealth, 1f, 1f);
     }
 
     protected abstract void OnDeath();
