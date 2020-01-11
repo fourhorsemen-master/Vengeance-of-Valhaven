b7c300866c33aeb9a9fad7d8e457c76aebf6cb66
pipkin94 <andrewgp94@hotmail.co.uk>
2020-01-11 11:09:49 +0000

    address Georges comments

--- Danki2/Assets/Scripts/Actor/Mortal/Components/HealthBar.cs
+++ Danki2/Assets/Scripts/Actor/Mortal/Components/HealthBar.cs
@@ -9,7 +9,13 @@ public class HealthBar : MonoBehaviour
 
     void Update()
     {
-        if (_healthBar == null || _healthBar.canvas == null)
+        if (_healthBar == null)
+        {
+            Debug.LogError("No image found for health bar");
+            return;
+        }
+
+        if (_healthBar.canvas == null)
         {
             return;
         }
@@ -20,22 +26,8 @@ public class HealthBar : MonoBehaviour
             return;
         }
 
-        FaceCamera();
-
         // Update the remaining health display
         float maxHealth = mortal.GetStat(Stat.MaxHealth);
         _healthBar.transform.localScale = new Vector3(mortal.Health / maxHealth, 1f, 1f);
-    }
-
-    /// <summary>
-    /// Rotate the health bar to face the camera, but lock the y-rotation
-    /// </summary>
-    private void FaceCamera()
-    {
-        var healthBarPosition = _healthBar.canvas.transform.position;
-        var cameraPosition = Camera.main.transform.position;
-        var lookAtPosition = 2 * healthBarPosition - cameraPosition;
-        lookAtPosition.x = healthBarPosition.x;
-        _healthBar.canvas.transform.LookAt(lookAtPosition);
     }
 }
---
+++ Danki2/Assets/Scripts/UI/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 lookAtPosition = 2 * transform.position - Camera.main.transform.position;
+        lookAtPosition.x = transform.position.x;
+        transform.LookAt(lookAtPosition);
+    }
+}
