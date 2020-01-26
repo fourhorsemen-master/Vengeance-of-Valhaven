7453e137e6088987957147e5cece4b152e169b1f
pipkin94 <andrewgp94@hotmail.co.uk>
2020-01-26 16:09:39 +0000

    added howl audio to wolf prefab and given it WolfFindPlayer Find Target behaviour with a howl range

---
+++ Danki2/Assets/Scripts/AI/Behaviour/Behaviours/WolfFindPlayer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Behaviour("Wolf Find Player", new string[] { "Aggro Range", "Howl Range" }, new AIAction[] { AIAction.FindTarget })]
+public class WolfFindPlayer : Behaviour
+{
+    private float _aggroRange;
+    private float _howlRange;
+    private Player _target = null;
+
+    public override void Initialize()
+    {
+        _aggroRange = Args[0];
+        _howlRange = Args[1];
+    }
+
+    public override void Behave(AI ai, Actor actor)
+    {
+        if (!_target)
+        {
+            _target = GameObject.FindObjectOfType<Player>();
+        }
+
+        float distanceToTarget = Vector3.Distance(
+            _target.transform.position,
+            actor.transform.position
+        );
+
+        if (distanceToTarget < _aggroRange)
+        {
+            ai.Target = _target;
+            CallFriends(_target, actor);
+        }
+    }
+
+    private void CallFriends(Player target, Actor actor)
+    {
+        actor.GetComponent<AudioSource>().Play();
+
+        IEnumerable<Wolf> friends = RoomManager.Instance.ActorCache
+            .Select(item =>
+            {
+                bool isWolf = item.Actor.TryGetComponent(out Wolf wolf);
+                return isWolf ? wolf : null;
+            })
+            .Where(wolf => wolf != null);
+
+        foreach (Wolf wolf in friends)
+        {
+            float distance = Vector3.Distance(
+                target.transform.position,
+                wolf.transform.position
+            );
+
+            if (distance < _howlRange)
+            {
+                if (wolf.TryGetComponent(out AI ai))
+                {
+                    ai.Target = target;
+                }
+            }
+        }
+    }
+}
