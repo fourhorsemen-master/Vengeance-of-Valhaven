624a114a0051e3a3320d69bd2b72a8e5da24ec88
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-01-28 21:12:20 +0000

    Implements Slash ability

--- Danki2/Assets/Scripts/Abilities/InstantCast/Slash.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Slash.cs
@@ -2,12 +2,31 @@
 
 public class Slash : InstantCast
 {
+    private static readonly float range = 3;
+    private static readonly float damageMultiplyer = 1.5f;
+
     public Slash(AbilityContext context) : base(context)
     {
     }
 
     public override void Cast()
     {
-        Debug.Log("Instant casting: Slash");
+        Actor owner = Context.Owner;
+        Vector3 position = owner.transform.position;
+        Vector3 target = Context.TargetPosition;
+
+        CollisionTemplateManager.Instance.GetCollidingActors(
+            CollisionTemplate.Wedge90,
+            range,
+            position,
+            Quaternion.FromToRotation(position, target)
+        ).ForEach(actor =>
+        {
+            if (true) // TODO: Should check Context.Owner.opposes(actor)...
+            {
+                float damage = owner.GetStat(Stat.Strength) * damageMultiplyer;
+                actor.ModifyHealth(-damage);
+            }
+        });
     }
 }
