0495f8327aeb486db02bdabf6d80b4d274a5f259
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-01-28 21:44:11 +0000

    Tweaks to slash

--- Danki2/Assets/Scripts/Abilities/InstantCast/Slash.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Slash.cs
@@ -2,7 +2,7 @@
 
 public class Slash : InstantCast
 {
-    private static readonly float range = 3;
+    private static readonly float range = 4;
     private static readonly float damageMultiplyer = 1.5f;
 
     public Slash(AbilityContext context) : base(context)
@@ -12,19 +12,22 @@ public class Slash : InstantCast
     public override void Cast()
     {
         Actor owner = Context.Owner;
+
         Vector3 position = owner.transform.position;
         Vector3 target = Context.TargetPosition;
+        target.y = 0;
+
+        float damage = owner.GetStat(Stat.Strength) * damageMultiplyer;
 
         CollisionTemplateManager.Instance.GetCollidingActors(
             CollisionTemplate.Wedge90,
             range,
             position,
-            Quaternion.FromToRotation(position, target)
+            Quaternion.LookRotation(target - position)
         ).ForEach(actor =>
         {
             if (true) // TODO: Should check Context.Owner.opposes(actor)...
             {
-                float damage = owner.GetStat(Stat.Strength) * damageMultiplyer;
                 actor.ModifyHealth(-damage);
             }
         });
