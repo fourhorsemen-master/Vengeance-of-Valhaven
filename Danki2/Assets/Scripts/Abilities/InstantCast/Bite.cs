3375d6c1a69fa35b8584a176a9069fe011501e26
pipkin94 <andrewgp94@hotmail.co.uk>
2020-02-01 16:17:16 +0000

    bite ability does damage, adjusted player health and wolf strength

--- Danki2/Assets/Scripts/Abilities/InstantCast/Bite.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Bite.cs
@@ -2,15 +2,35 @@
 
 public class Bite : InstantCast
 {
+    public static readonly float Range = 2f;
+
     public Bite(AbilityContext context) : base(context)
     {
     }
 
-    public static readonly float Range = 2f;
-
     public override void Cast()
     {
-        Debug.Log("Instant casting: Bite");
-        Context.Owner.Root(0.5f, Context.TargetPosition - Context.Owner.transform.position);
+        Actor owner = Context.Owner;
+
+        Vector3 position = owner.transform.position;
+        Vector3 target = Context.TargetPosition;
+        target.y = 0;
+
+        float damage = owner.GetStat(Stat.Strength);
+
+        CollisionTemplateManager.Instance.GetCollidingActors(
+            CollisionTemplate.Wedge90,
+            Range,
+            position,
+            Quaternion.LookRotation(target - position)
+        ).ForEach(actor =>
+        {
+            if (owner.Opposes(actor))
+            {
+                actor.ModifyHealth(-damage);
+            }
+        });
+
+        owner.Root(0.5f, Context.TargetPosition - Context.Owner.transform.position);
     }
 }
