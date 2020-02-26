033e27a07161f057ae741baf081777f14d006c70
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-02-26 09:09:36 +0000

    Implements smash ability

--- Danki2/Assets/Scripts/Abilities/InstantCast/Smash.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Smash.cs
@@ -2,12 +2,36 @@
 
 public class Smash : InstantCast
 {
+    private static readonly float _distanceFromCaster = 1f;
+    private static readonly float _radius = 1f;
+    private static readonly float _damageMultiplier = 2f;
+
     public Smash(AbilityContext context) : base(context)
     {
     }
 
     public override void Cast()
     {
-        Debug.Log("casting smash");
+        Actor owner = Context.Owner;
+
+        Vector3 position = owner.transform.position;
+        Vector3 target = Context.TargetPosition;
+        target.y = 0;
+        Vector3 direction = (target - position).normalized;
+        Vector3 center = position + (direction * _distanceFromCaster);
+
+        float damage = owner.GetStat(Stat.Strength) * _damageMultiplier;
+
+        CollisionTemplateManager.Instance.GetCollidingActors(
+            CollisionTemplate.Cylinder,
+            _radius,
+            center
+        ).ForEach(actor =>
+        {
+            if (owner.Opposes(actor))
+            {
+                actor.ModifyHealth(-damage);
+            }
+        });
     }
 }
