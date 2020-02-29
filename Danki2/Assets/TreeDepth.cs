12d65f984f0966c1c19b522a34aee46358febbc8
nick-wilson95 <nick.xue15@gmail.com>
2020-02-29 22:23:40 +0000

    Add tree-depth UI

---
+++ Danki2/Assets/TreeDepth.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TreeDepth : MonoBehaviour
+{
+    [SerializeField]
+    private Player _player = null;
+
+    [SerializeField]
+    private Image _repeatingImage = null;
+
+    private float _spriteWidth;
+    private float _spriteHeight;
+
+    private void Awake()
+    {
+        _spriteWidth = _repeatingImage.rectTransform.sizeDelta.x;
+        _spriteHeight = _repeatingImage.rectTransform.sizeDelta.y;
+    }
+
+    private void Start()
+    {
+        _player.AbilityTree.CurrentDepthSubject.Subscribe(TreeDepthChangeCallback);
+    }
+
+    private void TreeDepthChangeCallback(int newDepth)
+    {
+        _repeatingImage.rectTransform.sizeDelta = new Vector2(newDepth * _spriteWidth, _spriteHeight);
+    }
+}
