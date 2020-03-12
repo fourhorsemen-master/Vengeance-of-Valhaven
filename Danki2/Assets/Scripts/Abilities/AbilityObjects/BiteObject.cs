bd91ed4f1472516a1ab025357cdf79b0d5b97d01
George Armstrong <george.armstrong@mail.bcu.ac.uk>
2020-03-12 20:22:20 +0000

    Removed some old code, animator cleanup

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/BiteObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/BiteObject.cs
@@ -6,9 +6,6 @@ public class BiteObject : StaticAbilityObject
     public AudioSource biteSound = null;
     public override float StickTime { get; set; }
 
-    [SerializeField]
-    private Collider collider = null;
-
     public void Awake()
     {
         StickTime = biteSound.clip.length;
@@ -19,17 +16,5 @@ public class BiteObject : StaticAbilityObject
         base.Start();
 
         biteSound.Play();
-
-        DelayCollider(0.5f);
-    }
-
-    private IEnumerator DelayCollider( float waitTime )
-    {
-        yield return new WaitForSeconds(waitTime);
-
-        if(collider)
-        {
-            collider.enabled = true;
-        }
     }
 }
