2e6ff361344d729159390c9c8db925ff19480a54
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-03-13 20:21:15 +0000

    Adds getter for root node and makes MaxDepth a lambda

--- Danki2/Assets/Scripts/Abilities/Ability Tree/AbilityTree.cs
+++ Danki2/Assets/Scripts/Abilities/Ability Tree/AbilityTree.cs
@@ -1,12 +1,14 @@
 ﻿public class AbilityTree
 {
-    private readonly Node _rootNode;
+    private Node _rootNode;
 
     private Node _currentNode;
 
     private int _currentDepth;
 
-    public int MaxDepth { get; }
+    // The root node counts itself when calculating depth, as it is just the same as any
+    // other node. But in terms of the ability tree, we want to discount it.
+    public int MaxDepth => _rootNode.MaxDepth() - 1;
 
     public BehaviourSubject<Node> TreeWalkSubject { get; }
 
@@ -18,12 +20,13 @@
         _currentNode = _rootNode;
         _currentDepth = 0;
 
-        // The root node counts itself when calculating depth, as it is just the same as any
-        // other node. But in terms of the ability tree, we want to discount it.
-        MaxDepth = _rootNode.MaxDepth() - 1;
-
         TreeWalkSubject = new BehaviourSubject<Node>(_currentNode);
         CurrentDepthSubject = new BehaviourSubject<int>(_currentDepth);
+    }
+
+    public Node GetRootNode()
+    {
+        return _rootNode;
     }
 
     public bool CanWalkDirection(Direction direction)
