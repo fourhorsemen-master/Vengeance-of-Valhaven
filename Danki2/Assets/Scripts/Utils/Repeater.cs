069a9cacda4c3a22b3df5224ed7e432f189e36b1
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-03-24 20:47:00 +0000

    Adds docs to the Repeater class

--- Danki2/Assets/Scripts/Utils/Repeater.cs
+++ Danki2/Assets/Scripts/Utils/Repeater.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using UnityEngine;
 
+/// <summary>
+/// Helper class for running repeated code when outside of a Unity context and coroutines
+/// aren't an option. The Update() method should be called every frame once the Repeater
+/// has been created.
+/// </summary>
 public class Repeater
 {
     private readonly float interval;
@@ -8,6 +13,12 @@ public class Repeater
 
     private float currentTime = 0;
 
+    /// <param name="interval">How often to run the action, given in seconds</param>
+    /// <param name="action">The action to run</param>
+    /// <param name="runOnStart">
+    /// Whether to run the action when the repeater is created. If false then the action
+    /// won't run until the fist time the interval passes.
+    /// </param>
     public Repeater(float interval, Action action, bool runOnStart = true)
     {
         this.interval = interval;
@@ -20,6 +31,8 @@ public class Repeater
     {
         currentTime += Time.deltaTime;
 
+        // While loop so that if the deltaTime is greater than the interval, the action
+        // still gets called the right number of times.
         while (currentTime >= interval)
         {
             action.Invoke();
