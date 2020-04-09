f48d459dd8f8e096f4e1f6628817913b6cb2ffb0
unknown <nick.xue15@gmail.com>
2020-04-09 12:22:37 +0100

    stop movement on death

--- Danki2/Assets/Scripts/Actor/Actor.cs
+++ Danki2/Assets/Scripts/Actor/Actor.cs
@@ -39,6 +39,7 @@ public abstract class Actor : MonoBehaviour
     {
         if (health <= 0f && !Dead)
         {
+            MovementManager.ClearDestination();
             OnDeath();
             Dead = true;
         }
