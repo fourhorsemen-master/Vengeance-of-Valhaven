07836dd68108d540bcad6b8fce38d96c0dc55524
pipkin94 <andrewgp94@hotmail.co.uk>
2020-04-13 10:01:17 +0100

    setup dictionary of abilities on player

--- Danki2/Assets/Scripts/Actor/Player/Player.cs
+++ Danki2/Assets/Scripts/Actor/Player/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : Actor
 {
@@ -32,11 +33,26 @@ public class Player : Actor
 
     public AbilityTree AbilityTree { get; private set; }
 
+    public Dictionary<AbilityReference, int> AbilityInventory { get; private set; }
+
     public override ActorType Type => ActorType.Player;
 
     protected override void Awake()
     {
         base.Awake();
+
+        AbilityInventory = new Dictionary<AbilityReference, int>
+        {
+            { AbilityReference.Bite, 1 },
+            { AbilityReference.Pounce, 2 },
+            { AbilityReference.Slash, 3 },
+            { AbilityReference.Roll, 4 },
+            { AbilityReference.DaggerThrow, 5 },
+            { AbilityReference.Lunge, 6 },
+            { AbilityReference.Smash, 7 },
+            { AbilityReference.Fireball, 8 },
+            { AbilityReference.Whirlwind, 9 },
+        };
 
         AbilityTree = AbilityTreeFactory.CreateTree(
             AbilityTreeFactory.CreateNode(
