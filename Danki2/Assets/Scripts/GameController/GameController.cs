430498330b28d15fc00a06344848474dccd0c9e1
pipkin94 <andrewgp94@hotmail.co.uk>
2020-04-13 09:41:49 +0100

    toggle UI

---
+++ Danki2/Assets/Scripts/GameController/GameController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController>
+{
+    public GameState GameState { get; set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        GameState = GameState.Playing;
+    }
+}
