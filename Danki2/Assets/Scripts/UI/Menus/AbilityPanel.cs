774c6e3fdadd056853e51518f801930d29737ccb
pipkin94 <andrewgp94@hotmail.co.uk>
2020-04-13 12:07:00 +0100

    spawns ability panels based on players ability inventory

---
+++ Danki2/Assets/Scripts/UI/Menus/AbilityPanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Image iconPanelImage;
+
+    [SerializeField]
+    private Text namePanelText;
+
+    [SerializeField]
+    private Text quantityPanelText;
+
+    public void InitialisePanel(AbilityReference ability, int quantity)
+    {
+        iconPanelImage.sprite = AbilityIconManager.Instance.GetIcon(ability);
+
+        namePanelText.text = System.Enum.GetName(typeof(AbilityReference), ability);
+
+        quantityPanelText.text = quantity.ToString();
+    }
+}
--- Danki2/Assets/Scripts/UI/Menus/AbilityTreeMenuController.cs
+++ Danki2/Assets/Scripts/UI/Menus/AbilityTreeMenuController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class AbilityTreeMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Player player;
+
+    [SerializeField]
+    private GameObject abilityTreeContent;
+
+    [SerializeField]
+    private GameObject abilityPanelPrefab;
+
     void Start()
     {
-        
+        PopulateAbilityList();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PopulateAbilityList()
     {
-        
+        //destroy all current panels
+        foreach (Transform child in this.abilityTreeContent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (KeyValuePair<AbilityReference, int> item in this.player.AbilityInventory)
+        {
+            if (item.Value > 0)
+            {
+                GameObject abilityPanel = Instantiate(this.abilityPanelPrefab, Vector3.zero, Quaternion.identity);
+                abilityPanel.transform.parent = this.abilityTreeContent.transform;
+
+                abilityPanel.GetComponent<AbilityPanel>().InitialisePanel(item.Key, item.Value);
+
+                // determine Vector3 location for abilityPanel
+
+                // link to icon, name and quantity - do on abilityPanel script where it can reference children
+            }
+        }
     }
 }
