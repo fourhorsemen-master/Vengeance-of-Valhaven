7274b0455d33be404074ca66654518ce28f10085
pipkin94 <andrewgp94@hotmail.co.uk>
2020-04-17 16:36:19 +0100

    setup InterruptionManager script

---
+++ Danki2/Assets/Scripts/Actor/Services/Interruption/InterruptionManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterruptionManager
+{
+    private Dictionary<InterruptionType, List<Action>> interruptionRegister = new EnumDictionary<InterruptionType, List<Action>>(
+        () => new List<Action>()
+    );
+
+    public void Register(InterruptionType interruptionType, Action action)
+    {
+        interruptionRegister[interruptionType].Add(action);
+    }
+
+    public void Interrupt(InterruptionType interruptionType)
+    {
+        switch (interruptionType)
+        {
+            case InterruptionType.Soft:
+                interruptionRegister[InterruptionType.Soft].ForEach(a => a());
+                break;
+            case InterruptionType.Hard:
+                interruptionRegister[InterruptionType.Soft].ForEach(a => a());
+                interruptionRegister[InterruptionType.Hard].ForEach(a => a());
+                break;
+        }
+    }
+}
