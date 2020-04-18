22ca5fd9fbef5cc829cd20877dd81632eab58afc
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-04-18 17:28:53 +0100

    Adds template code for the parser, next step is to fully implement this

---
+++ Danki2/Assets/Scripts/Abilities/Tooltips/Parsing/Parser.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class Parser
+{
+    public List<TemplatedTooltipSegment> Parse(List<Token> tokens)
+    {
+        List<TemplatedTooltipSegment> templatedTooltipSegments = new List<TemplatedTooltipSegment>();
+
+        return templatedTooltipSegments;
+    }
+}
---
+++ Danki2/Assets/Scripts/Abilities/Tooltips/Parsing/TemplatedTooltipSegment.cs
@@ -0,0 +1,11 @@
+﻿public class TemplatedTooltipSegment
+{
+    public TemplatedTooltipSegmentType Type { get; }
+    public string Value { get; }
+
+    public TemplatedTooltipSegment(TemplatedTooltipSegmentType type, string value)
+    {
+        Type = type;
+        Value = value;
+    }
+}
