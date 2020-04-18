53ed64cb8ae416fe5ddd995b8fdd0c91ccb67b4a
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-04-18 16:03:12 +0100

    Adds lexer for tooltip strings

---
+++ Danki2/Assets/Scripts/Abilities/Tooltips/Lexing/Lexer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+public class Lexer
+{
+    private const char OpenBrace = '{';
+    private const char CloseBrace = '}';
+
+    private readonly string test = "Deals {DAMAGE} damage to targets in a cone.";
+    
+    public List<Token> Lex(string tooltip)
+    {
+        List<Token> tokens = new List<Token>();
+
+        string currentString = "";
+        foreach (char c in tooltip)
+        {
+            if (IsString(c))
+            {
+                currentString += c;
+            } else if (IsBrace(c))
+            {
+                if (!currentString.Equals(""))
+                {
+                    tokens.Add(new Token(TokenType.String, currentString));
+                    currentString = "";
+                }
+
+                tokens.Add(new Token(TokenType.Brace, c.ToString()));
+            }
+        }
+
+        if (!currentString.Equals(""))
+        {
+            tokens.Add(new Token(TokenType.String, currentString));
+        }
+
+        return tokens;
+    }
+
+    private bool IsString(char c)
+    {
+        return !IsBrace(c);
+    }
+
+    private bool IsBrace(char c)
+    {
+        return c.Equals(OpenBrace) || c.Equals(CloseBrace);
+    }
+}
