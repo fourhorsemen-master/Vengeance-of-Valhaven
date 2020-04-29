e52604de79e9abf6a7b797a5599ef01bc476fe93
pipkin94 <andrewgp94@hotmail.co.uk>
2020-04-29 16:01:37 +0100

    removed the floating health bar editor script as this is no longer required. Updated the health bar script to now show a third form of health bar which is white and shows a damage taken bar which lags the health bar.

--- Danki2/Assets/Scripts/Actor/Components/HealthBar.cs
+++ Danki2/Assets/Scripts/Actor/Components/HealthBar.cs
@@ -4,29 +4,62 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField]
-    private Image _healthBar = null;
+    private Image healthBar = null;
+    [SerializeField]
+    private Image damageLagBar = null;
+
+    private const float damageLagBarOffset = 0.5f;
+    private const float recentDamageIncrement = 1f;
+    private const float recentDamageIncrementScalar = 5f;
+
     public Actor actor;
+    private float previousHealthProportion;
+    private float recentDamage = 0f;
+
+    private void Start()
+    {
+        damageLagBar.transform.localScale = new Vector3(0f, 1f, 1f);
+    }
 
     private void Update()
     {
-        if (_healthBar == null)
+        if (healthBar == null)
         {
             Debug.LogError("No image found for health bar");
             return;
         }
 
-        if (_healthBar.canvas == null)
+        if (healthBar.canvas == null)
         {
             return;
         }
 
         if (actor.HealthManager.Health <= 0)
         {
-            _healthBar.canvas.enabled = false;
+            healthBar.canvas.enabled = false;
             return;
         }
 
-        // Update the remaining health display
-        _healthBar.transform.localScale = new Vector3((float)actor.HealthManager.Health / actor.HealthManager.MaxHealth, 1f, 1f);
+        // Set the health bar to the correct width and position the damage lag bar at the end of the health bar.
+        float healthProportion = (float)actor.HealthManager.Health / actor.HealthManager.MaxHealth;        
+        healthBar.transform.localScale = new Vector3(healthProportion, 1f, 1f);
+        damageLagBar.rectTransform.localPosition = new Vector3(healthProportion - damageLagBarOffset, 0f, 0f);
+        
+        // Tick the recent damage down at the non-linear rate set by the damage increment constants (cannot become less than 0).
+        recentDamage = Mathf.Max(
+            0f, 
+            recentDamage - (Time.deltaTime * (recentDamage + recentDamageIncrement) / recentDamageIncrementScalar)
+        );
+        
+        // Adjust the recent damage by any damage or healing in the current frame (cannot become less than 0). Note: -ve means healing.
+        recentDamage = Mathf.Max(
+            0f, 
+            recentDamage + previousHealthProportion - healthProportion
+        );
+        
+        // Set the damage lag bar to the correct width.
+        damageLagBar.transform.localScale = new Vector3(recentDamage, 1f, 1f);
+
+        previousHealthProportion = healthProportion;
     }
 }
