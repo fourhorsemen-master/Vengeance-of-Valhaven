db37ff77e92ccf46b6ae9a99e0e8a26854bff1d5
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-05-03 00:47:41 +0100

    improve random patrol

--- Danki2/Assets/Scripts/AI/Behaviour/Behaviours/Patrol/RandomPatrol.cs
+++ Danki2/Assets/Scripts/AI/Behaviour/Behaviours/Patrol/RandomPatrol.cs
@@ -2,10 +2,12 @@
 using UnityEngine.AI;
 
 [Behaviour("Random patrol", new string[] { "Max destination distance", "Repath frequency" }, new AIAction[] { AIAction.Patrol })]
-public class LocalPatrol : Behaviour
+public class RandomPatrol : Behaviour
 {
     private float maxDestinationDistance;
     private float repathFrequency;
+
+    private bool repathedRecently = false;
 
     public override void Initialize()
     {
@@ -15,15 +17,20 @@ public class LocalPatrol : Behaviour
 
     public override void Behave(Actor actor)
     {
+        if (repathedRecently) return;
+
         // Set new destination randomly on avergae once per second (maths not guaranteed).
         if (Random.Range(0f, 1f) > Time.deltaTime * repathFrequency) return;
 
         Vector2 randomOffset = Random.insideUnitCircle * maxDestinationDistance;
-        Vector3 randomDestination = actor.transform.position + new Vector3(randomOffset.x, randomOffset.y);
+        if (randomOffset.magnitude < 1f) return;
 
-        if (NavMesh.SamplePosition(randomDestination, out NavMeshHit hit, 1f, NavMesh.AllAreas))
+        Vector3 randomDestination = actor.transform.position + new Vector3(randomOffset.x, 0f, randomOffset.y);
+        if (NavMesh.SamplePosition(randomDestination, out NavMeshHit hit, 0.1f, NavMesh.AllAreas))
         {
             actor.MovementManager.StartPathfinding(hit.position);
+            repathedRecently = true;
+            actor.WaitAndAct(repathFrequency, () => repathedRecently = false);
         }
     }
 }
