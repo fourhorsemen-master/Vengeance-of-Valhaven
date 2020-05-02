4fbe914d81ebf92bc793a27309d599193e92527d
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-05-02 17:10:09 +0100

    Adds docs to ListUtils

--- Danki2/Assets/Scripts/Utils/ListUtils.cs
+++ Danki2/Assets/Scripts/Utils/ListUtils.cs
@@ -2,6 +2,12 @@
 
 public static class ListUtils
 {
+    /// <summary>
+    /// Helper method that returns a list containing the one given item.
+    /// </summary>
+    /// <param name="item"> The item to be in the list </param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static List<T> Singleton<T>(T item)
     {
         return new List<T> { item };
