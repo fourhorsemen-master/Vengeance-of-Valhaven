57be6d53ad95ed720c3dd1abb0975bcc6333ab9c
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-05-02 15:01:44 +0100

    Adds a static class for getting the parsed tooltip segments

---
+++ Danki2/Assets/Scripts/Abilities/Tooltips/ParsedTooltipLookup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class ParsedTooltipLookup
+{
+    private static readonly Lexer lexer = new Lexer();
+    private static readonly TokenValidator validator = new TokenValidator();
+    private static readonly Parser parser = new Parser();
+
+    private static readonly Dictionary<AbilityReference, List<TemplatedTooltipSegment>> lookup =
+        new Dictionary<AbilityReference, List<TemplatedTooltipSegment>>();
+
+    /// <summary>
+    /// Gets the list of templated tooltip segments for the given ability reference. These values are cached, as once
+    /// a tooltip has been parsed, its templated tooltip segments won't change again.
+    /// </summary>
+    /// <param name="abilityReference"> The ability to get the segments for </param>
+    /// <returns> The list of templated tooltip segments </returns>
+    public static List<TemplatedTooltipSegment> GetTemplatedTooltipSegments(AbilityReference abilityReference)
+    {
+        if (lookup.TryGetValue(abilityReference, out List<TemplatedTooltipSegment> templatedTooltipSegments))
+        {
+            return templatedTooltipSegments;
+        }
+
+        templatedTooltipSegments = GenerateTemplatedTooltipSegments(abilityReference);
+        lookup[abilityReference] = templatedTooltipSegments;
+        return templatedTooltipSegments;
+    }
+
+    private static List<TemplatedTooltipSegment> GenerateTemplatedTooltipSegments(AbilityReference abilityReference)
+    {
+        string tooltip = AbilityLookup.GetAbilityTooltip(abilityReference);
+        List<Token> tokens = lexer.Lex(tooltip);
+
+        if (!validator.HasValidSyntax(tokens))
+        {
+            Debug.LogError($"Tooltip does not have valid syntax, value was: \"{tooltip}\"");
+            return new List<TemplatedTooltipSegment>();
+        }
+
+        return parser.Parse(tokens);
+    }
+}
