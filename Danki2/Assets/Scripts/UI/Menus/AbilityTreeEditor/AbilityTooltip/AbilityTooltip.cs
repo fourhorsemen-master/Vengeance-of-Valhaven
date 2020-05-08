ab14c3c9fc889ed1424fb1c2eb4a33406556ce01
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-05-08 22:46:28 +0100

    added tooltip UI placeholder and component

---
+++ Danki2/Assets/Scripts/UI/Menus/AbilityTreeEditor/AbilityTooltip/AbilityTooltip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AbilityTooltip : Singleton<AbilityTooltip>
+{
+    [SerializeField]
+    private Text title = null;
+
+    [SerializeField]
+    private Text description = null;
+
+    [SerializeField]
+    private RectTransform abilityOrbPanel = null;
+
+    [SerializeField]
+    private TooltipAbilityOrb tooltipAbilityOrb = null;
+
+    private PlayerTooltipBuilder tooltipBuilder;
+
+    private void Start()
+    {
+        Player player = RoomManager.Instance.Player;
+        tooltipBuilder = new PlayerTooltipBuilder(player);
+    }
+
+    public void UpdateTooltip(Node node)
+    {
+        title.text = node.Ability.ToString();
+
+        List<TooltipSegment> segments = tooltipBuilder.Build(node);
+
+    }
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
