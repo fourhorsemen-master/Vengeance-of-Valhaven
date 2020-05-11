6025d444b46241c7e25bd8dd0288f07cd73a527d
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-05-11 17:01:43 +0100

    Adds AbilityAttribute and builds ability builders using this attribute

---
+++ Danki2/Assets/Scripts/Abilities/AbilityAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class AbilityAttribute : Attribute
+{
+    public AbilityReference AbilityReference { get; }
+
+    public AbilityAttribute(AbilityReference abilityReference)
+    {
+        AbilityReference = abilityReference;
+    }
+}
--- Danki2/Assets/Scripts/Abilities/Metadata/AbilityMetadataLookup.cs
+++ Danki2/Assets/Scripts/Abilities/Metadata/AbilityMetadataLookup.cs
@@ -1,25 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 public class AbilityMetadataLookup : Singleton<AbilityMetadataLookup>
 {
     public SerializedMetadataLookup serializedMetadataLookup = new SerializedMetadataLookup();
 
-    private Dictionary<AbilityReference, string> displayNameLookup = new Dictionary<AbilityReference, string>();
-    private Dictionary<AbilityReference, string> tooltipLookup = new Dictionary<AbilityReference, string>();
-    private Dictionary<AbilityReference, AbilityData> baseAbilityDataLookup = new Dictionary<AbilityReference, AbilityData>();
-    private Dictionary<AbilityReference, OrbType?> abilityOrbTypeLookup = new Dictionary<AbilityReference, OrbType?>();
-    private Dictionary<AbilityReference, Dictionary<OrbType, int>> generatedOrbsLookup = new Dictionary<AbilityReference, Dictionary<OrbType, int>>();
+    private readonly Dictionary<AbilityReference, string> displayNameLookup =
+        new Dictionary<AbilityReference, string>();
+    private readonly Dictionary<AbilityReference, string> tooltipLookup =
+        new Dictionary<AbilityReference, string>();
+    private readonly Dictionary<AbilityReference, AbilityData> baseAbilityDataLookup =
+        new Dictionary<AbilityReference, AbilityData>();
+    private readonly Dictionary<AbilityReference, OrbType?> abilityOrbTypeLookup =
+        new Dictionary<AbilityReference, OrbType?>();
+    private readonly Dictionary<AbilityReference, Dictionary<OrbType, int>> generatedOrbsLookup =
+        new Dictionary<AbilityReference, Dictionary<OrbType, int>>();
+
+    private readonly Dictionary<AbilityReference, Func<Actor, AbilityData, InstantCast>> instantCastBuilderLookup =
+        new Dictionary<AbilityReference, Func<Actor, AbilityData, InstantCast>>();
+    private readonly Dictionary<AbilityReference, Func<Actor, AbilityData, Channel>> channelBuilderLookup =
+        new Dictionary<AbilityReference, Func<Actor, AbilityData, Channel>>();
+    private readonly Dictionary<AbilityReference, AbilityType> abilityTypeLookup =
+        new Dictionary<AbilityReference, AbilityType>();
 
     protected override void Awake()
     {
         base.Awake();
         
-        BuildLookups();
+        BuildMetadataLookups();
+        BuildAbilityBuilderLookups();
     }
 
-    private void BuildLookups()
+    private void BuildMetadataLookups()
     {
         foreach (AbilityReference abilityReference in Enum.GetValues(typeof(AbilityReference)))
         {
@@ -34,5 +49,66 @@ public class AbilityMetadataLookup : Singleton<AbilityMetadataLookup>
                 .GroupBy(o => o)
                 .ToDictionary(g => g.Key, g => g.Count());
         }
+    }
+
+    private void BuildAbilityBuilderLookups()
+    {
+        List<AttributeData<AbilityAttribute>> abilityAttributeData = ReflectionUtils
+            .GetAttributeData<AbilityAttribute>(Assembly.GetExecutingAssembly());
+
+        if (!IsValidAbilityAttributeData(abilityAttributeData)) return;
+
+        Dictionary<AbilityReference, Type> abilityReferenceToType = abilityAttributeData
+            .ToDictionary(d => d.Attribute.AbilityReference, d => d.Type);
+
+        foreach (AbilityReference abilityReference in Enum.GetValues(typeof(AbilityReference)))
+        {
+            Type type = abilityReferenceToType[abilityReference];
+            ConstructorInfo constructor = type.GetConstructor(new [] {typeof(Actor), typeof(AbilityData)});
+            if (constructor == null)
+            {
+                Debug.Log($"Could not find valid constructor for ability: {abilityReference}");
+                return;
+            }
+
+            if (type.IsSubclassOf(typeof(InstantCast)))
+            {
+                instantCastBuilderLookup[abilityReference] = (a, b) => (InstantCast)constructor.Invoke(new object[] {a, b});
+                abilityTypeLookup[abilityReference] = AbilityType.InstantCast;
+            }
+
+            if (type.IsSubclassOf(typeof(Channel)))
+            {
+                channelBuilderLookup[abilityReference] = (a, b) => (Channel)constructor.Invoke(new object[] {a, b});
+                abilityTypeLookup[abilityReference] = AbilityType.Channel;
+            }
+        }
+    }
+
+    private bool IsValidAbilityAttributeData(List<AttributeData<AbilityAttribute>> abilityAttributeData)
+    {
+        List<AbilityReference> abilityReferences = abilityAttributeData
+            .Select(d => d.Attribute.AbilityReference)
+            .ToList();
+
+        if (abilityReferences.Distinct().Count() != abilityReferences.Count)
+        {
+            Debug.LogError("Ability attributes are not distinct.");
+            return false;
+        }
+
+        if (abilityReferences.Count != Enum.GetValues(typeof(AbilityReference)).Length)
+        {
+            Debug.LogError("Ability attributes found do not match all AbilityReferences. There may be abilities missing the attribute.");
+            return false;
+        }
+
+        if (!abilityAttributeData.All(d => d.Type.IsSubclassOf(typeof(Ability))))
+        {
+            Debug.LogError("Found ability attribute on class that does not inherit from ability.");
+            return false;
+        }
+
+        return true;
     }
 }
