93296d0848d39d9e794cd3718a3104c71ad3e152
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-05-21 21:02:50 +0100

    Adds damage modification to ability methods for bonuses

--- Danki2/Assets/Scripts/Abilities/Ability.cs
+++ Danki2/Assets/Scripts/Abilities/Ability.cs
@@ -18,29 +18,29 @@ public abstract class Ability
         SuccessFeedbackSubject = new Subject<bool>();
     }
 
-    protected void DealPrimaryDamage(Actor target)
+    protected void DealPrimaryDamage(Actor target, int damageModifier = 0)
     {
-        Owner.DamageTarget(target, AbilityData.PrimaryDamage);
+        Owner.DamageTarget(target, AbilityData.PrimaryDamage + damageModifier);
     }
 
-    protected void ApplyPrimaryDamageAsDOT(Actor target, float duration, float tickRate = 1)
+    protected void ApplyPrimaryDamageAsDOT(Actor target, float duration, float tickRate = 1, int damageModifier = 0)
     {
-        target.EffectManager.AddActiveEffect(new DOT(AbilityData.PrimaryDamage, duration, tickRate), duration);
+        target.EffectManager.AddActiveEffect(new DOT(AbilityData.PrimaryDamage + damageModifier, duration, tickRate), duration);
     }
 
-    protected void DealSecondaryDamage(Actor target)
+    protected void DealSecondaryDamage(Actor target, int damageModifier = 0)
     {
-        Owner.DamageTarget(target, AbilityData.SecondaryDamage);
+        Owner.DamageTarget(target, AbilityData.SecondaryDamage + damageModifier);
     }
 
-    protected void ApplySecondaryDamageAsDOT(Actor target, float duration, float tickRate = 1)
+    protected void ApplySecondaryDamageAsDOT(Actor target, float duration, float tickRate = 1, int damageModifier = 0)
     {
-        target.EffectManager.AddActiveEffect(new DOT(AbilityData.SecondaryDamage, duration, tickRate), duration);
+        target.EffectManager.AddActiveEffect(new DOT(AbilityData.SecondaryDamage + damageModifier, duration, tickRate), duration);
     }
 
-    protected void Heal()
+    protected void Heal(int healModifier = 0)
     {
-        Owner.HealthManager.ReceiveHeal(AbilityData.Heal);
+        Owner.HealthManager.ReceiveHeal(AbilityData.Heal + healModifier);
     }
 
     protected bool HasBonus(string bonus)
