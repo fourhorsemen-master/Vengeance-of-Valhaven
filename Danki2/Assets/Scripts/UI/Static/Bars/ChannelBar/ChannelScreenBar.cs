799501bea1ec0d41513c4472257f734f8b1ea3f8
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-05-26 00:12:28 +0100

    Add editor for channel bar colour pickers

---
+++ Danki2/Assets/Scripts/UI/Static/Bars/ChannelBar/ChannelScreenBar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChannelScreenBar : MonoBehaviour
+{
+    public ChannelBarColourDictionary barColourLookup = new ChannelBarColourDictionary(Color.white);
+
+    public Image channelBar = null;
+
+    private Player player;
+    private ChannelService channelService;
+
+    private void Start()
+    {
+        player = RoomManager.Instance.Player;
+        channelService = player.ChannelService;
+        channelService.ChannelStartSubject.Subscribe(channelType =>
+            channelBar.color = barColourLookup[channelType]
+        );
+    }
+
+    private void Update()
+    {
+        float width = 0f;
+
+        if (
+            player.AbilityManager.CastingStatus == CastingStatus.ChannelingLeft
+            || player.AbilityManager.CastingStatus == CastingStatus.ChannelingRight
+        )
+        {
+            width = channelService.RemainingDuration / channelService.TotalDuration;
+        }
+
+        channelBar.transform.localScale = new Vector3(width, 1f, 1f);
+    }
+}
