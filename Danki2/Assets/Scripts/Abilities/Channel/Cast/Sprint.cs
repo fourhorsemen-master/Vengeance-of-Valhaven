cc4788fc4a1eeab795a1c639cae536da4397d9a0
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-05-28 11:48:48 +0100

    Uses const for speed modification

--- Danki2/Assets/Scripts/Abilities/Channel/Cast/Sprint.cs
+++ Danki2/Assets/Scripts/Abilities/Channel/Cast/Sprint.cs
@@ -5,7 +5,7 @@ public class Sprint : Cast
 {
     public override float Duration => 1;
 
-    private const float SpeedModification = 2;
+    private const int SpeedModification = 2;
     private const float SprintDuration = 6;
 
     public Sprint(Actor owner, AbilityData abilityData, string[] availableBonuses)
@@ -15,6 +15,6 @@ public class Sprint : Cast
 
     public override void End(Vector3 target)
     {
-        Owner.EffectManager.AddActiveEffect(new SpeedModifier(2), SprintDuration);
+        Owner.EffectManager.AddActiveEffect(new SpeedModifier(SpeedModification), SprintDuration);
     }
 }
