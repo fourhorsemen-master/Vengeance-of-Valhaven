ce5c1205f9c9ad4568bcc0e63e6ad16f9ed7c3ab
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-05-31 14:17:07 +0100

    Make the OrbType classes

---
+++ Danki2/Assets/Scripts/Abilities/Lookup/Editor/OrbLookupEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(OrbLookup))]
+public class OrbLookupEditor : Editor
+{
+    private readonly EnumDictionary<OrbType, bool> foldoutStatus = new EnumDictionary<OrbType, bool>(false);
+
+    public override void OnInspectorGUI()
+    {
+        OrbLookup orbLookup = (OrbLookup)target;
+
+        foreach (OrbType orbType in Enum.GetValues(typeof(OrbType)))
+        {
+            foldoutStatus[orbType] = EditorGUILayout.Foldout(
+                foldoutStatus[orbType],
+                orbType.ToString()
+            );
+
+            if (!foldoutStatus[orbType]) return;
+
+            EditorGUI.indentLevel++;
+
+            orbLookup.displayNameMap[orbType] = EditorGUILayout.TextField(orbLookup.displayNameMap[orbType]);
+
+            orbLookup.colourMap[orbType] = EditorGUILayout.ColorField(orbLookup.colourMap[orbType]);
+
+            orbLookup.spriteMap[orbType] = (Sprite)EditorGUILayout.ObjectField(
+                orbType.ToString(),
+                orbLookup.spriteMap[orbType],
+                typeof(Sprite),
+                false,
+                null
+            );
+
+            EditorGUI.indentLevel--;
+        }
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
