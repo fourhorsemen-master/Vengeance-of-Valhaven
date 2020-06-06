6c7326a06848686ede92d085a1482aecf3982bcc
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-06-06 12:38:40 +0100

    Adds empty Meditate ability

---
+++ Danki2/Assets/Scripts/Abilities/Channel/Charge/Meditate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Meditate)]
+public class Meditate : Charge
+{
+    protected override float ChargeTime => 5;
+    
+    public Meditate(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void End(Vector3 target)
+    {
+        Debug.Log("Ending meditate");
+    }
+
+    public override void Cancel(Vector3 target)
+    {
+        Debug.Log("Cancelling meditate");
+    }
+}
