03329766f212b795c7c4cba18f82225a89242360
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-06-06 23:11:37 +0100

    Passes layer mask through to ray cast to only include actors when targeting

--- Danki2/Assets/Scripts/UnityHelpers/Layers.cs
+++ Danki2/Assets/Scripts/UnityHelpers/Layers.cs
@@ -1,4 +1,7 @@
-﻿public static class Layers
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Layers
 {
     public static int Default => 0;
     public static int TransparentFX => 1;
@@ -8,4 +11,15 @@
     public static int Ethereal => 8;
     public static int Floor => 9;
     public static int Actors => 10;
+
+    /// <summary>
+    /// Returns a layer mask, which includes the given layers. This is an integer that, when in binary format,
+    /// represents the layer mask. Each bit corresponds to a layer and is 1 iff the layer is in the mask.
+    /// </summary>
+    /// <param name="layers"> They layers to include in the mask </param>
+    /// <returns></returns>
+    public static int GetLayerMask(List<int> layers)
+    {
+        return layers.Select(layer => 1 << layer).Sum();
+    }
 }
