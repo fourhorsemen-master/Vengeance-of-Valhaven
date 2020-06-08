1cb4a10da71f56dd2cc489feda3d0061ed746a01
pipkin94 <andrewgp94@hotmail.co.uk>
2020-06-08 17:59:08 +0100

    bash ability added with audio and icon

---
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/BashObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BashObject : StaticAbilityObject
+{
+    public AudioSource bashSound = null;
+
+    public override float StickTime => bashSound.clip.length;
+
+    public static void Create(Vector3 position, Quaternion rotation)
+    {
+        BashObject prefab = AbilityObjectPrefabLookup.Instance.BashObjectPrefab;
+        Instantiate(prefab, position, rotation);
+    }
+}
---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Bash.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Bash)]
+public class Bash : InstantCast
+{
+    private const float StunDuration = 1f;
+    private const float Range = 3f;
+    private const float PauseDuration = 0.3f;
+
+    public Bash(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 target)
+    {
+        Vector3 position = Owner.transform.position;
+        Vector3 castDirection = target - position;
+        castDirection.y = 0f;
+
+        bool hasDealtDamage = false;
+
+        CollisionTemplateManager.Instance.GetCollidingActors(
+            CollisionTemplate.Wedge90,
+            Range,
+            position,
+            Quaternion.LookRotation(castDirection)
+        ).ForEach(actor =>
+        {
+            if (Owner.Opposes(actor))
+            {
+                DealPrimaryDamage(actor);
+                actor.EffectManager.AddActiveEffect(new Stun(StunDuration), StunDuration);
+                hasDealtDamage = true;
+            }
+        });
+
+        SuccessFeedbackSubject.Next(hasDealtDamage);
+
+        BashObject.Create(position, Quaternion.LookRotation(castDirection));
+
+        Owner.MovementManager.LookAt(target);
+        Owner.MovementManager.Stun(PauseDuration);
+        
+        if (hasDealtDamage)
+        {
+            CustomCamera.Instance.AddShake(ShakeIntensity.Medium);
+        }
+    }
+}
