78586fa946d7563ed2d8eb82061cff1aa19d9c57
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-06-13 22:22:20 +0100

    progress

---
+++ Danki2/Assets/Scripts/UI/Menus/AbilityTreeEditor/AbilityTree/Insertion/AbilityInserter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class AbilityInserter : MonoBehaviour
+{
+    [SerializeField]
+    private AbilityInsertionArea topLeftArrow = null;
+
+    [SerializeField]
+    private AbilityInsertionArea topRightArrow = null;
+
+    [SerializeField]
+    private AbilityInsertionArea bottomRightArrow = null;
+
+    [SerializeField]
+    private AbilityInsertionArea bottomLeftArrow = null;
+
+    [SerializeField]
+    private AbilityInsertionArea centralArea = null;
+
+    private void Start()
+    {
+        topLeftArrow.MouseUpSubject.Subscribe();
+    }
+}
---
+++ Danki2/Assets/Scripts/UI/Menus/AbilityTreeEditor/AbilityTree/Insertion/AbilityInsertionArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityInsertionArea : MonoBehaviour
+{
+    public Subject MouseUpSubject = new Subject();
+
+    public void OnMouseUp()
+    {
+        MouseUpSubject.Next();
+    }
+}
