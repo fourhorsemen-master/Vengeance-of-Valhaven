ec5fbdb5fe30919a0d430e5048c1046b9c025bff
pipkin94 <andrewgp94@hotmail.co.uk>
2020-06-13 22:20:57 +0100

    renamed actor to target on cast methods for abilities, variable renamed and root changed to stun on Hook script

--- Danki2/Assets/Scripts/Abilities/InstantCast/Hook.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Hook.cs
@@ -4,7 +4,6 @@
 public class Hook : InstantCast
 {
     private const float range = 10f;
-    private const float pauseDuration = 0.3f;
     private const float pullSpeed = 8f;
     private const float pullOffset = 2f;
     private const float stunDuration = 2f;
@@ -18,34 +17,32 @@ public class Hook : InstantCast
         SuccessFeedbackSubject.Next(false);
     }
 
-    public override void Cast(Actor actor)
+    public override void Cast(Actor target)
     {
-        float distanceToCaster = Vector3.Distance(actor.transform.position, Owner.transform.position);
+        float distanceToTarget = Vector3.Distance(target.transform.position, Owner.transform.position);
 
-        if (distanceToCaster > range)
+        if (distanceToTarget > range)
         {
             SuccessFeedbackSubject.Next(false);
             return;
         }
 
-        float pullDuration = (distanceToCaster - pullOffset) / pullSpeed;
-        Vector3 pullDirection = Owner.transform.position - actor.transform.position;
-        Vector3 pullFaceDirection = actor.transform.forward;
+        float pullDuration = (distanceToTarget - pullOffset) / pullSpeed;
+        Vector3 pullDirection = Owner.transform.position - target.transform.position;
+        Vector3 pullFaceDirection = target.transform.forward;
 
-        Owner.MovementManager.Root(pullDuration);
-        actor.MovementManager.LockMovement(
+        Owner.MovementManager.LookAt(target.transform.position);
+        Owner.MovementManager.Stun(pullDuration);
+        target.MovementManager.LockMovement(
             pullDuration,
             pullSpeed,
             pullDirection,
             pullFaceDirection
-            );
+        );
 
-        actor.WaitAndAct(pullDuration, () => actor.MovementManager.Stun(stunDuration));
+        target.WaitAndAct(pullDuration, () => target.MovementManager.Stun(stunDuration));
 
         SuccessFeedbackSubject.Next(true);
-
-        Owner.MovementManager.LookAt(actor.transform.position);
-        Owner.WaitAndAct(pullDuration, () => Owner.MovementManager.Stun(pauseDuration));
 
         HookObject.Create(Owner.transform);
 
