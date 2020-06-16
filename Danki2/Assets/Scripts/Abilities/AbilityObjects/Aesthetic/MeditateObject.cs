a2522654b84845ad7baa5f48f720e2695e0e84be
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-06-16 19:03:49 +0100

    Adds camera shake to the meditate heartbeat

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/MeditateObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/MeditateObject.cs
@@ -5,6 +5,9 @@ public class MeditateObject : MonoBehaviour
     [SerializeField]
     private AudioSource meditateSound = null;
 
+    [SerializeField, Range(0, 2)]
+    private float heartbeatInterval = 1f;
+
     [SerializeField, Range(0, 1)]
     private float initialVolume = 0f;
 
@@ -13,16 +16,20 @@ public class MeditateObject : MonoBehaviour
 
     private float maxDuration;
 
+    private Repeater repeater;
+
     private void Start()
     {
         meditateSound.volume = initialVolume;
-        meditateSound.Play();
+        repeater = new Repeater(heartbeatInterval, PlayHeartbeat);
     }
 
     private void Update()
     {
         // Linear function from initialVolume to finalVolume.
         meditateSound.volume += (Time.deltaTime / maxDuration) * (finalVolume - initialVolume);
+
+        repeater.Update();
     }
 
     public static MeditateObject Create(Transform casterTransform, float maxDuration)
@@ -37,5 +44,11 @@ public class MeditateObject : MonoBehaviour
     public void Destroy()
     {
         Destroy(gameObject);
+    }
+
+    private void PlayHeartbeat()
+    {
+        meditateSound.Play();
+        CustomCamera.Instance.AddShake(ShakeIntensity.Medium);
     }
 }
