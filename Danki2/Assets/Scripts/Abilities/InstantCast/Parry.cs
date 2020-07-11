4e3df82a74e27fd5aac32a1cc9e9d1bebf775af7
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-07-11 18:07:30 +0100

    Adds empty parry instant cast

---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Parry.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Parry)]
+public class Parry : InstantCast
+{
+    public Parry(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 target)
+    {
+        Debug.Log("Casting Parry...");
+    }
+}
