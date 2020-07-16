616bf08e50d8774c12e130b318c17cec917a11bf
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-07-16 22:10:39 +0100

    do the refactor

--- Danki2/Assets/Scripts/Actor/Player/AbilityManager.cs
+++ Danki2/Assets/Scripts/Actor/Player/AbilityManager.cs
@@ -14,9 +14,6 @@ public class AbilityManager
     private Subscription<bool> abilityFeedbackSubscription;
     private ActionControlState previousActionControlState = ActionControlState.None;
     private ActionControlState currentActionControlState = ActionControlState.None;
-    private Vector3 targetPosition = Vector3.zero;
-    private Subscription targetDeathSubscription;
-    private Enemy target = null;
 
     public float RemainingCooldownProportion => remainingAbilityCooldown / abilityCooldown;
     public CastingStatus CastingStatus { get; private set; } = CastingStatus.Ready;
@@ -28,7 +25,6 @@ public class AbilityManager
         this.abilityTimeoutLimit = abilityTimeoutLimit;
         this.abilityCooldown = abilityCooldown;
 
-        updateSubject.Subscribe(UpdateTarget);
         updateSubject.Subscribe(TickAbilityCooldown);
         lateUpdateSubject.Subscribe(HandleAbilities);
         this.player.RollSubject.Subscribe(Whiff);
@@ -43,68 +39,6 @@ public class AbilityManager
     public void SetCurrentControlState(ActionControlState controlState)
     {
         currentActionControlState = controlState;
-    }
-
-    private void UpdateTarget()
-    {
-        // First, we raycast for an actor (ie. by ignoring other layers)
-        bool mouseHitCollider = MouseGamePositionFinder.Instance.TryGetMouseGamePosition(
-            out Vector3 mousePosition,
-            out Collider collider,
-            Layers.GetLayerMask(new[] { Layers.Actors })
-        );
-
-        // Then, if we don't hit any actors, we raycast for any collider
-        if (!mouseHitCollider)
-        {
-            mouseHitCollider = MouseGamePositionFinder.Instance.TryGetMouseGamePosition(out mousePosition, out collider);
-        }
-
-        // Then, if no colliders are hit, we use the mouse position on a horizontal plane at the players height.
-        if (!mouseHitCollider)
-        {
-            mousePosition = MouseGamePositionFinder.Instance.GetMousePlanePosition(player.transform.position.y, true);
-        }
-
-        SetTargetPosition(mousePosition);
-
-        if (collider != null && collider.gameObject.CompareTag(Tags.Enemy))
-        {
-            Enemy enemy = collider.gameObject.GetComponent<Enemy>();
-            if (!enemy.Dead) SetTarget(enemy);
-        }
-        else
-        {
-            RemoveTarget();
-        }
-    }
-
-    private void SetTargetPosition(Vector3 mousePosition)
-    {
-        targetPosition = mousePosition;
-        player.ChannelService.TargetPosition = targetPosition;
-    }
-
-    private void SetTarget(Enemy enemy)
-    {
-        if (enemy == target) return;
-
-        RemoveTarget();
-
-        enemy.PlayerTargeted.Next(true);
-        targetDeathSubscription = enemy.DeathSubject.Subscribe(() => RemoveTarget());
-        target = enemy;
-        player.ChannelService.Target = enemy;
-    }
-
-    private void RemoveTarget()
-    {
-        if (target == null) return;
-
-        target.PlayerTargeted.Next(false);
-        targetDeathSubscription.Unsubscribe();
-        target = null;
-        player.ChannelService.Target = null;
     }
 
     private void TickAbilityCooldown()
@@ -200,9 +134,9 @@ public class AbilityManager
             case AbilityType.InstantCast:
                 abilityCast = player.InstantCastService.Cast(
                     abilityReference,
-                    targetPosition,
+                    player.TargetFinder.TargetPosition,
                     subject => abilityFeedbackSubscription = subject.Subscribe(AbilityFeedbackSubscription),
-                    target
+                    player.TargetFinder.Target
                 );
                 nextStatus = CastingStatus.Cooldown;
                 break;
---
+++ Danki2/Assets/Scripts/Actor/Player/PlayerTargetFinder.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Maintains a Target Actor and a TargetLocation Vector3.
+/// </summary>
+public class PlayerTargetFinder
+{
+    private readonly Player player;
+    private Subscription targetDeathSubscription;
+
+    public Vector3 TargetPosition { get; private set; }
+
+    public Enemy Target { get; private set; }
+
+    public PlayerTargetFinder(Player player, Subject updateSubject)
+    {
+        this.player = player;
+        updateSubject.Subscribe(UpdateTarget);
+    }
+
+    private void UpdateTarget()
+    {
+        // First, we raycast for an actor (ie. by ignoring other layers)
+        bool mouseHitCollider = MouseGamePositionFinder.Instance.TryGetMouseGamePosition(
+            out Vector3 mousePosition,
+            out Collider collider,
+            Layers.GetLayerMask(new[] { Layers.Actors })
+        );
+
+        // Then, if we don't hit any actors, we raycast for any collider
+        if (!mouseHitCollider)
+        {
+            mouseHitCollider = MouseGamePositionFinder.Instance.TryGetMouseGamePosition(out mousePosition, out collider);
+        }
+
+        // Then, if no colliders are hit, we use the mouse position on a horizontal plane at the players height.
+        if (!mouseHitCollider)
+        {
+            mousePosition = MouseGamePositionFinder.Instance.GetMousePlanePosition(player.transform.position.y, true);
+        }
+
+        SetTargetPosition(mousePosition);
+
+        if (collider != null && collider.gameObject.CompareTag(Tags.Enemy))
+        {
+            Enemy enemy = collider.gameObject.GetComponent<Enemy>();
+            if (!enemy.Dead) SetTarget(enemy);
+        }
+        else
+        {
+            RemoveTarget();
+        }
+    }
+
+    private void SetTargetPosition(Vector3 mousePosition)
+    {
+        TargetPosition = mousePosition;
+        player.ChannelService.TargetPosition = TargetPosition;
+    }
+
+    private void SetTarget(Enemy enemy)
+    {
+        if (enemy == Target) return;
+
+        RemoveTarget();
+
+        enemy.PlayerTargeted.Next(true);
+        targetDeathSubscription = enemy.DeathSubject.Subscribe(() => RemoveTarget());
+        Target = enemy;
+        player.ChannelService.Target = enemy;
+    }
+
+    private void RemoveTarget()
+    {
+        if (Target == null) return;
+
+        Target.PlayerTargeted.Next(false);
+        targetDeathSubscription.Unsubscribe();
+        Target = null;
+        player.ChannelService.Target = null;
+    }
+}
