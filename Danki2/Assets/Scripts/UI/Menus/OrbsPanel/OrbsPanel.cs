ea6a74dae5db44c07ee07113235236274f6c09bf
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-07-20 20:09:58 +0100

    Adds Placeholder OrbsPanel script

---
+++ Danki2/Assets/Scripts/UI/Menus/OrbsPanel/OrbsPanel.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OrbsPanel : MonoBehaviour
+{
+    private void Start()
+    {
+        Initialise();
+    }
+
+    private void Initialise()
+    {
+        EnumUtils.ForEach<OrbType>(orbType =>
+        {
+            Debug.Log(OrbLookup.Instance.GetDisplayName(orbType));
+        });
+    }
+}
