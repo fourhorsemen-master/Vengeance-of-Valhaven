ee17c874d73ca595237fb7328834820a57c4527b
pipkin94 <andrewgp94@hotmail.co.uk>
2020-07-20 22:06:10 +0100

    projectile logic

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/NetThrowObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/NetThrowObject.cs
@@ -3,4 +3,17 @@ using UnityEngine;
 
 public class NetThrowObject : ProjectileObject
 {
+    [SerializeField]
+    private AudioSource hitAudioSource = null;
+
+    public static NetThrowObject Create(Quaternion rotation, float velocity, float throwAngle, float projectileTime)
+    {
+        NetThrowObject prefab = AbilityObjectPrefabLookup.Instance.NetThrowObjectPrefab;
+        return Instantiate(prefab, position, rotation);
+    }
+
+    public void PlayHitSound()
+    {
+        hitAudioSource.Play();
+    }
 }
--- Danki2/Assets/Scripts/Abilities/Channel/Cast/NetThrow.cs
+++ Danki2/Assets/Scripts/Abilities/Channel/Cast/NetThrow.cs
@@ -3,24 +3,51 @@
 [Ability(AbilityReference.NetThrow)]
 public class NetThrow : Cast
 {
-    protected override float CastTime => 1;
+    protected override float CastTime => 2f;
 
-    private const int SpeedModification = 3;
-    private const float SprintDuration = 5;
+    private const float minimumThrowDistance = 3f;
+    private const float maximumThrowDistance = 15f;
+
+    private float gravity => Physics.gravity.magnitude;
+    private float throwVelocity => Mathf.Sqrt(Mathf.Pow(maximumThrowDistance, 2) / gravity); // R,max = V^2/g
+
+    private const float netRootTime = 4f;
 
     public NetThrow(Actor owner, AbilityData abilityData, string[] availableBonuses)
         : base(owner, abilityData, availableBonuses)
     {
     }
 
-    public override void End(Vector3 target) => End();
-
-    public override void End(Actor actor) => End();
-
-    private void End()
+    public override void End(Vector3 target)
     {
-        LinearStatModification speedModification = new LinearStatModification(Stat.Speed, SpeedModification);
-        Owner.EffectManager.AddActiveEffect(speedModification, SprintDuration);
+        // interpret target
+        Vector3 ownerXZPosition = Owner.transform.position;
+        ownerXZPosition.y = 0;
+        Vector3 targetXZPosition = target;
+        targetXZPosition.y = 0;
+
+        float distance = Vector3.Distance(ownerXZPosition, targetXZPosition);
+        distance = Mathf.Clamp(distance, minimumThrowDistance, maximumThrowDistance);
+
+        float throwHeightOffset = target.y - Owner.transform.position.y;
+
+        float throwAngle =
+            Mathf.Atan(
+                Mathf.Pow(throwVelocity, 2) / (gravity * distance)
+                + Mathf.Sqrt(
+                    Mathf.Pow(throwVelocity, 4)
+                    - gravity * (gravity * Mathf.Pow(distance, 2) + 2 * throwHeightOffset * Mathf.Pow(throwVelocity, 2))
+                    )
+                );
+
+        float projectileTime = distance / (throwVelocity * Mathf.Cos(throwAngle));
+
+        // instantiate a net throw object
+        Owner.transform.LookAt(target, Owner.transform.up);
+
+        NetThrowObject netThrowObject = NetThrowObject.Create()
+
+        // handle rooting of enemies & success feedback
         SuccessFeedbackSubject.Next(true);
     }
 }
