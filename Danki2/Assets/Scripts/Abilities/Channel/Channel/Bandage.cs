4bc7ccd20295249cee06dcb18e7d0698f82c4b41
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-07-25 14:38:55 +0100

    Adds empty bandage ability with ability data and icon

---
+++ Danki2/Assets/Scripts/Abilities/Channel/Channel/Bandage.cs
@@ -0,0 +1,9 @@
+﻿[Ability(AbilityReference.Bandage, new []{"Perseverance"})]
+public class Bandage : Channel
+{
+    public override float Duration => 5f;
+    
+    public Bandage(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+}
