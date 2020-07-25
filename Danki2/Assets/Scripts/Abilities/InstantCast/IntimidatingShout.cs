4ec89e6d50c0cb0d819ff8abf7ed32fa6d3e968b
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-07-25 16:04:50 +0100

    Adds placeholder intimidating shout with icon and ability data

---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/IntimidatingShout.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.IntimidatingShout)]
+public class IntimidatingShout : InstantCast
+{
+    public IntimidatingShout(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 target)
+    {
+        Debug.Log("Intimidating shout...");
+    }
+}
