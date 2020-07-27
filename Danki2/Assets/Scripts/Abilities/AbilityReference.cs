0c9c4aa5f5df199b2206e160d94e0430487e95b2
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-07-27 21:59:15 +0100

    Adds placeholder rend ability with icon and ability data

--- Danki2/Assets/Scripts/Abilities/AbilityReference.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityReference.cs
@@ -19,5 +19,6 @@
     Backstab,
     PiercingRush,
     Parry,
-    IntimidatingShout
+    IntimidatingShout,
+    Rend
 }
---
+++ Danki2/Assets/Scripts/Abilities/Channel/Charge/Rend.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Rend)]
+public class Rend : Charge
+{
+    private const int DamageMultiplier = 3;
+    private const float DotDuration = 3f;
+    
+    protected override float ChargeTime => 3f;
+
+    public Rend(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cancel(Vector3 target) => End();
+
+    public override void End(Vector3 target) => End();
+
+    private void End()
+    {
+        Debug.Log(TimeCharged);
+    }
+}
