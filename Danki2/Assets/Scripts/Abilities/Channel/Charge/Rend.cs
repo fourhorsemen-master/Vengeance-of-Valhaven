16282094a9155a5d2ed0955ab194e062094268bf
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-07-29 19:39:42 +0100

    Implements rend game logic

--- Danki2/Assets/Scripts/Abilities/Channel/Charge/Rend.cs
+++ Danki2/Assets/Scripts/Abilities/Channel/Charge/Rend.cs
@@ -1,15 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 [Ability(AbilityReference.Rend)]
 public class Rend : Charge
 {
     private const int DamageMultiplier = 3;
+    private const float Range = 3f;
     private const float DotDuration = 3f;
+
+    private bool successful = false;
     
     protected override float ChargeTime => 3f;
 
     public Rend(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
     {
+    }
+
+    protected override void Continue()
+    {
+        if (successful) return;
+        if (TimeCharged < 1) return;
+
+        SuccessFeedbackSubject.Next(true);
+        successful = true;
     }
 
     public override void Cancel(Vector3 target) => End();
@@ -18,6 +31,20 @@ public class Rend : Charge
 
     private void End()
     {
-        Debug.Log(TimeCharged);
+        if (!successful)
+        {
+            SuccessFeedbackSubject.Next(false);
+            return;
+        }
+
+        int totalDamage = Mathf.FloorToInt(TimeCharged) * DamageMultiplier;
+        
+        CollisionTemplateManager.Instance
+            .GetCollidingActors(CollisionTemplate.Cylinder, Range, Owner.transform.position)
+            .Where(Owner.Opposes)
+            .ForEach(actor =>
+            {
+                actor.EffectManager.AddActiveEffect(new DOT(totalDamage, DotDuration), DotDuration);
+            });
     }
 }
