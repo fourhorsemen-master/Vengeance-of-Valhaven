90a0e1a33abeb2ff2d5edf6045052ebe75c78366
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-08-16 17:17:08 +0100

    Adds orb tooltip with orb name

---
+++ Danki2/Assets/Scripts/UI/Menus/OrbsPanel/OrbTooltip.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OrbTooltip : Tooltip<OrbTooltip>
+{
+    [SerializeField]
+    private Text description = null;
+
+    private bool heightInitialised = false;
+
+    public void Activate(OrbType orbType)
+    {
+        ActivateTooltip();
+
+        description.text = OrbLookup.Instance.GetDisplayName(orbType);
+
+        if (!heightInitialised)
+        {
+            this.NextFrame(SetHeight);
+            heightInitialised = true;
+        }
+        else
+        {
+            SetHeight();
+        }
+    }
+
+    public void Deactivate() => DeactivateTooltip();
+
+    private void SetHeight()
+    {
+        description.rectTransform.sizeDelta = new Vector2(
+            description.rectTransform.sizeDelta.x,
+            description.preferredHeight
+        );
+
+        tooltipPanel.sizeDelta = new Vector2(
+            tooltipPanel.sizeDelta.x,
+            description.preferredHeight + 12f
+        );
+    }
+}
