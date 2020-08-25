5f031025668beb3e884df455499ca28b1cb9eab2
pipkin94 <andrewgp94@hotmail.co.uk>
2020-08-25 17:41:34 +0100

    changed name over to poisonDagger

--- Danki2/Assets/Scripts/Abilities/InstantCast/PoisonDagger.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/PoisonDagger.cs
@@ -1,19 +1,19 @@
 ﻿using UnityEngine;
 
-[Ability(AbilityReference.DaggerThrow)]
-public class DaggerThrow : InstantCast
+[Ability(AbilityReference.PoisonDagger)]
+public class PoisonDagger : InstantCast
 {
     private const float DaggerSpeed = 20f;
-    private const float DotDuration = 3f;
+    private const float DotDuration = 5f;
 
-    public DaggerThrow(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    public PoisonDagger(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
     {
     }
 
     public override void Cast(Vector3 target)
     {
         Quaternion rotation = Quaternion.LookRotation(target - Owner.Centre);
-        DaggerObject.Fire(Owner, OnCollision, DaggerSpeed, Owner.Centre, rotation);
+        PoisonDaggerObject.Fire(Owner, OnCollision, DaggerSpeed, Owner.Centre, rotation);
     }
 
     private void OnCollision(GameObject gameObject)
