54bc91292f3a3981556f3207c128981c389f1df9
pipkin94 <andrewgp94@hotmail.co.uk>
2020-08-26 17:17:24 +0100

    scripts added for disengage, ability prefab lookup and reference updated

---
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/DisengageObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DisengageObject : StaticAbilityObject
+{
+    public AudioSource dashSound = null;
+
+    public override float StickTime => dashSound.clip.length;
+
+    public static void Create(Transform casterTransform)
+    {
+        DashObject prefab = AbilityObjectPrefabLookup.Instance.DashObjectPrefab;
+        Instantiate(prefab, casterTransform);
+    }
+}
---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Disengage.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Disengage, new[] { "Parting Shot" })]
+public class Disengage : InstantCast
+{
+    private const float MinMovementDuration = 0.1f;
+    private const float MaxMovementDuration = 0.3f;
+    private const float LeapSpeedMultiplier = 6f;
+
+    private const float StunRange = 2f;
+    private const float StunDuration = 3f;
+
+    public Disengage(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 target)
+    {
+        Vector3 position = Owner.transform.position;
+        Vector3 direction = target - position;
+        direction.y = position.y;
+
+        float distance = Vector3.Distance(target, position);
+        float leapSpeed = Owner.GetStat(Stat.Speed) * LeapSpeedMultiplier;
+        float duration = Mathf.Clamp(distance / leapSpeed, MinMovementDuration, MaxMovementDuration);
+
+
+        Owner.MovementManager.TryLockMovement(MovementLockType.Dash, duration, leapSpeed, direction, direction);
+
+        LeapObject.Create(Owner.transform);
+
+        SuccessFeedbackSubject.Next(true);
+
+        if (HasBonus("Momentum")) Owner.WaitAndAct(duration, StunSurroundingEnemies);
+    }
+
+    private void StunSurroundingEnemies()
+    {
+        CollisionTemplateManager.Instance.GetCollidingActors(
+            CollisionTemplate.Cylinder,
+            StunRange,
+            Owner.transform.position
+        ).ForEach(actor =>
+        {
+            if (Owner.Opposes(actor))
+            {
+                actor.EffectManager.AddActiveEffect(new Stun(), StunDuration);
+            }
+        });
+    }
+}
