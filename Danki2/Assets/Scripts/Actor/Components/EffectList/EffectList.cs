c74b463a76b572f869cecfda91d4adac66042904
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-08-27 21:08:41 +0100

    Adds prefabs for effect list

---
+++ Danki2/Assets/Scripts/Actor/Components/EffectList/EffectList.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public enum EffectRenderMode
+{
+    WorldSpace,
+    ScreenSpace
+}
+
+public class EffectList : MonoBehaviour
+{
+    [SerializeField, HideInInspector]
+    private EffectListItem worldSpaceEffectListItemPrefab = null;
+    
+    [SerializeField, HideInInspector]
+    private EffectListItem screenSpaceEffectListItemPrefab = null;
+
+    [SerializeField]
+    private Actor actor = null;
+
+    [SerializeField]
+    private EffectRenderMode effectRenderMode = EffectRenderMode.WorldSpace;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            AddImage();
+        }
+    }
+
+    private void AddImage()
+    {
+        if (effectRenderMode.Equals(EffectRenderMode.WorldSpace))
+        {
+            Instantiate(worldSpaceEffectListItemPrefab, transform);
+            return;
+        }
+        
+        if (effectRenderMode.Equals(EffectRenderMode.ScreenSpace))
+        {
+            Instantiate(screenSpaceEffectListItemPrefab, transform);
+        }
+    }
+}
