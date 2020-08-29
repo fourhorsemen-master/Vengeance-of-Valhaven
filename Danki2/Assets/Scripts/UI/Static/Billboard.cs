2c9cf3c52197b7b0c35931291e51aa3b5e9530d1
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-08-29 17:17:56 +0100

    Adds comment to explain why Billboard uses LateUpdate

--- Danki2/Assets/Scripts/UI/Static/Billboard.cs
+++ Danki2/Assets/Scripts/UI/Static/Billboard.cs
@@ -2,6 +2,11 @@
 
 public class Billboard : MonoBehaviour
 {
+    /// <summary>
+    /// Note: This is LateUpdate() rather than Update() so that it happens after any position updates to parent game
+    ///       objects that this component is on. Otherwise this could be called before the parent game object's Update()
+    ///       and, if it has moved, would cause this game object to be out of position for a frame.
+    /// </summary>
     private void LateUpdate()
     {
         Vector3 lookAtPosition = 2 * transform.position - Camera.main.transform.position;
