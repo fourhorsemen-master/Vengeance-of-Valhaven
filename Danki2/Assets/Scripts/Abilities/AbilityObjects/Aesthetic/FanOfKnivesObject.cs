3d80272c1f760a9afce133a1408f8f27002094af
pipkin94 <andrewgp94@hotmail.co.uk>
2020-08-31 17:39:27 +0100

    changed collision logic to a delayed projectile version

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/FanOfKnivesObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/FanOfKnivesObject.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 public class FanOfKnivesObject : ProjectileObject
 {
     [SerializeField]
-    private AudioSource fireSound = null;
-
-    [SerializeField]
     private AudioSource collisionSound = null;
 
     public static void Fire(Actor caster, Action<GameObject> collisionCallback, float speed, Vector3 position, Quaternion rotation, bool playFireAudio = false)
@@ -14,19 +11,10 @@ public class FanOfKnivesObject : ProjectileObject
         FanOfKnivesObject fanOfKnivesObject = Instantiate(AbilityObjectPrefabLookup.Instance.FanOfKnivesObjectPrefab, position, rotation);
 
         fanOfKnivesObject.InitialiseProjectile(caster, collisionCallback, speed).SetSticky(2f);
-
-        if (playFireAudio) fanOfKnivesObject.FireAudio();
-    }
-
-    private void FireAudio()
-    {
-        fireSound.Play();
     }
 
     protected override void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.GetComponentInParent<FanOfKnivesObject>() != null) return;
-
         base.OnTriggerEnter(other);
 
         if (other.gameObject != caster.gameObject) collisionSound.Play();
--- Danki2/Assets/Scripts/Abilities/InstantCast/FanOfKnives.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/FanOfKnives.cs
@@ -6,6 +6,7 @@ public class FanOfKnives : InstantCast
     private const int numberOfKnives = 3;
     private const float knifeArcAngle = 45f;
     private const float knifeSpeed = 10f;
+    private const float knifeCastInterval = 0.07f;
 
     private int collisionCounter = 0;
 
@@ -16,12 +17,18 @@ public class FanOfKnives : InstantCast
     public override void Cast(Vector3 target)
     {
         Quaternion rotation = Quaternion.LookRotation(target - Owner.Centre);
-        
+
+        Owner.MovementManager.Pause(knifeCastInterval * numberOfKnives);
+
         for (float i = 0; i < numberOfKnives; i++)
         {
             float angleOffset = ((i / (numberOfKnives - 1)) - 0.5f) * knifeArcAngle;
             Quaternion castRotation = rotation * Quaternion.Euler(Vector3.up * angleOffset);
-            FanOfKnivesObject.Fire(Owner, OnCollision, knifeSpeed, Owner.Centre, castRotation);
+
+            Owner.WaitAndAct(
+                knifeCastInterval * i,
+                () => FanOfKnivesObject.Fire(Owner, OnCollision, knifeSpeed, Owner.Centre, castRotation)
+                );    
         }
     }
 
