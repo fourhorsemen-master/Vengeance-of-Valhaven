a30883ebb30b4c564923dae285d37d6e7eae15ed
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-08-31 13:05:46 +0100

    Adds empty hamstring ability

---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Hamstring.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Hamstring, new []{"Hack"})]
+public class Hamstring : InstantCast
+{
+    public Hamstring(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 target)
+    {
+        Debug.Log("Casting hamstring");
+    }
+}
