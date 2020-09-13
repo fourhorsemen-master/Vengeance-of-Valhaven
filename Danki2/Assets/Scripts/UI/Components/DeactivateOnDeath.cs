03ccbea0de79e4b59ca2bf6e7be50e3632cd9d27
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-09-13 17:26:57 +0100

    Adds DeactivateOnDeath component and editor

---
+++ Danki2/Assets/Scripts/UI/Components/DeactivateOnDeath.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DeactivateOnDeath : MonoBehaviour
+{
+    public bool usePlayer = false;
+    public Actor actor = null;
+
+    private void Start()
+    {
+        Subject deathSubject = usePlayer
+            ? RoomManager.Instance.Player.DeathSubject
+            : actor.DeathSubject;
+
+        deathSubject.Subscribe(Deactivate);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
