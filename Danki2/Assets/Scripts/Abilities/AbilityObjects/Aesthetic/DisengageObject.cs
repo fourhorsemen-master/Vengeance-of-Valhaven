184e7b290fc7238eee5d0c05fcceb80b1ede8573
pipkin94 <andrewgp94@hotmail.co.uk>
2020-09-14 15:38:05 +0100

    updated the instantiate for leap and disengage to use Quarternion.identity instead of the casters rotation to prevent the land pfx being orientated incorrectly

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/DisengageObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/DisengageObject.cs
@@ -12,7 +12,7 @@ public class DisengageObject : StaticAbilityObject
 
     public static void Create(Transform casterTransform, float duration)
     {
-        DisengageObject disengageObject = Instantiate(AbilityObjectPrefabLookup.Instance.DisengageObjectPrefab, casterTransform.position, casterTransform.rotation);
+        DisengageObject disengageObject = Instantiate(AbilityObjectPrefabLookup.Instance.DisengageObjectPrefab, casterTransform.position, Quaternion.identity);
 
         disengageObject.WaitAndAct(duration, () =>
         {
