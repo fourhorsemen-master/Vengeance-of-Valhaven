19caa6f63c9c2055dfc6c212948b3e7c1967cf68
pipkin94 <andrewgp94@hotmail.co.uk>
2020-09-16 17:46:24 +0100

    bash pfx with a colour for hitting and missing

--- Danki2/Assets/Scripts/Abilities/InstantCast/Bash.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Bash.cs
@@ -1,21 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
 
 [Ability(AbilityReference.Bash)]
 public class Bash : InstantCast
 {
     private const float StunDuration = 1f;
-    private const float Range = 2f;
+    private const float Range = 3f;
     private const float PauseDuration = 0.3f;
 
     public Bash(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
     {
     }
 
+    public override void Cast(Actor target)
+    {
+        Vector3 castPosition = target.Centre;
+
+        if (NavMesh.SamplePosition(castPosition, out NavMeshHit navMeshHit, Range, -1))
+        {
+            castPosition = navMeshHit.position;
+        }
+
+        Cast(castPosition);
+    }
+
     public override void Cast(Vector3 target)
     {
+        Owner.MovementManager.LookAt(target);
+        Owner.MovementManager.Pause(PauseDuration);
+
         Vector3 position = Owner.transform.position;
         Vector3 castDirection = target - Owner.Centre;
         Quaternion castRotation = GetMeleeCastRotation(castDirection);
+
+        if (Range < Vector3.Distance(target, position))
+        {
+            SuccessFeedbackSubject.Next(false);
+            return;
+        }
 
         bool hasDealtDamage = false;
 
@@ -30,10 +52,7 @@ public class Bash : InstantCast
 
         SuccessFeedbackSubject.Next(hasDealtDamage);
 
-        BashObject.Create(position, castRotation);
-
-        Owner.MovementManager.LookAt(target);
-        Owner.MovementManager.Pause(PauseDuration);
+        BashObject.Create(target, hasDealtDamage);
         
         if (hasDealtDamage)
         {
