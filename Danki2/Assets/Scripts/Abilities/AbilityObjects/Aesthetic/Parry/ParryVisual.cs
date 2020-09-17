7d99bb07dc9bb27aa359809b6de6e424ab4a3142
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-09-17 18:02:03 +0100

    Adds setup for individual parry visuals

---
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/Aesthetic/Parry/ParryVisual.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ParryVisual : StaticAbilityObject
+{
+    public override float StickTime => 2;
+
+    public static void Create(ParryVisual prefab, Vector3 position)
+    {
+        Instantiate(prefab, position, Quaternion.identity);
+    }
+}
