4de3cf82ca111af1ea15d70de28f1fe10fe9f2df
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-10-03 13:07:21 +0100

    Hooks up tracking of kills to the static UI

--- Danki2/Assets/Scripts/UI/Static/Tracking/KillTracker.cs
+++ Danki2/Assets/Scripts/UI/Static/Tracking/KillTracker.cs
@@ -4,9 +4,10 @@ using UnityEngine.UI;
 public class KillTracker : MonoBehaviour
 {
     [SerializeField]
-    private Text number = null;
+    private Text text = null;
 
     private void Start()
     {
+        RoomManager.Instance.KillsSubject.Subscribe(i => text.text = i.ToString());
     }
 }
