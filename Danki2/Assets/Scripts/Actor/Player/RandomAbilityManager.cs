8f289813179d1ea06692aa390c933d477dcfda94
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-10-04 11:12:09 +0100

    Adds random ability when the wave ends

--- Danki2/Assets/Scripts/Actor/Player/RandomAbilityManager.cs
+++ Danki2/Assets/Scripts/Actor/Player/RandomAbilityManager.cs
@@ -1,18 +1,45 @@
-﻿public class RandomAbilityManager
+﻿using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class RandomAbilityManager
 {
     private readonly Player player;
+
+    private readonly List<AbilityReference> availableAbilities = new List<AbilityReference>();
+
+    private readonly ISet<AbilityReference> blockedAbilities = new HashSet<AbilityReference>
+    {
+        AbilityReference.Fireball,
+        AbilityReference.Pounce,
+        AbilityReference.Bite
+    };
 
     public RandomAbilityManager(Player player)
     {
         this.player = player;
 
+        SetupAvailableAbilities();
         RoomManager.Instance.WaveStartSubject.Subscribe(OnWaveStart);
+    }
+
+    private void SetupAvailableAbilities()
+    {
+        EnumUtils.ForEach<AbilityReference>(abilityReference =>
+        {
+            if (blockedAbilities.Contains(abilityReference)) return;
+            availableAbilities.Add(abilityReference);
+        });
     }
 
     private void OnWaveStart(int wave)
     {
         if (wave == 1) return;
         
-        player.AbilityTree.AddToInventory(AbilityReference.Slash);
+        player.AbilityTree.AddToInventory(GetRandomAbility());
+    }
+
+    private AbilityReference GetRandomAbility()
+    {
+        return availableAbilities[Random.Range(0, availableAbilities.Count)];
     }
 }
