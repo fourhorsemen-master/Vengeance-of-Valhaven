04e427e259faa8e90f9c7addbc6cc7bc4d7ca7c7
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-10-04 14:29:20 +0100

    Adds example music that can fade in and out

---
+++ Danki2/Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MusicManager : Singleton<MusicManager>
+{
+    private const float TargetVolume = 1;
+
+    [SerializeField]
+    private float fadeTime = 3f;
+    
+    [SerializeField]
+    private AudioSource[] audioSources = null;
+
+    private Coroutine fadeCoroutine;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)) StartCombatMusic();
+        if (Input.GetKeyDown(KeyCode.L)) StopCombatMusic();
+    }
+
+    public void StartCombatMusic()
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(SelectAudioSource().FadeInRoutine(fadeTime, TargetVolume));
+    }
+
+    public void StopCombatMusic()
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(SelectAudioSource().FadeOutRoutine(fadeTime));
+    }
+
+    private AudioSource SelectAudioSource()
+    {
+        return audioSources[Random.Range(0, audioSources.Length)];
+    }
+}
