afcbfe71eb76bd3424b67df483a8fe22fb410ca8
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-10-05 19:49:26 +0100

    Adds placeholder menu with correct input to activate it

---
+++ Danki2/Assets/Scripts/UI/Menus/GameMenu/GameMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        GameStateController.Instance.GameStateTransitionSubject.Subscribe(gameState =>
+        {
+            gameObject.SetActive(gameState == GameState.InGameMenu);
+        });
+    }
+}
