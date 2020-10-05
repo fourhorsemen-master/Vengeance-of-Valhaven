a3244a879bd21d62402c5d83be642d451ca4f3fa
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-10-05 20:18:43 +0100

    Adds random placeholder quotes to the game menu

---
+++ Danki2/Assets/Scripts/UI/Menus/GameMenu/RandomQuote.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class RandomQuote : MonoBehaviour
+{
+    [SerializeField]
+    private Text text = null;
+    
+    [SerializeField]
+    private string[] quotes = null;
+
+    private void Start()
+    {
+        GameStateController.Instance.GameStateTransitionSubject.Subscribe(_ => SelectRandomQuote());
+    }
+
+    private void SelectRandomQuote()
+    {
+        text.text = $"\"{quotes[Random.Range(0, quotes.Length)]}\"";
+    }
+}
