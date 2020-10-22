f4c99c6d5e3e15ac3fff2df449cb1fb76fbccc7d
Anthony Murphy <am13571@my.bristol.ac.uk>
2020-10-22 15:30:23 +0100

    Adds AllTrigger which triggers if all given triggers trigger

---
+++ Danki2/Assets/Scripts/AI2/AllTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+public class AllTrigger : IAiTrigger
+{
+    private readonly IAiTrigger[] triggers;
+    
+    public AllTrigger(params IAiTrigger[] triggers)
+    {
+        this.triggers = triggers;
+    }
+
+    public void Activate()
+    {
+        foreach (IAiTrigger trigger in triggers)
+        {
+            trigger.Activate();
+        }
+    }
+
+    public bool Triggers()
+    {
+        return triggers.All(t => t.Triggers());
+    }
+
+    public void Deactivate()
+    {
+        foreach (IAiTrigger trigger in triggers)
+        {
+            trigger.Deactivate();
+        }
+    }
+}
