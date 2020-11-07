5605830730e69de7314e82f18f823b07cb5fd580
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-11-07 23:07:17 +0000

    add new registry calss and use it for highlightManager

--- Danki2/Assets/Scripts/Actor/Services/HighlightManager.cs
+++ Danki2/Assets/Scripts/Actor/Services/HighlightManager.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -7,69 +6,33 @@ public class HighlightManager
 {
     private readonly MeshRenderer meshRenderer;
 
-    private readonly Dictionary<Guid, float> highlights = new Dictionary<Guid, float>();
-
-    private readonly Dictionary<Guid, float> durations = new Dictionary<Guid, float>();
-
     private float currentIntensity = 0;
+
+    private Registry<float> registry;
 
     public HighlightManager(Subject updateSubject, MeshRenderer meshRenderer)
     {
         this.meshRenderer = meshRenderer;
 
+        registry = new Registry<float>(updateSubject);
+
         updateSubject.Subscribe(ApplyCurrentHighlight);
     }
 
-    public Guid AddIndefiniteHighlight(float intensity) => AddHighlight(intensity);
+    public Guid AddIndefiniteHighlight(float intensity) => registry.AddIndefinite(intensity);
 
-    public Guid AddTemporaryHighlight(float intensity, float duration)
-    {
-        Guid id = AddHighlight(intensity);
+    public Guid AddTemporaryHighlight(float intensity, float duration) => registry.AddTemporary(intensity, duration);
 
-        durations.Add(id, duration);
-
-        return id;
-    }
-
-    public void RemoveHightlight(Guid id)
-    {
-        // Note that Dicionary.Remove returns false if the key is not found.
-        highlights.Remove(id);
-        durations.Remove(id);
-    }
-
-    private Guid AddHighlight(float intensity)
-    {
-        Guid id = Guid.NewGuid();
-
-        highlights.Add(id, intensity);
-
-        return id;
-    }
+    public void RemoveHightlight(Guid id) => registry.Remove(id);
 
     private void ApplyCurrentHighlight()
     {
-        TickDurations();
-
-        var nexIntensity = highlights.Any() ? highlights.Values.Max() : 0f;
+        var nexIntensity = registry.Entities.Any() ? registry.Entities.Values.Max() : 0f;
 
         if (currentIntensity == nexIntensity) return;
 
         currentIntensity = nexIntensity;
         Color highlight = new Color(nexIntensity, nexIntensity, nexIntensity);
         meshRenderer.material.SetEmissiveColour(highlight);
-    }
-
-    private void TickDurations()
-    {
-        durations.Keys.ToList().ForEach(id =>
-        {
-            durations[id] -= Time.deltaTime;
-
-            if (durations[id] <= 0)
-            {
-                RemoveHightlight(id);
-            }
-        });
     }
 }
---
+++ Danki2/Assets/Scripts/Utils/Registry.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Registry<TEntity>
+{
+    public Dictionary<Guid, TEntity> Entities { get; } = new Dictionary<Guid, TEntity>();
+
+    public Dictionary<Guid, float> TotalDurations { get; } = new Dictionary<Guid, float>();
+
+    public Dictionary<Guid, float> Durations { get; } = new Dictionary<Guid, float>();
+
+    public Registry(Subject updateSubject)
+    {
+        updateSubject.Subscribe(TickDurations);
+    }
+
+    public Guid AddIndefinite(TEntity entity) => Add(entity);
+
+    public Guid AddTemporary(TEntity entity, float duration)
+    {
+        Guid id = Add(entity);
+
+        Durations.Add(id, duration);
+        TotalDurations.Add(id, duration);
+
+        return id;
+    }
+
+    public void Remove(Guid id)
+    {
+        // Note that Dicionary.Remove returns false if the key is not found.
+        Entities.Remove(id);
+        Durations.Remove(id);
+        TotalDurations.Remove(id);
+    }
+
+    private Guid Add(TEntity entity)
+    {
+        Guid id = Guid.NewGuid();
+
+        Entities.Add(id, entity);
+
+        return id;
+    }
+
+    private void TickDurations()
+    {
+        Durations.Keys.ToList().ForEach(id =>
+        {
+            Durations[id] -= Time.deltaTime;
+
+            if (Durations[id] <= 0)
+            {
+                Remove(id);
+            }
+        });
+    }
+}
