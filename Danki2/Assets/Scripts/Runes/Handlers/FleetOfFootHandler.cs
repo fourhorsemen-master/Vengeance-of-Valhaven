5c68254ac8539038b4c2cf772fa93a07349222e9
nick.xue15@gmail.com <nick.xue15@gmail.com>
2020-12-12 16:24:25 +0000

    initial rune concept

---
+++ Danki2/Assets/Scripts/Runes/Handlers/FleetOfFootHandler.cs
@@ -0,0 +1,9 @@
+﻿public class FleetOfFootHandler : IStatPipe
+{
+    public float ProcessStat(Stat stat, float value)
+    {
+        if (stat == Stat.Speed) return value * 1.3f;
+
+        return value;
+    }
+}
---
+++ Danki2/Assets/Scripts/Runes/RuneManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+public class RuneManager
+{
+	private readonly Player player;
+
+	private readonly HashSet<Rune> runes = new HashSet<Rune>();
+
+	public RuneManager(Player player)
+	{
+		this.player = player;
+	}
+
+	public bool HasRune(Rune rune) => runes.Contains(rune);
+
+	// Can't have duplicate runes
+	public bool TryAddRune(Rune rune)
+	{
+		if (!runes.Add(rune)) return false;
+
+		Activate(rune);
+		return true;
+	}
+
+	// No need for a Deactivate method as runes can't be removed
+	public void Activate(Rune rune)
+	{
+		switch (rune)
+		{
+			case Rune.FleetOfFoot:
+				player.StatsManager.RegisterPipe(new FleetOfFootHandler());
+				break;
+		}
+	}
+}
