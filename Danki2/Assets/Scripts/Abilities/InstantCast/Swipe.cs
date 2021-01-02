cb5567b1ce6f4468c87a8703c3b51eba3afa83c5
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-01-02 16:18:35 +0000

    add some logic to the ability

--- Danki2/Assets/Scripts/Abilities/InstantCast/Swipe.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Swipe.cs
@@ -3,12 +3,50 @@
 [Ability(AbilityReference.Swipe)]
 public class Swipe : InstantCast
 {
+    private const float DashDuration = 0.1f;
+    private const float DashSpeedMultiplier = 2f;
+    private const float PauseDuration = 0.3f;
+    private const float DamageRadius = 3f;
+
     public Swipe(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
     {
     }
 
     public override void Cast(Vector3 floorTargetPosition, Vector3 offsetTargetPosition)
     {
-        throw new System.NotImplementedException();
+        Owner.MovementManager.LookAt(floorTargetPosition);
+
+        Owner.MovementManager.TryLockMovement(MovementLockType.Dash, DashDuration, Owner.StatsManager.Get(Stat.Speed) * DashSpeedMultiplier, Owner.transform.forward, Owner.transform.forward);
+
+        Owner.InterruptibleAction(DashDuration, InterruptionType.Hard, DamageOnLand);
+    }
+
+    private void DamageOnLand()
+    {
+        bool hasDealtDamage = false;
+
+        CollisionTemplateManager.Instance.GetCollidingActors(
+            CollisionTemplate.Wedge90,
+            DamageRadius,
+            Owner.transform.position,
+            Quaternion.LookRotation(Owner.transform.forward)
+        ).ForEach(actor =>
+        {
+            if (Owner.Opposes(actor))
+            {
+                DealPrimaryDamage(actor);
+                hasDealtDamage = true;
+            }
+        });
+
+        SweepingStrikeObject.Create(Owner.Centre, GetMeleeCastRotation(Owner.transform.forward));
+
+        Owner.MovementManager.Pause(PauseDuration);
+        SuccessFeedbackSubject.Next(hasDealtDamage);
+
+        if (hasDealtDamage)
+        {
+            CustomCamera.Instance.AddShake(ShakeIntensity.Medium);
+        }
     }
 }
