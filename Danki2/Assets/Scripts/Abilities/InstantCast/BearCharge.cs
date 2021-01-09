f43f68c1d0fc701b95885a15c6a3ed105385c1eb
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-01-09 16:25:07 +0000

    Adds setup for bear charge ability

---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/BearCharge.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.BearCharge)]
+public class BearCharge : InstantCast
+{
+    public BearCharge(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 floorTargetPosition, Vector3 offsetTargetPosition)
+    {
+        Debug.Log("Casting bear charge...");
+    }
+}
