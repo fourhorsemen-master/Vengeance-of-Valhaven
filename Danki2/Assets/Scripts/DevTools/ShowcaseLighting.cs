9dd63e7146f23a695fbc4f4b067d5576914a7ba2
George Armstrong <george.armstrong@mail.bcu.ac.uk>
2021-01-10 15:16:53 +0000

    Adds a new scene along with a new component to cycle through some lighting presets

---
+++ Danki2/Assets/Scripts/DevTools/ShowcaseLighting.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowcaseLighting : MonoBehaviour
+{
+
+    public List<GameObject> Lights;
+
+    private GameObject CurrentActiveLight = null;
+    private bool IsVolumetricsEnabled = false;
+
+    // Start is called before the first frame update
+    void OnValidate()
+    {
+        if(Lights.Count > 0)
+		{
+            CurrentActiveLight = Lights[0];
+            CurrentActiveLight.SetActive(true);
+		}
+
+        foreach (GameObject Light in Lights)
+		{
+            if( !Object.ReferenceEquals(Light, CurrentActiveLight))
+			{
+                Light.SetActive(false);
+			}
+		}
+    }
+
+    public void NextLight()
+	{
+        int index = Lights.IndexOf(CurrentActiveLight);
+        index = ++index % Lights.Count;
+        SetActiveLight(index);
+	}
+
+    public void PrevLight()
+	{
+        int index = Lights.IndexOf(CurrentActiveLight);
+        index = TrueMod(--index, Lights.Count);
+        SetActiveLight(index);
+	}
+
+    public void HomeLight()
+	{
+        SetActiveLight(0);
+	}
+
+    private void SetActiveLight(int NewActiveLightIndex)
+	{
+        CurrentActiveLight.SetActive(false);
+        CurrentActiveLight = Lights[NewActiveLightIndex];
+        CurrentActiveLight.SetActive(true);
+	}
+
+    //A proper modulo, handling negatives as it should.
+    private int TrueMod(int x, int m)
+	{
+        return (x % m + m) % m;
+	}
+}
---
+++ Danki2/Assets/Scripts/Editor/EditorShowcaseLighting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShowcaseLighting))]
+public class EditorShowcaseLighting : Editor
+{
+	SerializedProperty LightsProp;
+
+	private void OnEnable()
+	{
+		LightsProp = serializedObject.FindProperty("Lights");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		ShowcaseLighting SCL = (ShowcaseLighting)target;
+
+		EditorGUILayout.BeginHorizontal(GUILayout.MinHeight(30f));
+		if (GUILayout.Button("Prev"))
+		{
+			SCL.PrevLight();
+		}
+		if (GUILayout.Button("Home"))
+		{
+			SCL.HomeLight();
+		}
+		if (GUILayout.Button("Next"))
+		{
+			SCL.NextLight();
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.PropertyField(LightsProp, new GUIContent("Lights"));
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
