0fd25131f341f575f4268802545dd83076485988
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-01-14 15:25:52 +0000

    set up advance component

--- Danki2/Assets/Scripts/AI/AIs/BearAi.cs
+++ Danki2/Assets/Scripts/AI/AIs/BearAi.cs
@@ -4,10 +4,47 @@ public class BearAi : Ai
 {
     [SerializeField] private Bear bear = null;
 
+    [Header("General")]
+    [SerializeField] private float aggroDistance = 0;
+    [SerializeField] [Tooltip("Value must be less than maxAttackRange.")] private float minAdvanceRange = 0;
+    [SerializeField] [Tooltip("Value must be greater than minAdvanceRange.")] private float maxAttackRange = 0;
+
+    [Header("Advance")]
+    [SerializeField] private float advanceMinTransitionTime = 0;
+    [SerializeField] private float advanceMaxTransitionTime = 0;
+
     protected override Actor Actor => bear;
 
     protected override IStateMachineComponent BuildStateMachineComponent()
     {
-        return new NoOpComponent();
+        Player player = RoomManager.Instance.Player;
+
+        IStateMachineComponent advanceStateMachine = new StateMachine<AdvanceState>(AdvanceState.Walk)
+            .WithComponent(AdvanceState.Walk, new MoveTowards(bear, player)) // note - make this a walk
+            .WithComponent(AdvanceState.Run, new MoveTowards(bear, player))
+            .WithTransition(AdvanceState.Walk, AdvanceState.Run, new RandomTimeElapsed(advanceMinTransitionTime, advanceMaxTransitionTime) | new TakesDamage(bear))
+            .WithTransition(AdvanceState.Run, AdvanceState.Walk, new RandomTimeElapsed(advanceMinTransitionTime, advanceMaxTransitionTime));
+
+        IStateMachineComponent attackStateMachine = new NoOpComponent();
+
+        return new StateMachine<State>(State.Idle)
+            .WithComponent(State.Advance, advanceStateMachine)
+            .WithComponent(State.Attack, attackStateMachine)
+            .WithTransition(State.Idle, State.Advance, new DistanceLessThan(bear, player, aggroDistance) | new TakesDamage(bear))
+            .WithTransition(State.Advance, State.Attack, new DistanceLessThan(bear, player, minAdvanceRange))
+            .WithTransition(State.Attack, State.Advance, new DistanceGreaterThan(bear, player, maxAttackRange));
+    }
+
+    private enum State
+    {
+        Idle,
+        Advance,
+        Attack
+    }
+
+    private enum AdvanceState
+    {
+        Walk,
+        Run
     }
 }
