fdb1d68f84b263ee3004e89d00f37f6777dd01ba
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-01-14 14:46:43 +0000

    setup bear class

--- Danki2/Assets/Scripts/Actor/Enemy/Bear.cs
+++ Danki2/Assets/Scripts/Actor/Enemy/Bear.cs
@@ -1,4 +1,33 @@
-﻿public class Bear : Enemy
+﻿using System;
+using UnityEngine;
+
+public class Bear : Enemy
 {
     public override ActorType Type => ActorType.Bear;
+
+    public void Swipe()
+    {
+        InstantCastService.TryCast(
+            AbilityReference.Swipe,
+            GetSwipeTargetPosition(transform.position),
+            GetSwipeTargetPosition(Centre)
+        );
+    }
+
+    public void Charge()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Maul()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Cleave()
+    {
+        throw new NotImplementedException();
+    }
+
+    private Vector3 GetSwipeTargetPosition(Vector3 origin) => origin + transform.forward;
 }
