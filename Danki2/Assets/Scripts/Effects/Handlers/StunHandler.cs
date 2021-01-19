73e87ea53bb32f4f83c6f73cb8cbbf680498c429
pipkin94 <andrewgp94@hotmail.co.uk>
2021-01-19 20:29:18 +0000

    indent added

--- Danki2/Assets/Scripts/Effects/Handlers/StunHandler.cs
+++ Danki2/Assets/Scripts/Effects/Handlers/StunHandler.cs
@@ -7,8 +7,8 @@
         this.actor = actor;
 
         actor.EffectManager.ActiveEffectAddedSubject
-        .Where(e => e == ActiveEffect.Stun)
-        .Subscribe(_ => actor.InterruptionManager.Interrupt(InterruptionType.Hard));
+            .Where(e => e == ActiveEffect.Stun)
+            .Subscribe(_ => actor.InterruptionManager.Interrupt(InterruptionType.Hard));
     }
 
     public bool Stuns() => actor.EffectManager.HasActiveEffect(ActiveEffect.Stun);
