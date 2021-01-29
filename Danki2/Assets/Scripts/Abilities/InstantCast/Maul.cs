bcd7892c25807ffda4ea1a744b5c7ffe4c9cd853
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-01-29 15:56:12 +0000

    fix success feedback

--- Danki2/Assets/Scripts/Abilities/InstantCast/Maul.cs
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Maul.cs
@@ -3,11 +3,12 @@
 [Ability(AbilityReference.Maul)]
 public class Maul : InstantCast
 {
-    private const int BiteCount = 3;
+    private const int TotalBiteCount = 3;
     private const float BiteInterval = 0.5f;
     private const float BiteRange = 2.5f;
 
     private Direction direction = Direction.Left;
+    private int biteCount = 0;
 
     public Maul(Actor owner, AbilityData abilityData, string[] availableBonuses) : base(owner, abilityData, availableBonuses)
     {
@@ -21,11 +22,13 @@ public class Maul : InstantCast
 
         MaulObject maulObject = MaulObject.Create(Owner.AbilitySource);
 
-        Owner.InterruptibleIntervalAction(BiteInterval, InterruptionType.Hard, () => Bite(castDirection, maulObject), 0f, BiteCount);
+        Owner.InterruptibleIntervalAction(BiteInterval, InterruptionType.Hard, () => Bite(castDirection, maulObject), 0f, TotalBiteCount);
     }
 
     private void Bite(Vector3 castDirection, MaulObject maulObject)
     {
+        biteCount++;
+
         Vector3 horizontalDirection = Vector3.Cross(castDirection, Vector3.up).normalized;
         int directionMultiplier = direction == Direction.Right ? 1 : -1;
         Vector3 randomisedcastDirection = castDirection.normalized + horizontalDirection * 0.25f * directionMultiplier;
@@ -48,11 +51,15 @@ public class Maul : InstantCast
         if (hasDealtDamage)
         {
             CustomCamera.Instance.AddShake(ShakeIntensity.Medium);
+            SuccessFeedbackSubject.Next(true);
+        }
+        else if (biteCount == TotalBiteCount)
+        {
+            SuccessFeedbackSubject.Next(false);
         }
 
-        direction = direction == Direction.Left ? Direction.Right : Direction.Left;
-
-        SuccessFeedbackSubject.Next(hasDealtDamage);
         Owner.MovementManager.Pause(BiteInterval);
+
+        direction = direction == Direction.Left ? Direction.Right : Direction.Left;
     }
 }
