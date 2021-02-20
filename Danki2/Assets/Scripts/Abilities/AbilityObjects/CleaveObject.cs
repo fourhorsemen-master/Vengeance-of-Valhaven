56c500a41aeee89a27fb3584d8e4b0f5dd053cdb
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-02-20 14:43:07 +0000

    set up cleave ability

---
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/CleaveObject.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CleaveObject : MonoBehaviour
+{    public static CleaveObject Create(Vector3 position, Quaternion rotation)
+    {
+        CleaveObject prefab = AbilityObjectPrefabLookup.Instance.CleaveObjectPrefab;
+        return Instantiate(prefab, position, rotation);
+    }
+}
---
+++ Danki2/Assets/Scripts/Abilities/InstantCast/Cleave.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Cleave)]
+public class Cleave : InstantCast
+{
+    private const float Range = 4f;
+    private const float PauseDuration = 0.3f;
+
+    public Cleave(Actor owner, AbilityData abilityData, string fmodStartEvent, string fmodEndEvent, string[] availableBonuses)
+        : base(owner, abilityData, fmodStartEvent, fmodEndEvent, availableBonuses)
+    {
+    }
+
+    public override void Cast(Vector3 floorTargetPosition, Vector3 offsetTargetPosition)
+    {
+        Vector3 castDirection = floorTargetPosition - Owner.transform.position;
+        Quaternion castRotation = GetMeleeCastRotation(castDirection);
+
+        CleaveObject.Create(Owner.AbilitySource, castRotation);
+
+        bool hasDealtDamage = false;
+
+        CollisionTemplateManager.Instance.GetCollidingActors(CollisionTemplate.Wedge180, Range, Owner.CollisionTemplateSource, castRotation)
+            .Where(actor => Owner.Opposes(actor))
+            .ForEach(actor =>
+            {
+                DealPrimaryDamage(actor);
+                hasDealtDamage = true;
+            });
+
+        SuccessFeedbackSubject.Next(hasDealtDamage);
+
+        PlayStartEvent();
+        SlashObject.Create(Owner.AbilitySource, castRotation);
+
+        Owner.MovementManager.LookAt(floorTargetPosition);
+        Owner.MovementManager.Pause(PauseDuration);
+
+        var shakeIntensity = hasDealtDamage ? ShakeIntensity.High : ShakeIntensity.Low;
+        CustomCamera.Instance.AddShake(shakeIntensity);
+    }
+}
