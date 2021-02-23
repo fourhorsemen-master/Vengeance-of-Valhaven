89b98dfbc63a46eb14b48f957970b2ff1f14ee47
pipkin94 <andrewgp94@hotmail.co.uk>
2021-02-23 17:36:19 +0000

    does damage, no animation

--- Danki2/Assets/Scripts/Abilities/Channel/Cast/Execute.cs
+++ Danki2/Assets/Scripts/Abilities/Channel/Cast/Execute.cs
@@ -5,6 +5,8 @@ public class Execute : Cast
 {
     private const float CastRange = 3f;
     private const float PauseDuration = 0.3f;
+    private const float AdditionalDamagePerUnit = 10f; // A value of 10f here would increase the additional damage for every 10% health missing.
+    private const int AdditionalDamageMultiplier = 3; // The amount of additional damage to do per unit missing health.
 
     public Execute(Actor owner, AbilityData abilityData, string fmodStartEvent, string fmodEndEvent, string[] availableBonuses, float duration)
         : base(owner, abilityData, fmodStartEvent, fmodEndEvent, availableBonuses, duration)
@@ -22,6 +24,13 @@ public class Execute : Cast
             SuccessFeedbackSubject.Next(false);
         }
 
+        int additionalDamageUnits = Mathf.CeilToInt((1f - target.HealthManager.HealthProportion) * 100f / AdditionalDamagePerUnit);
+        int additionalDamage = additionalDamageUnits * AdditionalDamageMultiplier;
+
+        DealPrimaryDamage(target, additionalDamage);
+        if (HasBonus("Finishing Touch")) target.EffectManager.AddStack(StackingEffect.Bleed);
+
         Owner.MovementManager.Pause(PauseDuration);
+        SuccessFeedbackSubject.Next(true);
     }
 }
