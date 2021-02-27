1e607f23fdbf4f3a30bb941c3fc9c85802e8dc5f
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-02-27 15:45:30 +0000

    improve supplementary tooltip prefab

--- Danki2/Assets/Scripts/UI/Menus/AbilityTreeEditor/AbilityTooltip/AbilitySupplementaryTooltip.cs
+++ Danki2/Assets/Scripts/UI/Menus/AbilityTreeEditor/AbilityTooltip/AbilitySupplementaryTooltip.cs
@@ -4,14 +4,10 @@ using UnityEngine.UI;
 public class AbilitySupplementaryTooltip : MonoBehaviour
 {
     [SerializeField]
-    private Text title = null;
-
-    [SerializeField]
     private Text description = null;
 
     public void Setup(Keyword keyword)
     {
-        title.text = KeywordLookup.Instance.GetDisplayName(keyword);
-        description.text = KeywordLookup.Instance.GetDescription(keyword);
+        description.text = $"<b>{KeywordLookup.Instance.GetDisplayName(keyword)}:</b> {KeywordLookup.Instance.GetDescription(keyword)}";
     }
 }
