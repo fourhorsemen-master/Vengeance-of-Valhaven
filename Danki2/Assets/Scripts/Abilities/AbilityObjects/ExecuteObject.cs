ac351adf11255d884c20e52902a3619abb4d0474
pipkin94 <andrewgp94@hotmail.co.uk>
2021-02-28 18:59:57 +0000

    vfx added

--- Danki2/Assets/Scripts/Abilities/AbilityObjects/ExecuteObject.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityObjects/ExecuteObject.cs
@@ -1,13 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.VFX;
 
 public class ExecuteObject : StaticAbilityObject
 {
     public override float StickTime => 5f;
 
-    public static void Create(Vector3 position, float scaleFactor = 1f)
+    [SerializeField]
+    private VisualEffect CastVisualEffect = null;
+
+    [SerializeField]
+    private VisualEffect DamageVisualEffect = null;
+
+    public static void Create(Vector3 position, Quaternion rotation, Subject onCastFail, Subject<Vector3> onCastComplete)
     {
-        ExecuteObject prefab = AbilityObjectPrefabLookup.Instance.ExecuteObjectPrefab;
-        ExecuteObject smashObject = Instantiate(prefab, position, Quaternion.identity);
-        smashObject.gameObject.transform.localScale *= scaleFactor;
+        ExecuteObject executeObject = Instantiate(AbilityObjectPrefabLookup.Instance.ExecuteObjectPrefab, position, rotation);
+
+        onCastComplete.Subscribe(location => executeObject.OnCastComplete(location));
+        onCastFail.Subscribe(executeObject.OnCastFail);
+    }
+
+    private void OnCastComplete(Vector3 location)
+    {
+        CastVisualEffect.enabled = false;
+        this.transform.position = location;
+        DamageVisualEffect.enabled = true;
+    }
+
+    private void OnCastFail()
+    {
+        CastVisualEffect.Stop();
     }
 }
--- Danki2/Assets/Scripts/Abilities/Channel/Cast/Execute.cs
+++ Danki2/Assets/Scripts/Abilities/Channel/Cast/Execute.cs
@@ -3,14 +3,30 @@
 [Ability(AbilityReference.Execute, new[] { "Finishing Touch" })]
 public class Execute : Cast
 {
-    private const float CastRange = 3f;
+    private const float CastRange = 5f;
     private const float PauseDuration = 0.3f;
     private const float AdditionalDamagePerUnit = 10f; // A value of 10f here would increase the additional damage for every 10% health missing.
     private const int AdditionalDamageMultiplier = 3; // The amount of additional damage to do per unit missing health.
 
+    private readonly Subject onCastFail = new Subject();
+    private readonly Subject<Vector3> onCastComplete = new Subject<Vector3>();
+
     public Execute(Actor owner, AbilityData abilityData, string fmodStartEvent, string fmodEndEvent, string[] availableBonuses, float duration)
         : base(owner, abilityData, fmodStartEvent, fmodEndEvent, availableBonuses, duration)
     {
+    }
+
+    protected override void Start()
+    {
+        ExecuteObject.Create(Owner.transform.position, Owner.transform.rotation, onCastFail, onCastComplete);
+    }
+
+    protected override void Cancel() => onCastFail.Next();
+
+    public override void End(Vector3 floorTargetPosition, Vector3 offsetTargetPosition)
+    {
+        SuccessFeedbackSubject.Next(false);
+        onCastFail.Next();
     }
 
     public override void End(Actor target)
@@ -22,13 +38,19 @@ public class Execute : Cast
         if (distance > CastRange)
         {
             SuccessFeedbackSubject.Next(false);
+            onCastFail.Next();
+            return;
         }
+
+        onCastComplete.Next(target.transform.position);
 
         int additionalDamageUnits = Mathf.CeilToInt((1f - target.HealthManager.HealthProportion) * 100f / AdditionalDamagePerUnit);
         int additionalDamage = additionalDamageUnits * AdditionalDamageMultiplier;
 
         DealPrimaryDamage(target, additionalDamage);
         if (HasBonus("Finishing Touch")) target.EffectManager.AddStack(StackingEffect.Bleed);
+
+        CustomCamera.Instance.AddShake(ShakeIntensity.High);
 
         Owner.MovementManager.Pause(PauseDuration);
         SuccessFeedbackSubject.Next(true);
