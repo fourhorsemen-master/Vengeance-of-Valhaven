564755fcf972ad336d698de781d810451e4120df
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-02 14:29:16 +0000

    Adds placeholder main menu with logging buttons

---
+++ Danki2/Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Continue()
+    {
+        Debug.Log("Continuing previous game...");
+    }
+    
+    public void NewGame()
+    {
+        Debug.Log("Starting new game...");
+    }
+}
