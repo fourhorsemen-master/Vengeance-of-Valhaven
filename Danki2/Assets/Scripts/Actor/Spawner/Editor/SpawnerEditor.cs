f8b749bc4a362e09f713dafcaa2f32d571f53d56
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-08 10:44:54 +0000

    Adds actor lookup to spawner and editor

---
+++ Danki2/Assets/Scripts/Actor/Spawner/Editor/SpawnerEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Spawner))]
+public class SpawnerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Spawner spawner = (Spawner) target;
+
+        spawner.id = EditorGUILayout.IntField("ID", spawner.id);
+
+        EnumUtils.ForEach<ActorType>(actorType =>
+        {
+            spawner.prefabLookup[actorType] = (Actor) EditorGUILayout.ObjectField(
+                $"{actorType.ToString()} prefab",
+                spawner.prefabLookup[actorType],
+                typeof(Actor),
+                false,
+                null
+            );
+        });
+
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+        }
+    }
+}
---
+++ Danki2/Assets/Scripts/Actor/Spawner/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] public int id = 0;
+    [SerializeField] public SpawnerDictionary prefabLookup = new SpawnerDictionary(defaultValue: null);
+
+    public int Id => id;
+
+    public Actor Spawn(ActorType actorType)
+    {
+        if (prefabLookup.ContainsKey(actorType)) return Instantiate(prefabLookup[actorType], transform.position, transform.rotation);
+
+        Debug.LogError($"No prefab found for actor type: {actorType.ToString()}.");
+        return null;
+    }
+}
