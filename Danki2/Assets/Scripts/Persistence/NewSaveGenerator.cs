7699784766927f4531eb7b5b9e48a1524fd3ce24
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-08 11:02:47 +0000

    Pulls out generating of new save data into separate class

---
+++ Danki2/Assets/Scripts/Persistence/NewSaveGenerator.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+
+public static class NewSaveGenerator
+{
+    private const int SaveDataVersion = 1;
+
+    public static SaveData Generate()
+    {
+        return new SaveData
+        {
+            Version = SaveDataVersion,
+            PlayerHealth = 100,
+            AbilityTree = AbilityTreeFactory.CreateTree(
+                new EnumDictionary<AbilityReference, int>(3),
+                AbilityTreeFactory.CreateNode(AbilityReference.SweepingStrike),
+                AbilityTreeFactory.CreateNode(AbilityReference.Lunge)
+            ),
+            CurrentSceneId = 0,
+            SceneSaveDataLookup = GenerateNewSceneSaveDataLookup(),
+            SceneTransitions = GenerateNewSceneTransitions()
+        };
+    }
+
+    private static Dictionary<int, SceneSaveData> GenerateNewSceneSaveDataLookup()
+    {
+        return new Dictionary<int, SceneSaveData>
+        {
+            [0] =
+                new SceneSaveData
+                {
+                    Id = 0,
+                    Scene = Scene.GameplayScene1,
+                    SceneType = SceneType.Combat,
+                    CombatSceneSaveData = new CombatSceneSaveData
+                    {
+                        EnemiesCleared = false,
+                        SpawnerIdToSpawnedActor = new Dictionary<int, ActorType>
+                        {
+                            [0] = ActorType.Wolf
+                        }
+                    }
+                },
+            [1] = new SceneSaveData
+            {
+                Id = 1,
+                Scene = Scene.GameplayScene2,
+                SceneType = SceneType.Combat,
+                CombatSceneSaveData = new CombatSceneSaveData
+                {
+                    EnemiesCleared = false,
+                    SpawnerIdToSpawnedActor = new Dictionary<int, ActorType>
+                    {
+                        [0] = ActorType.Wolf,
+                        [1] = ActorType.Wolf
+                    }
+                }
+            },
+            [2] = new SceneSaveData
+            {
+                Id = 2,
+                Scene = Scene.GameplayScene3,
+                SceneType = SceneType.Combat,
+                CombatSceneSaveData = new CombatSceneSaveData
+                {
+                    EnemiesCleared = false,
+                    SpawnerIdToSpawnedActor = new Dictionary<int, ActorType>
+                    {
+                        [0] = ActorType.Bear
+                    }
+                }
+            },
+            [3] = new SceneSaveData
+            {
+                Id = 3,
+                Scene = Scene.GameplayVictoryScene,
+                SceneType = SceneType.Victory
+            }
+        };
+    }
+
+    private static Dictionary<int, List<int>> GenerateNewSceneTransitions()
+    {
+        return new Dictionary<int, List<int>>
+        {
+            [0] = ListUtils.Singleton(1),
+            [1] = ListUtils.Singleton(2),
+            [2] = ListUtils.Singleton(3)
+        };
+    }
+}
--- Danki2/Assets/Scripts/Persistence/PersistenceManager.cs
+++ Danki2/Assets/Scripts/Persistence/PersistenceManager.cs
@@ -1,9 +1,5 @@
-﻿using System.Collections.Generic;
-
-public class PersistenceManager : NotDestroyedOnLoadSingleton<PersistenceManager>
+﻿public class PersistenceManager : NotDestroyedOnLoadSingleton<PersistenceManager>
 {
-    private const int SaveDataVersion = 1;
-    
     public virtual SaveData SaveData { get; private set; }
 
     protected virtual void Start()
@@ -14,7 +10,7 @@ public class PersistenceManager : NotDestroyedOnLoadSingleton<PersistenceManager
             return;
         }
 
-        SaveData = GenerateNewSaveData();
+        SaveData = NewSaveGenerator.Generate();
         SaveDataManager.Instance.Save(SaveData);
     }
 
@@ -44,89 +40,5 @@ public class PersistenceManager : NotDestroyedOnLoadSingleton<PersistenceManager
             CombatSceneSaveData combatSceneSaveData = currentSceneSaveData.CombatSceneSaveData;
             combatSceneSaveData.EnemiesCleared = CombatSceneManager.Instance.EnemiesCleared;
         }
-    }
-    
-    private SaveData GenerateNewSaveData()
-    {
-        return new SaveData
-        {
-            Version = SaveDataVersion,
-            PlayerHealth = 100,
-            AbilityTree = AbilityTreeFactory.CreateTree(
-                new EnumDictionary<AbilityReference, int>(3),
-                AbilityTreeFactory.CreateNode(AbilityReference.SweepingStrike),
-                AbilityTreeFactory.CreateNode(AbilityReference.Lunge)
-            ),
-            CurrentSceneId = 0,
-            SceneSaveDataLookup = GenerateNewSceneSaveDataLookup(),
-            SceneTransitions = GenerateNewSceneTransitions()
-        };
-    }
-
-    private Dictionary<int, SceneSaveData> GenerateNewSceneSaveDataLookup()
-    {
-        return new Dictionary<int, SceneSaveData>
-        {
-            [0] =
-                new SceneSaveData
-                {
-                    Id = 0,
-                    Scene = Scene.GameplayScene1,
-                    SceneType = SceneType.Combat,
-                    CombatSceneSaveData = new CombatSceneSaveData
-                    {
-                        EnemiesCleared = false,
-                        SpawnerIdToSpawnedActor = new Dictionary<int, ActorType>
-                        {
-                            [0] = ActorType.Wolf
-                        }
-                    }
-                },
-            [1] = new SceneSaveData
-            {
-                Id = 1,
-                Scene = Scene.GameplayScene2,
-                SceneType = SceneType.Combat,
-                CombatSceneSaveData = new CombatSceneSaveData
-                {
-                    EnemiesCleared = false,
-                    SpawnerIdToSpawnedActor = new Dictionary<int, ActorType>
-                    {
-                        [0] = ActorType.Wolf,
-                        [1] = ActorType.Wolf
-                    }
-                }
-            },
-            [2] = new SceneSaveData
-            {
-                Id = 2,
-                Scene = Scene.GameplayScene3,
-                SceneType = SceneType.Combat,
-                CombatSceneSaveData = new CombatSceneSaveData
-                {
-                    EnemiesCleared = false,
-                    SpawnerIdToSpawnedActor = new Dictionary<int, ActorType>
-                    {
-                        [0] = ActorType.Bear
-                    }
-                }
-            },
-            [3] = new SceneSaveData
-            {
-                Id = 3,
-                Scene = Scene.GameplayVictoryScene,
-                SceneType = SceneType.Victory
-            }
-        };
-    }
-
-    Dictionary<int, List<int>> GenerateNewSceneTransitions()
-    {
-        return new Dictionary<int, List<int>>
-        {
-            [0] = ListUtils.Singleton(1),
-            [1] = ListUtils.Singleton(2),
-            [2] = ListUtils.Singleton(3)
-        };
     }
 }
