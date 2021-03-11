c18ef4a537bd1b9df18951e0ebd633a5323d76af
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-11 17:19:43 +0000

    Reverts back so that demo scene works from entry scene

--- Danki2/Assets/Scripts/Scenes/Randomisation/ModuleManager.cs
+++ Danki2/Assets/Scripts/Scenes/Randomisation/ModuleManager.cs
@@ -4,38 +4,38 @@ using UnityEngine;
 
 public class ModuleManager : Singleton<ModuleManager>
 {
-    // private void Start()
-    // {
-    //     Random.InitState(PersistenceManager.Instance.SaveData.CurrentRoomSaveData.ModuleSeed);
-    //
-    //     List<ModuleSocket> sockets = FindObjectsOfType<ModuleSocket>().ToList();
-    //     sockets.Sort();
-    //     
-    //     sockets.ForEach(socket =>
-    //     {
-    //         List<GameObject> prefabsWithTags = ModuleLookup.Instance.GetPrefabsWithTags(socket.SocketType, socket.Tags);
-    //         GameObject prefab = RandomUtils.Choice(prefabsWithTags);
-    //         Instantiate(prefab, socket.transform);
-    //     });
-    // }
-
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.K)) Regenerate();
-    }
-
-    private void Regenerate()
-    {
+        Random.InitState(PersistenceManager.Instance.SaveData.CurrentRoomSaveData.ModuleSeed);
+    
         List<ModuleSocket> sockets = FindObjectsOfType<ModuleSocket>().ToList();
         sockets.Sort();
-
+        
         sockets.ForEach(socket =>
         {
-            foreach (Transform t in socket.transform) Destroy(t.gameObject);
-
             List<GameObject> prefabsWithTags = ModuleLookup.Instance.GetPrefabsWithTags(socket.SocketType, socket.Tags);
             GameObject prefab = RandomUtils.Choice(prefabsWithTags);
             Instantiate(prefab, socket.transform);
         });
     }
+
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.K)) Regenerate();
+    // }
+    //
+    // private void Regenerate()
+    // {
+    //     List<ModuleSocket> sockets = FindObjectsOfType<ModuleSocket>().ToList();
+    //     sockets.Sort();
+    //
+    //     sockets.ForEach(socket =>
+    //     {
+    //         foreach (Transform t in socket.transform) Destroy(t.gameObject);
+    //
+    //         List<GameObject> prefabsWithTags = ModuleLookup.Instance.GetPrefabsWithTags(socket.SocketType, socket.Tags);
+    //         GameObject prefab = RandomUtils.Choice(prefabsWithTags);
+    //         Instantiate(prefab, socket.transform);
+    //     });
+    // }
 }
