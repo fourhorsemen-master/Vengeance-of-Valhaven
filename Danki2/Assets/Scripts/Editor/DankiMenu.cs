6e644cbfa0a0066d4efff5efa0879e4cc7f15740
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-03-13 21:53:37 +0000

    add Danki/Run Entry Scene

---
+++ Danki2/Assets/Scripts/Editor/DankiMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+[InitializeOnLoad]
+public static class DankiMenu
+{
+    [MenuItem("Danki/Run Entry Scene")]
+    private static void RunEntryScene()
+    {
+        EditorApplication.OpenScene("Assets/Scenes/MetaScenes/EntryScene/EntryScene.unity");
+        
+        EditorApplication.isPlaying = true;
+    }
+}
