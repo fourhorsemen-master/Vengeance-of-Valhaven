b8a81f6863722c50213ecbbc186e11ac76a6567a
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-15 16:04:01 +0000

    Only makes room transitioner active if it is a valid transition in the save data

--- Danki2/Assets/Scripts/RoomManagement/RoomTransitioner.cs
+++ Danki2/Assets/Scripts/RoomManagement/RoomTransitioner.cs
@@ -5,17 +5,21 @@ public class RoomTransitioner : MonoBehaviour
     private const float TransitionDistance = 1;
 
     [SerializeField] public int id = 0;
-    [SerializeField] private Color color = Color.white;
     [SerializeField] private Light pointLight = null;
 
+    private bool active = false;
+    
     private void Start()
     {
-        pointLight.color = color;
+        active = PersistenceManager.Instance.SaveData.CurrentRoomSaveData.RoomTransitionerIdToNextRoomId.ContainsKey(id);
+        if (!active) return;
+
         GameplayRoomTransitionManager.Instance.CanTransitionSubject.Subscribe(HandleCanTransitionSubject);
     }
 
     private void Update()
     {
+        if (!active) return;
         if (!GameplayRoomTransitionManager.Instance.CanTransition) return;
         if (transform.DistanceFromPlayer() > TransitionDistance) return;
 
