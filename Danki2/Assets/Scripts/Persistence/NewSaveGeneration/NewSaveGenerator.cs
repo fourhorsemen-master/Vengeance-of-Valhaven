f4a41e2c84fbd7f854d12fff116f3ab1828a5a18
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-17 09:05:41 +0000

    Adds RoomLayoutNode and WIP in NewSaveGeneration

--- Danki2/Assets/Scripts/Persistence/NewSaveGeneration/NewSaveGenerator.cs
+++ Danki2/Assets/Scripts/Persistence/NewSaveGeneration/NewSaveGenerator.cs
@@ -1,15 +1,37 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public static class NewSaveGenerator
 {
     private const int SaveDataVersion = 1;
 
+    private const int MinRoomExits = 1;
+    private const int MaxRoomExits = 3;
+    private const int MinRoomDepth = 2;
+    private const int MaxRoomDepth = 3;
+
+    private static Dictionary<Pole, Pole> ExitDirectionToEntranceDirectionLookup = new Dictionary<Pole, Pole>
+    {
+        [Pole.North] = Pole.South,
+        [Pole.East] = Pole.West,
+        [Pole.South] = Pole.North,
+        [Pole.West] = Pole.East,
+    };
+    
     /// <summary>
     /// Generates a new save data object which can be used to start a new game.
     /// </summary>
-    public static SaveData Generate()
+    /// <param name="seed"> An optional seed to generate the save with </param>
+    public static SaveData Generate(int seed = -1)
     {
+        if (seed == -1) seed = Random.Range(0, int.MaxValue);
+        Random.InitState(seed);
+
+        RoomLayoutNode rootNode = new RoomLayoutNode();
+        RecursivelyGenerateChildren(rootNode, 1);
+        SetIds(rootNode);
+
         return new SaveData
         {
             Version = SaveDataVersion,
@@ -21,10 +43,152 @@ public static class NewSaveGenerator
             ),
             CurrentRoomId = 0,
             DefeatRoomId = 8,
-            RoomSaveDataLookup = GenerateNewRoomSaveDataLookup()
+            RoomSaveDataLookup = GenerateNewRoomSaveDataLookup(rootNode)
         };
     }
 
+    private static void RecursivelyGenerateChildren(RoomLayoutNode node, int currentDepth)
+    {
+        int numberOfChildren = Random.Range(MinRoomExits, MaxRoomExits + 1);
+
+        for (int i = 0; i < numberOfChildren; i++)
+        {
+            RoomLayoutNode childNode = new RoomLayoutNode();
+            node.Children.Add(childNode);
+
+            int nextDepth = currentDepth + 1;
+            if (ShouldGenerateChildren(childNode, nextDepth)) RecursivelyGenerateChildren(childNode, nextDepth);
+        }
+    }
+
+    /// <summary>
+    /// A weighted probability that decides whether a node should have children based on its depth in the tree.
+    /// This works out so that every depth is generated with equal probability.
+    /// </summary>
+    private static bool ShouldGenerateChildren(RoomLayoutNode node, int nodeDepth)
+    {
+        if (nodeDepth < MinRoomDepth) return true;
+        if (nodeDepth >= MaxRoomDepth) return false;
+        return Random.value < 1 / (MaxRoomDepth - nodeDepth + 1);
+    }
+
+    private static void SetIds(RoomLayoutNode rootNode)
+    {
+        int currentId = 0;
+        rootNode.IterateDown(n =>
+        {
+            n.Id = currentId;
+            currentId++;
+        });
+    }
+    
+    private static Dictionary<int, RoomSaveData> GenerateNewRoomSaveDataLookup(RoomLayoutNode rootNode)
+    {
+        Dictionary<int, RoomSaveData> roomSaveDataLookup = new Dictionary<int, RoomSaveData>();
+        RecursivelyGenerateRoomSaveData(rootNode, Pole.South, roomSaveDataLookup);
+        return roomSaveDataLookup;
+    }
+
+    private static void RecursivelyGenerateRoomSaveData(
+        RoomLayoutNode currentNode,
+        Pole entranceDirection,
+        Dictionary<int, RoomSaveData> roomSaveDataLookup
+    )
+    {
+        RoomSaveData roomSaveData = new RoomSaveData();
+        roomSaveData.Id = currentNode.Id;
+
+        int numberOfExits = currentNode.Children.Count;
+        Scene selectedScene = RandomUtils.Choice(GetUsableScenes(entranceDirection, numberOfExits));
+        roomSaveData.Scene = selectedScene;
+
+        roomSaveData.RoomType = RoomType.Combat;
+
+        roomSaveData.CombatRoomSaveData = new CombatRoomSaveData()
+        {
+            EnemiesCleared = false,
+            SpawnerIdToSpawnedActor = GetValidRandomSpawnedActors(selectedScene)
+        };
+        
+        Pole cameraOrientation = GetValidRandomCameraOrientation(selectedScene, entranceDirection, numberOfExits);
+
+        Dictionary<int, int> roomTransitionerIdToNextRoomId = new Dictionary<int, int>();
+        List<int> exitIds = GetValidRandomExitIds(selectedScene, entranceDirection, cameraOrientation, numberOfExits);
+
+        for (int i = 0; i < exitIds.Count; i++)
+        {
+            roomTransitionerIdToNextRoomId[exitIds[i]] = currentNode.Children[i].Id;
+        }
+
+        roomSaveData.RoomTransitionerIdToNextRoomId = roomTransitionerIdToNextRoomId;
+        
+        
+        roomSaveData.ModuleSeed = Random.Range(0, int.MaxValue);
+        
+        roomSaveData.CameraOrientation = cameraOrientation;
+
+        roomSaveData.PlayerSpawnerId = GetValidRandomEntranceId(selectedScene, entranceDirection, cameraOrientation);
+
+        roomSaveDataLookup[roomSaveData.Id] = roomSaveData;
+
+        for (int i = 0; i < currentNode.Children.Count; i++)
+        {
+            RecursivelyGenerateRoomSaveData(
+                currentNode.Children[i],
+                ExitDirectionToEntranceDirectionLookup[SceneLookup.Instance.sceneDataLookup[selectedScene].GameplaySceneData.ExitData.First(d => d.Id == exitIds[i]).Side],
+                roomSaveDataLookup
+            );
+        }
+    }
+
+    private static List<Scene> GetUsableScenes(Pole entranceDirection, int numberOfExits)
+    {
+        // TODO: Select appropriate scenes.
+        return ListUtils.Singleton(Scene.RandomisedScene);
+    }
+
+    private static Pole GetValidRandomCameraOrientation(Scene scene, Pole entranceDirection, int numberOfExits)
+    {
+        return Pole.North;
+    }
+
+    private static int GetValidRandomEntranceId(Scene scene, Pole entranceDirection, Pole cameraOrientation)
+    {
+        return 0;
+    }
+
+    private static List<int> GetValidRandomExitIds(
+        Scene scene,
+        Pole entranceDirection,
+        Pole cameraOrientation,
+        int exitCount
+    )
+    {
+        List<int> list = new List<int>();
+        
+        if (exitCount == 1) list.Add(0);
+
+        if (exitCount == 2)
+        {
+            list.Add(0);
+            list.Add(1);
+        }
+
+        if (exitCount == 3)
+        {
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+        }
+        
+        return list;
+    }
+
+    private static Dictionary<int, ActorType> GetValidRandomSpawnedActors(Scene scene)
+    {
+        return new Dictionary<int, ActorType>{[0] = ActorType.Wolf};
+    }
+    
     private static Dictionary<int, RoomSaveData> GenerateNewRoomSaveDataLookup()
     {
         return new Dictionary<int, RoomSaveData>
---
+++ Danki2/Assets/Scripts/Persistence/NewSaveGeneration/RoomLayoutNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RoomLayoutNode
+{
+    public List<RoomLayoutNode> Children { get; } = new List<RoomLayoutNode>();
+    public int Id { get; set; } = 0;
+
+    public void IterateDown(Action<RoomLayoutNode> action)
+    {
+        action(this);
+        if (Children.Count == 0) return;
+        Children.ForEach(c => c.IterateDown(action));
+    }
+}
