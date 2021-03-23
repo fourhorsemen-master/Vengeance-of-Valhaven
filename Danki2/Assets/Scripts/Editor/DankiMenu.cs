b86933a8957435f415bddf1565fe15918cccef87
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-03-23 14:32:39 +0000

    add danki menu item for assetshowcase

--- Danki2/Assets/Scripts/Editor/DankiMenu.cs
+++ Danki2/Assets/Scripts/Editor/DankiMenu.cs
@@ -3,11 +3,25 @@ using UnityEditor.SceneManagement;
 
 public static class DankiMenu
 {
-    [MenuItem("Danki/Run Entry Scene")]
+    [MenuItem("Danki/Entry Scene")]
     private static void RunEntryScene()
     {
+        if (EditorApplication.isPlaying)
+        {
+            EditorSceneManager.LoadScene("Assets/Scenes/MetaScenes/EntryScene/EntryScene.unity");
+            return;
+        }
+
         EditorSceneManager.OpenScene("Assets/Scenes/MetaScenes/EntryScene/EntryScene.unity");
         
+        EditorApplication.isPlaying = true;
+    }
+
+    [MenuItem("Danki/Asset Showcase")]
+    private static void RunAssetShowcase()
+    {
+        EditorSceneManager.OpenScene("Assets/Scenes/AssetShowcase/AssetShowcase.unity");
+
         EditorApplication.isPlaying = true;
     }
 }
