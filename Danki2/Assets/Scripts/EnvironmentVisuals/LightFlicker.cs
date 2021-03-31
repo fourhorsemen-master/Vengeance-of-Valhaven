343f1df5dcd2b78d617519fb65b08b79300a6579
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-31 12:32:32 +0100

    Adds flickering light to shrine and props in south west area

---
+++ Danki2/Assets/Scripts/EnvironmentVisuals/LightFlicker.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField] private Light lightSource = null;
+    [SerializeField] private PerlinNoiseEmitter perlinNoiseEmitter = null;
+
+    private void Update() => lightSource.intensity = perlinNoiseEmitter.Value;
+}
---
+++ Danki2/Assets/Scripts/Utils/Components/PerlinNoiseEmitter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PerlinNoiseEmitter : MonoBehaviour
+{
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float min = 0;
+    [SerializeField] private float max = 0;
+
+    private float x = 0;
+
+    public float Value { get; private set; }
+
+    private void Update()
+    {
+        x += speed * Time.deltaTime;
+        Value = min + (max - min) * Mathf.PerlinNoise(x, 0);
+    }
+}
