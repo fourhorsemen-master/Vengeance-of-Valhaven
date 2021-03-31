5a415fbef0e3b305ce10242786a9a1180ef7a482
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-03-31 15:26:05 +0100

    Adds extra props around the shrine with some random movement

--- Danki2/Assets/Scripts/Utils/Components/PerlinNoiseEmitter.cs
+++ Danki2/Assets/Scripts/Utils/Components/PerlinNoiseEmitter.cs
@@ -2,13 +2,20 @@
 
 public class PerlinNoiseEmitter : MonoBehaviour
 {
+    private const float MaxStartOffset = 1000;
+
     [SerializeField] private float speed = 0;
     [SerializeField] private float min = 0;
     [SerializeField] private float max = 0;
 
-    private float x = 0;
+    private float x;
 
     public float Value { get; private set; }
+
+    private void Start()
+    {
+        x = Random.Range(0, MaxStartOffset);
+    }
 
     private void Update()
     {
---
+++ Danki2/Assets/Scripts/Utils/Components/PerlinNoiseEmitter2D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PerlinNoiseEmitter2D : MonoBehaviour
+{
+    private const float MaxStartOffset = 1000;
+
+    [SerializeField] private float speed1 = 0;
+    [SerializeField] private float min1 = 0;
+    [SerializeField] private float max1 = 0;
+    [SerializeField] private float speed2 = 0;
+    [SerializeField] private float min2 = 0;
+    [SerializeField] private float max2 = 0;
+
+    private float x;
+    private float y;
+    
+    public float Value1 { get; private set; }
+    public float Value2 { get; private set; }
+
+    private void Start()
+    {
+        x = Random.Range(0, MaxStartOffset);
+        y = Random.Range(0, MaxStartOffset);
+    }
+
+    private void Update()
+    {
+        x += speed1 * Time.deltaTime;
+        y += speed2 * Time.deltaTime;
+        
+        Value1 = min1 + (max1 - min1) * Mathf.PerlinNoise(x, 0);
+        Value2 = min2 + (max2 - min1) * Mathf.PerlinNoise(0, y);
+    }
+}
