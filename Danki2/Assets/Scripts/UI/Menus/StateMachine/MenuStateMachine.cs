2b0d2798ddfa6ff4158381040209d37e850ea8a0
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-09 12:24:27 +0100

    Removes redundant state machine enum

--- Danki2/Assets/Scripts/UI/Menus/StateMachine/MenuStateMachine.cs
+++ Danki2/Assets/Scripts/UI/Menus/StateMachine/MenuStateMachine.cs
@@ -11,34 +11,51 @@ public class MenuStateMachine : StateMachineMonoBehaviour
     
     protected override IStateMachineComponent BuildStateMachineComponent()
     {
-        return new StateMachine<State>(State.Playing)
-            .WithComponent(State.Playing, new GameplayStateComponent(GameplayState.Playing))
-            .WithComponent(State.Paused, new GameplayStateComponent(GameplayState.InPauseMenu))
-            .WithComponent(State.AbilityTree, new GameplayStateComponent(GameplayState.InAbilityTreeEditor))
-            .WithComponent(State.AbilitySelection, new GameplayStateComponent(GameplayState.InAbilitySelection))
-            .WithTransition(State.Playing, State.Paused, new ButtonDown(PauseButtonName))
-            .WithTransition(State.Playing, State.AbilityTree, new ButtonDown(AbilityTreeButtonName))
+        return new StateMachine<GameplayState>(GameplayState.Playing)
+            .WithComponent(GameplayState.Playing, new GameplayStateComponent(GameplayState.Playing))
+            .WithComponent(GameplayState.InPauseMenu, new GameplayStateComponent(GameplayState.InPauseMenu))
+            .WithComponent(GameplayState.InAbilityTreeEditor, new GameplayStateComponent(GameplayState.InAbilityTreeEditor))
+            .WithComponent(GameplayState.InAbilitySelection, new GameplayStateComponent(GameplayState.InAbilitySelection))
             .WithTransition(
-                State.Playing,
-                State.AbilitySelection,
+                GameplayState.Playing,
+                GameplayState.InPauseMenu,
+                new ButtonDown(PauseButtonName)
+            )
+            .WithTransition(
+                GameplayState.Playing,
+                GameplayState.InAbilityTreeEditor,
+                new ButtonDown(AbilityTreeButtonName)
+            )
+            .WithTransition(
+                GameplayState.Playing,
+                GameplayState.InAbilitySelection,
                 new ShrineExists() & new CanInteractWithShrine() & new ButtonDown(AbilitySelectionButtonName)
             )
-            .WithTransition(State.Paused, State.Playing, new ButtonDown(PauseButtonName) | new SubjectEmitted(pauseMenu.ContinueClickedSubject))
-            .WithTransition(State.AbilityTree, State.Playing, new ButtonDown(AbilityTreeButtonName))
-            .WithTransition(State.AbilityTree, State.Paused, new ButtonDown(PauseButtonName))
             .WithTransition(
-                State.AbilitySelection,
-                State.Playing,
-                new SubjectEmitted(abilitySelectionMenu.SkipClickedSubject) | new SubjectEmitted(abilitySelectionMenu.ConfirmClickedSubject) | new ButtonDown(AbilitySelectionButtonName)
+                GameplayState.InPauseMenu,
+                GameplayState.Playing,
+                new ButtonDown(PauseButtonName) | new SubjectEmitted(pauseMenu.ContinueClickedSubject))
+            .WithTransition(
+                GameplayState.InAbilityTreeEditor,
+                GameplayState.Playing,
+                new ButtonDown(AbilityTreeButtonName)
             )
-            .WithTransition(State.AbilitySelection, State.Paused, new ButtonDown(PauseButtonName));
-    }
-
-    private enum State
-    {
-        Playing,
-        Paused,
-        AbilityTree,
-        AbilitySelection
+            .WithTransition(
+                GameplayState.InAbilityTreeEditor,
+                GameplayState.InPauseMenu,
+                new ButtonDown(PauseButtonName)
+            )
+            .WithTransition(
+                GameplayState.InAbilitySelection,
+                GameplayState.Playing,
+                new SubjectEmitted(abilitySelectionMenu.SkipClickedSubject) |
+                new SubjectEmitted(abilitySelectionMenu.ConfirmClickedSubject) |
+                new ButtonDown(AbilitySelectionButtonName)
+            )
+            .WithTransition(
+                GameplayState.InAbilitySelection,
+                GameplayState.InPauseMenu,
+                new ButtonDown(PauseButtonName)
+            );
     }
 }
