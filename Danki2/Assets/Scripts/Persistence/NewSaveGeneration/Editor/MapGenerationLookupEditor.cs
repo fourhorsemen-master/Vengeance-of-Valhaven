4c68b7dce2a118a36cc4a35658f373510d8105fb
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-12 11:42:21 +0100

    Adds weight data to map generation lookup

--- Danki2/Assets/Scripts/Persistence/NewSaveGeneration/Editor/MapGenerationLookupEditor.cs
+++ Danki2/Assets/Scripts/Persistence/NewSaveGeneration/Editor/MapGenerationLookupEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(MapGenerationLookup))]
@@ -6,15 +7,17 @@ public class MapGenerationLookupEditor : Editor
 {
     private MapGenerationLookup mapGenerationLookup;
     
+    private readonly EnumDictionary<RoomType, bool> foldoutStatus = new EnumDictionary<RoomType, bool>(false);
+    
     public override void OnInspectorGUI()
     {
         mapGenerationLookup = (MapGenerationLookup) target;
 
         EditorUtils.ShowScriptLink(mapGenerationLookup);
         
-        EditAbilityChoices();
-        EditDepthData();
-        EditExitData();
+        EditLayoutData();
+        EditorUtils.VerticalSpace();
+        EditRoomDataLookup();
         
         if (GUI.changed)
         {
@@ -22,21 +25,58 @@ public class MapGenerationLookupEditor : Editor
         }
     }
 
-    private void EditAbilityChoices()
+    private void EditLayoutData()
     {
+        EditorUtils.Header("Layout Data");
+        EditorGUI.indentLevel++;
+        
         mapGenerationLookup.AbilityChoices = EditorGUILayout.IntField("Ability Choices", mapGenerationLookup.AbilityChoices);
-    }
-    
-    private void EditDepthData()
-    {
         mapGenerationLookup.MinRoomDepth = EditorGUILayout.IntField("Min Room Depth", mapGenerationLookup.MinRoomDepth);
         mapGenerationLookup.MaxRoomDepth = EditorGUILayout.IntField("Max Room Depth", mapGenerationLookup.MaxRoomDepth);
-
-    }
-
-    private void EditExitData()
-    {
         mapGenerationLookup.MinRoomExits = EditorGUILayout.IntField("Min Room Exits", mapGenerationLookup.MinRoomExits);
         mapGenerationLookup.MaxRoomExits = EditorGUILayout.IntField("Max Room Exits", mapGenerationLookup.MaxRoomExits);
+        
+        EditorGUI.indentLevel--;
+    }
+
+    private void EditRoomDataLookup()
+    {
+        EditorUtils.Header("Room Data");
+        EditorGUI.indentLevel++;
+
+        EnumUtils.ForEach<RoomType>(roomType =>
+        {
+            foldoutStatus[roomType] = EditorGUILayout.Foldout(foldoutStatus[roomType], roomType.ToString());
+            if (!foldoutStatus[roomType]) return;
+
+            EditorGUI.indentLevel++;
+            
+            EditRoomData(mapGenerationLookup.RoomDataLookup[roomType]);
+
+            EditorGUI.indentLevel--;
+            EditorUtils.VerticalSpace();
+        });
+
+        EditorGUI.indentLevel--;
+    }
+
+    private void EditRoomData(RoomData roomData)
+    {
+        roomData.AvailableInPool = EditorGUILayout.Toggle("Available In Pool", roomData.AvailableInPool);
+        EditorGUI.BeginDisabledGroup(!roomData.AvailableInPool);
+
+        EditorUtils.Header("Weights");
+        EditorGUI.indentLevel++;
+
+        int index = 1;
+        EditorUtils.ResizeableList(
+            roomData.Weights,
+            weight => EditorGUILayout.IntField($"Weight {index++}", weight),
+            0
+        );
+        
+        EditorGUI.indentLevel--;
+        
+        EditorGUI.EndDisabledGroup();
     }
 }
--- Danki2/Assets/Scripts/Persistence/NewSaveGeneration/MapGenerationLookup.cs
+++ Danki2/Assets/Scripts/Persistence/NewSaveGeneration/MapGenerationLookup.cs
@@ -1,4 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class RoomData
+{
+    [SerializeField] private bool availableInPool = true;
+    [SerializeField] private List<int> weights = new List<int>();
+
+    public bool AvailableInPool { get => availableInPool; set => availableInPool = value; }
+    public List<int> Weights { get => weights; set => weights = value; }
+}
+
+[Serializable]
+public class RoomDataLookup : SerializableEnumDictionary<RoomType, RoomData>
+{
+    public RoomDataLookup(RoomData defaultValue) : base(defaultValue){}
+    public RoomDataLookup(Func<RoomData> defaultValueProvider) : base(defaultValueProvider) {}
+}
 
 public class MapGenerationLookup : Singleton<MapGenerationLookup>
 {
@@ -9,12 +28,16 @@ public class MapGenerationLookup : Singleton<MapGenerationLookup>
     [SerializeField] private int minRoomExits = 0;
     [SerializeField] private int maxRoomExits = 0;
 
+    [SerializeField] private RoomDataLookup roomDataLookup = new RoomDataLookup(() => new RoomData());
+
     public int AbilityChoices { get => abilityChoices; set => abilityChoices = value; }
 
     public int MinRoomDepth { get => minRoomDepth; set => minRoomDepth = value; }
     public int MaxRoomDepth { get => maxRoomDepth; set => maxRoomDepth = value; }
     public int MinRoomExits { get => minRoomExits; set => minRoomExits = value; }
     public int MaxRoomExits { get => maxRoomExits; set => maxRoomExits = value; }
+
+    public RoomDataLookup RoomDataLookup { get => roomDataLookup; set => roomDataLookup = value; }
 
     protected override bool DestroyOnLoad => false;
 }
