816428068970d44c4f3330a1955b2bb161d01408
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-13 11:34:16 +0100

    Adds indicator sockets to transition modules and adds some example indicator sockets

--- Danki2/Assets/Scripts/RoomManagement/Transitions/TransitionModule.cs
+++ Danki2/Assets/Scripts/RoomManagement/Transitions/TransitionModule.cs
@@ -3,8 +3,25 @@ using UnityEngine;
 
 public class TransitionModule : MonoBehaviour
 {
+    [SerializeField] private List<IndicatorSocket> indicatorSockets = null;
+
     public void InstantiateIndicatedRoomTypes(List<RoomType> indicatedRoomTypes)
     {
-        
+        if (indicatedRoomTypes.Count > indicatorSockets.Count)
+        {
+            Debug.LogError("Tried to instantiate more room type indicators than were available on the transition module");
+            return;
+        }
+
+        for (int i = 0; i < indicatedRoomTypes.Count; i++)
+        {
+            GameObject prefab = RandomUtils.Choice(
+                TransitionModuleLookup
+                    .Instance
+                    .TransitionModuleDictionary[indicatedRoomTypes[i]]
+                    .IndicatorPrefabs
+            );
+            Instantiate(prefab, indicatorSockets[i].transform);
+        }
     }
 }
