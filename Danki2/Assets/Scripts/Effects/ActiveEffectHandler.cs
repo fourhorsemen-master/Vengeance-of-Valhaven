7bbd561e39768249b577b0c13794e2558da21f92
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-13 16:07:54 +0100

    Changes poison to be an active effect rather than a stacking effect

---
+++ Danki2/Assets/Scripts/Effects/ActiveEffectHandler.cs
@@ -0,0 +1,13 @@
+﻿public abstract class ActiveEffectHandler : EffectHandler<ActiveEffect>
+{
+    protected sealed override Subject<ActiveEffect> EffectAddedSubject => effectManager.ActiveEffectAddedSubject;
+    protected sealed override Subject<ActiveEffect> EffectRemovedSubject => effectManager.ActiveEffectRemovedSubject;
+
+    protected ActiveEffectHandler(
+        Actor actor,
+        EffectManager effectManager,
+        ActiveEffect effectToHandle,
+        float tickInterval,
+        float tickStartDelay = 0
+    ) : base(actor, effectManager, effectToHandle, tickInterval, tickStartDelay) {}
+}
---
+++ Danki2/Assets/Scripts/Effects/StackingEffectHandler.cs
@@ -0,0 +1,13 @@
+﻿public abstract class StackingEffectHandler : EffectHandler<StackingEffect>
+{
+    protected sealed override Subject<StackingEffect> EffectAddedSubject => effectManager.StackingEffectAddedSubject;
+    protected sealed override Subject<StackingEffect> EffectRemovedSubject => effectManager.StackingEffectRemovedSubject;
+
+    protected StackingEffectHandler(
+        Actor actor,
+        EffectManager effectManager,
+        StackingEffect effectToHandle,
+        float tickInterval,
+        float tickStartDelay = 0
+    ) : base(actor, effectManager, effectToHandle, tickInterval, tickStartDelay) {}
+}
