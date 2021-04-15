5da1d27b9eef1c0e58b7b65eb76958966b792dc5
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-15 17:14:37 +0100

    Adds groundwork for consume ability

---
+++ Danki2/Assets/Scripts/Abilities/Channel/Cast/Consume.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[Ability(AbilityReference.Consume, new []{ "Vampiric" })]
+public class Consume : Cast
+{
+    public Consume(AbilityConstructionArgs arguments) : base(arguments) {}
+
+    public override void End(Vector3 floorTargetPosition, Vector3 offsetTargetPosition)
+    {
+        Debug.Log("Ending consume");
+    }
+}
