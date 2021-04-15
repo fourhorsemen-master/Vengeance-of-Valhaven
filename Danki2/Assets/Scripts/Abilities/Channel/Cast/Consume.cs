85ff8bbabdf494967f845c3e90fb0fd20523da96
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-15 17:33:04 +0100

    Basic implementation of consume

--- Danki2/Assets/Scripts/Abilities/Channel/Cast/Consume.cs
+++ Danki2/Assets/Scripts/Abilities/Channel/Cast/Consume.cs
@@ -3,10 +3,31 @@
 [Ability(AbilityReference.Consume, new []{ "Vampiric" })]
 public class Consume : Cast
 {
+    private const int DamagePerStack = 2;
+    private const float PauseDuration = 0.3f;
+    private const int HealPerStack = 1;
+    
+    private bool HasVampiric => HasBonus("Vampiric");
+    
     public Consume(AbilityConstructionArgs arguments) : base(arguments) {}
 
     public override void End(Vector3 floorTargetPosition, Vector3 offsetTargetPosition)
     {
-        Debug.Log("Ending consume");
+        int stacksConsumed = 0;
+
+        ActorCache.Instance.Cache
+            .Where(actorCacheItem => Owner.Opposes(actorCacheItem.Actor))
+            .ForEach(actorCacheItem =>
+            {
+                int stacks = actorCacheItem.Actor.EffectManager.GetStacks(StackingEffect.Bleed);
+                stacksConsumed += stacks;
+                actorCacheItem.Actor.EffectManager.RemoveStackingEffect(StackingEffect.Bleed);
+                actorCacheItem.Actor.HealthManager.ReceiveDamage(stacks * DamagePerStack, Owner);
+            });
+
+        SuccessFeedbackSubject.Next(stacksConsumed > 0);
+        Owner.MovementManager.Pause(PauseDuration);
+
+        if (HasVampiric) Owner.HealthManager.ReceiveHeal(stacksConsumed * HealPerStack);
     }
 }
