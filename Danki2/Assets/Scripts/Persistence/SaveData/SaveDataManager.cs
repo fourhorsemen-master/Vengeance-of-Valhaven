7d326807077dedde72fae1d6abc10f0817080eb0
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-19 13:03:01 +0100

    Runs save code in a separate thread

--- Danki2/Assets/Scripts/Persistence/SaveData/SaveDataManager.cs
+++ Danki2/Assets/Scripts/Persistence/SaveData/SaveDataManager.cs
@@ -1,16 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Threading;
+using UnityEngine;
 
 public class SaveDataManager : Singleton<SaveDataManager>
 {
     private string serializedSaveData = null;
 
     public bool HasSaveData => serializedSaveData != null;
+    public BehaviourSubject<bool> SavingSubject { get; } = new BehaviourSubject<bool>(false);
 
     protected override bool DestroyOnLoad => false;
 
-    public void Save(SaveData saveData) => serializedSaveData = JsonUtility.ToJson(saveData.Serialize());
+    public void Save(SaveData saveData)
+    {
+        SavingSubject.Next(true);
+        new Thread(SaveThreadStart).Start(saveData);
+    }
     
     public SaveData Load() => JsonUtility.FromJson<SerializableSaveData>(serializedSaveData).Deserialize();
 
     public void Clear() => serializedSaveData = null;
+
+    private void SaveThreadStart(object saveData)
+    {
+        serializedSaveData = JsonUtility.ToJson(((SaveData) saveData).Serialize());
+        SavingSubject.Next(false);
+    }
 }
