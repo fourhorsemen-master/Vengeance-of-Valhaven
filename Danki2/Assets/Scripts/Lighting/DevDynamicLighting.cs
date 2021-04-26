362d01cf9f8965e975e8b56701bb63df71a6d9ac
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-26 13:53:53 +0100

    Destroys dev dynamic lighting on start

--- Danki2/Assets/Scripts/Lighting/DevDynamicLighting.cs
+++ Danki2/Assets/Scripts/Lighting/DevDynamicLighting.cs
@@ -5,6 +5,11 @@ public class DevDynamicLighting : MonoBehaviour
     [SerializeField] public DynamicLighting dynamicLighting = null;
     [SerializeField] public float depthProportion = 0;
     
+    private void Start()
+    {
+        Destroy(this);
+    }
+
     public void InitialiseLights()
     {
         dynamicLighting.InitialiseLights(depthProportion);
