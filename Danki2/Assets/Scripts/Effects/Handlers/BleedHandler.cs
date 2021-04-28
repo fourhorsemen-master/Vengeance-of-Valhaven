c9ef68e0525210387254068903967df55d8b6ed1
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-28 15:27:31 +0100

    Handles deep wounds rune

--- Danki2/Assets/Scripts/Effects/Handlers/BleedHandler.cs
+++ Danki2/Assets/Scripts/Effects/Handlers/BleedHandler.cs
@@ -3,13 +3,18 @@
     private const float TickInterval = 1f;
     private const float TickStartDelay = 1f;
     private const int DamagePerStack = 1;
+
+    private const int DeepWoundsMultiplier = 3;
     
     public BleedHandler(Actor actor, EffectManager effectManager)
         : base(actor, effectManager, StackingEffect.Bleed, TickInterval, TickStartDelay) {}
 
     protected override void HandleEffectTicked()
     {
-        int damage = DamagePerStack * effectManager.GetStacks(StackingEffect.Bleed);
+        int damage = !actor.IsPlayer && ActorCache.Instance.Player.RuneManager.HasRune(Rune.DeepWounds)
+            ? DamagePerStack * effectManager.GetStacks(StackingEffect.Bleed) * DeepWoundsMultiplier
+            : DamagePerStack * effectManager.GetStacks(StackingEffect.Bleed);
+
         actor.HealthManager.TickDamage(damage);
     }
 }
