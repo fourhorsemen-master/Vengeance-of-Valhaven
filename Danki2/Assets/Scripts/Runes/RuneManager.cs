3186aa185d5cbfbc8bae19a23669cca0ce47a878
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-04-30 09:51:10 +0100

    Adds rune data to persistence manager

--- Danki2/Assets/Scripts/Runes/RuneManager.cs
+++ Danki2/Assets/Scripts/Runes/RuneManager.cs
@@ -4,12 +4,14 @@ using System.Linq;
 public class RuneManager
 {
     public List<RuneSocket> RuneSockets { get; }
+    public int NextRuneIndex { get; private set; }
     public Subject<Rune> RuneAddedSubject { get; } = new Subject<Rune>();
     public Subject<Rune> RuneRemovedSubject { get; } = new Subject<Rune>();
 
     public RuneManager(Player player)
     {
         RuneSockets = PersistenceManager.Instance.SaveData.RuneSockets;
+        NextRuneIndex = PersistenceManager.Instance.SaveData.NextRuneIndex;
 
         player.StatsManager.RegisterPipe(new IronSkinHandler(this, player));
         player.StatsManager.RegisterPipe(new FleetOfFootHandler(this, player));
@@ -32,5 +34,12 @@ public class RuneManager
         runeSocket.HasRune = false;
         runeSocket.Rune = default;
         RuneRemovedSubject.Next(previousRune);
+    }
+
+    public Rune GetNextRune()
+    {
+        Rune nextRune = PersistenceManager.Instance.SaveData.RuneOrder[NextRuneIndex];
+        NextRuneIndex = (NextRuneIndex + 1) % PersistenceManager.Instance.SaveData.RuneOrder.Count;
+        return nextRune;
     }
 }
