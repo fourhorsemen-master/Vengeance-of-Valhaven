7a986a0f5e8264ede71bbc7a143c3e18ee0878d2
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-05-06 12:01:55 +0100

    Groundwork for spawner test

---
+++ Danki2/Assets/Tests/Persistence/NewSaveGeneration/MapGenerationLookupTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+public class MapGenerationLookupTest
+{
+    [UnityTest]
+    public IEnumerator TestScenesAllHaveRequiredSpawners()
+    {
+        List<string> combatScenes = GetCombatScenes();
+        
+        TestUtils.InstantiatePrefab<DevPersistenceManager>();
+        Object.DontDestroyOnLoad(DevPersistenceManager.Instance);
+        
+        combatScenes.ForEach(combatScene =>
+        {
+            SceneManager.LoadScene(combatScene);
+        });
+
+        TestUtils.LoadEmptyScene();
+        Object.Destroy(DevPersistenceManager.Instance.gameObject);
+
+        yield return null;
+    }
+
+    private List<string> GetCombatScenes()
+    {
+        TestUtils.InstantiatePrefab<SceneLookup>();
+
+        List<string> combatScenes = SceneLookup.Instance.sceneDataLookup.Values
+            .Where(sceneData => sceneData.SceneType == SceneType.Gameplay)
+            .Where(sceneData => sceneData.GameplaySceneData.RoomTypes.Contains(RoomType.Combat))
+            .Select(sceneData => sceneData.FileName)
+            .ToList();
+        
+        SceneLookup.Instance.Destroy();
+        return combatScenes;
+    }
+}
