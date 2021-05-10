f58ce69272d002f75ae60c79ece558f8de857bdf
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-05-10 11:38:22 +0100

    Removes SingletonExists and puts logic into CanInteractWithShrine

--- Danki2/Assets/Scripts/StateMachine/Triggers/SingletonExists.cs
+++
@@ -1,6 +0,0 @@
-﻿public class SingletonExists<T> : StateMachineTrigger where T : Singleton<T>
-{
-    public override void Activate() {}
-    public override void Deactivate() {}
-    public override bool Triggers() => Singleton<T>.Exists;
-}
