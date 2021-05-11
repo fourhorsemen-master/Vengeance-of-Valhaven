ad15054081054a3a49f753efc5e47dac17cff94b
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-05-11 17:39:53 +0100

    initial progress on next room indicators

---
+++ Danki2/Assets/Scripts/UI/TransitionPointer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class TransitionPointer : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform rectTransform = null;
+
+    public Vector3 TransitionPosition { private get; set; }
+
+    public static void Create(TransitionPointer prefab, Vector3 transitionPosition)
+    {
+        Instantiate(prefab).TransitionPosition = transitionPosition;
+    }
+
+    private void Start() => SetPosition();
+
+    private void Update() => SetPosition();
+
+    private void SetPosition()
+    {
+        if (CustomCamera.Instance.PointInViewport(TransitionPosition /* + offset */))
+        {
+            Hover();
+        }
+        else
+        {
+            Point();
+        }
+    }
+
+    private void Hover() { }
+
+    private void Point() { }
+}
