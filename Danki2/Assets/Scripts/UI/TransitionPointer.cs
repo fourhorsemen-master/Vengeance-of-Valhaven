d861e9a2f3ee798b88dee95d561eb24ee7386720
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-05-12 21:21:31 +0100

    get arrows working

--- Danki2/Assets/Scripts/UI/TransitionPointer.cs
+++ Danki2/Assets/Scripts/UI/TransitionPointer.cs
@@ -1,35 +1,77 @@
-﻿using System;
+﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TransitionPointer : MonoBehaviour
 {
     [SerializeField]
     private RectTransform rectTransform = null;
 
-    public Vector3 TransitionPosition { private get; set; }
+    [SerializeField]
+    private Image image = null;
+
+    private const float edgeOffset = 20f;
+
+    private Vector3 transitionPosition;
 
     public static void Create(TransitionPointer prefab, Vector3 transitionPosition)
     {
-        Instantiate(prefab).TransitionPosition = transitionPosition;
+        Instantiate(prefab).SetTransitionPosition(transitionPosition);
     }
 
     private void Start() => SetPosition();
 
     private void Update() => SetPosition();
 
+    public void SetTransitionPosition(Vector3 transitionPosition)
+    {
+        this.transitionPosition = transitionPosition;
+    }
+
     private void SetPosition()
     {
-        if (CustomCamera.Instance.PointInViewport(TransitionPosition /* + offset */))
+        if (CustomCamera.Instance.PointInViewport(transitionPosition))
         {
-            Hover();
+            image.enabled = false;
         }
         else
         {
+            image.enabled = true;
             Point();
         }
     }
 
-    private void Hover() { }
+    private void Point()
+    {
+        Vector3 viewportPosition = CustomCamera.Instance.WorldToViewportPoint(transitionPosition);
 
-    private void Point() { }
+        Vector3 clampedPosition = ClampToScreenEdge(viewportPosition);
+
+        float pointerX = Mathf.Clamp(clampedPosition.x * Screen.width, edgeOffset, Screen.width - edgeOffset);
+        float pointerY = Mathf.Clamp(clampedPosition.y * Screen.height, edgeOffset, Screen.height - edgeOffset);
+
+        rectTransform.position = new Vector3(pointerX, pointerY);
+        rectTransform.rotation = Quaternion.FromToRotation(Vector3.down, new Vector3(clampedPosition.x - 0.5f, clampedPosition.y - 0.5f, 0f));
+    }
+
+    private Vector3 ClampToScreenEdge(Vector3 viewportPosition)
+    {
+        // Get the position relative to the centre of the screen
+        var adjustedPosition = viewportPosition - Vector3.one * 0.5f;
+
+        float x, y;
+
+        if (Mathf.Abs(adjustedPosition.x) > Mathf.Abs(adjustedPosition.y))
+        {
+            x = Mathf.Clamp(viewportPosition.x, 0, 1); // We're on the left or right screen edge
+            y = 0.5f * adjustedPosition.y / Mathf.Abs(adjustedPosition.x) + 0.5f;
+        }
+        else
+        {
+            x = 0.5f * adjustedPosition.x / Mathf.Abs(adjustedPosition.y) + 0.5f;
+            y = Mathf.Clamp(viewportPosition.y, 0, 1); // We're on the top or bottom screen edge
+        }
+
+        return new Vector3(x, y);
+    }
 }
