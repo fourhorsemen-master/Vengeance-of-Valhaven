b95c18772631cb3c138e2809ee76144a366b4c2e
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-05-13 14:33:28 +0100

    deactivate diegetic components from diegetic container

--- Danki2/Assets/Scripts/UI/Components/Diegetic.cs
+++ Danki2/Assets/Scripts/UI/Components/Diegetic.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Diegetic : MonoBehaviour
 {
     [SerializeField]
     private Actor actor = null;
 
+    [SerializeField]
+    private List<GameObject> deactivateOnDeath = null;
+
     public Actor Actor => actor;
+
+    private void Start()
+    {
+        Actor.DeathSubject.Subscribe(() =>
+            deactivateOnDeath.ForEach(o => o.gameObject.SetActive(false))
+        );
+    }
 }
