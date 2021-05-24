2a2b709745778aa1485838f8054467b91b6c5643
Andrew Parker <andrewgp94@hotmail.co.uk>
2021-05-24 13:55:36 +0100

    added prefab and updated rooms where it can be used

--- Danki2/Assets/Scripts/RoomManagement/RoomManagers/AbilitySelectionRoom/AbilityShrine.cs
+++ Danki2/Assets/Scripts/RoomManagement/RoomManagers/AbilitySelectionRoom/AbilityShrine.cs
@@ -3,34 +3,30 @@ using UnityEngine.UI;
 
 public class AbilityShrine : Singleton<AbilityShrine>, IShrine
 {
-    [SerializeField] private Text interactionText = null;
+    [SerializeField] private InteractionText interactionText = null;
     [SerializeField] private float interactionDistance = 0;
 
     private bool abilitySelected = false;
 
     private void Start()
     {
-        HideInteractionText();
+        interactionText.Hide();
 
         abilitySelected = AbilitySelectionRoomManager.Instance.AbilitySelected;
         AbilitySelectionRoomManager.Instance.AbilitySelectedSubject.Subscribe(() =>
         {
             abilitySelected = true;
-            HideInteractionText();
+            interactionText.Hide();
         });
     }
 
     private void Update()
     {
-        if (CanInteract()) ShowInteractionText();
-        else HideInteractionText();
+        if (CanInteract()) interactionText.Show();
+        else interactionText.Hide();
     }
 
     public bool CanInteract() => GameplayStateController.Instance.GameplayState == GameplayState.Playing &&
                                  !abilitySelected &&
                                  transform.DistanceFromPlayer() <= interactionDistance;
-    
-    private void ShowInteractionText() => interactionText.enabled = true;
-
-    private void HideInteractionText() => interactionText.enabled = false;
 }
