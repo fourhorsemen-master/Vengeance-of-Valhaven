63f62ac1d534a97b19c894a4cc8e7135283e30af
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-05-25 13:34:14 +0100

    Adds random rotation to roots

--- Danki2/Assets/Scripts/Actor/Enemy/ForestGolem/ForestGolemRoot.cs
+++ Danki2/Assets/Scripts/Actor/Enemy/ForestGolem/ForestGolemRoot.cs
@@ -7,12 +7,19 @@ public class ForestGolemRoot : MonoBehaviour
     [SerializeField] private AnimationCurve height = null;
     [SerializeField] private AnimationCurve rotation = null;
 
+    private void Start()
+    {
+        Rotate(Random.Range(0, 360));
+    }
+
     private void Update()
     {
         Vector3 localPosition = root.transform.localPosition;
         localPosition.y = height.Evaluate(timeTracker.Time);
         root.transform.localPosition = localPosition;
         
-        root.transform.Rotate(Vector3.up, rotation.Evaluate(timeTracker.Time));
+        Rotate(rotation.Evaluate(timeTracker.Time));
     }
+
+    private void Rotate(float angle) => root.transform.Rotate(Vector3.up, angle);
 }
