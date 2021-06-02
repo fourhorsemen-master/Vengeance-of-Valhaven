2541b79c354714536b8fd9b55c615bf7db198529
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-06-02 22:40:33 +0100

    add the menu item

--- Danki2/Assets/Scripts/Editor/DankiMenu.cs
+++ Danki2/Assets/Scripts/Editor/DankiMenu.cs
@@ -1,5 +1,8 @@
-﻿using UnityEditor;
+﻿using System.Linq;
+using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class DankiMenu
 {
@@ -17,5 +20,26 @@ public static class DankiMenu
         EditorSceneManager.OpenScene("Assets/Scenes/AssetShowcase/AssetShowcase.unity");
 
         EditorApplication.isPlaying = true;
+    }
+
+    [MenuItem("Danki/Toggle Dev Mode")]
+    private static void ToggleDevMode(MenuCommand menuCommand)
+    {
+        var metaObject = SceneManager.GetActiveScene()
+            .GetRootGameObjects()
+            .FirstOrDefault(o => o.name.ToLower() == "meta");
+
+        if (metaObject == null)
+        {
+            Debug.LogError("Can only toggle dev mode in a gameplay scene");
+            return;
+        }
+
+        var devManagerObject = metaObject.GetComponentsInChildren<DevPersistenceManager>(true)
+            .First().gameObject;
+
+        devManagerObject.SetActive(!devManagerObject.activeInHierarchy);
+
+        Selection.activeGameObject = devManagerObject;
     }
 }
