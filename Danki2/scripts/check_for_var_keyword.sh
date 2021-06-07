6a16e927c0242324feb4c63729bb3ead40cb3558
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-06-07 12:50:51 +0100

    Renames env vars to be more explicit

--- Danki2/scripts/check_for_var_keyword.sh
+++ Danki2/scripts/check_for_var_keyword.sh
@@ -1,6 +1,6 @@
 eval grep var -wr Danki2/Assets/Scripts \
-  --exclude="$FILES_TO_EXCLUDE" \
-  --exclude-dir="$DIRECTORIES_TO_EXCLUDE"
+  --exclude="$VAR_FILES_TO_EXCLUDE" \
+  --exclude-dir="$VAR_DIRECTORIES_TO_EXCLUDE"
 
 if (($? == 1)); then
   Found no instances of the var keyword, continuing build.
