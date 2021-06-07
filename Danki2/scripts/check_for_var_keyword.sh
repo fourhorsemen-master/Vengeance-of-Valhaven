faa7d0ba5f3f34e21cef04bb6611eed34b2d3db2
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-06-07 12:38:02 +0100

    Adds messages depending on task status

--- Danki2/scripts/check_for_var_keyword.sh
+++ Danki2/scripts/check_for_var_keyword.sh
@@ -3,6 +3,8 @@ eval grep var -wr Danki2/Assets/Scripts \
   --exclude-dir="$DIRECTORIES_TO_EXCLUDE"
 
 if (($? == 1)); then
+  Found no instances of the var keyword, continuing build.
   exit 0
 fi
+echo Found instances of the var keyword, aborting build.
 exit 1
