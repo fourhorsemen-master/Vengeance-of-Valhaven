1c9b4c79ff85631a38f39a42df6ca5aad3e795c1
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-06-07 20:51:22 +0100

    Fixes typo

--- Danki2/scripts/check_for_var_keyword.sh
+++ Danki2/scripts/check_for_var_keyword.sh
@@ -8,7 +8,7 @@ eval grep var -wr Danki2/Assets/Scripts \
   --exclude-dir="$VAR_DIRECTORIES_TO_EXCLUDE"
 
 if (($? == 1)); then
-  Found no instances of the var keyword, continuing build.
+  echo Found no instances of the var keyword, continuing build.
   exit 0
 fi
 echo Found instances of the var keyword, aborting build.
