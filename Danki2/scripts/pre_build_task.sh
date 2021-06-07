959e08d2a9d12730cc5757b0c8549ffa5ce27a5e
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-06-07 12:20:47 +0100

    Fail build if var is found

--- Danki2/scripts/pre_build_task.sh
+++ Danki2/scripts/pre_build_task.sh
@@ -1,2 +1,8 @@
-echo "pre build task testing..."
+eval grep var -wr Danki2/Assets/Scripts \
+  --exclude="$FILES_TO_EXCLUDE" \
+  --exclude-dir="$DIRECTORIES_TO_EXCLUDE"
+
+if (($? == 1)); then
+  exit 0
+fi
 exit 1
