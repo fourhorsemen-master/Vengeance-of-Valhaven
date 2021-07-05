9f089de5675c71a7e02bd159a1f5923be4785507
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-07-05 14:17:21 +0100

    First pass at moving over bear charge

--- Danki2/Assets/Scripts/Actor/Enemy/Bear.cs
+++ Danki2/Assets/Scripts/Actor/Enemy/Bear.cs
@@ -14,6 +14,21 @@ public class Bear : Enemy
     [SerializeField] private float swipePauseDuration = 0f;
     [SerializeField] private float swipeDamageRange = 0f;
 
+    [Header("Charge")]
+    [SerializeField] private int chargeDamage = 0;
+    [SerializeField] private float chargeSpeed = 0;
+    [SerializeField] private float chargeEffectInterval = 0;
+    [SerializeField] private int chargeEffectRepetitions = 0;
+    [SerializeField] private float chargeRotationRate = 0;
+    [SerializeField] private float chargeDamageRange = 0;
+    [SerializeField] private float chargePauseDuration = 0;
+    [SerializeField] private float chargeKnockBackDuration = 0;
+    [SerializeField] private float chargeKnockBackSpeed = 0;
+
+    private Actor chargeTarget = null;
+    private Vector3 chargeDirection;
+    private bool charging = false;
+    
     public override ActorType Type => ActorType.Bear;
 
     public Subject CleaveSubject { get; } = new Subject();
@@ -23,6 +38,13 @@ public class Bear : Enemy
         base.Start();
 
         HealthManager.ModifiedDamageSubject.Subscribe(_ => Roar());
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        
+        if (charging) ContinueCharge();
     }
 
     public void Swipe()
@@ -63,9 +85,65 @@ public class Bear : Enemy
         MovementManager.Pause(swipePauseDuration);
     }
 
-    public void Charge()
+    public void Charge(Actor target)
     {
-        ChannelService.TryStartChannel(AbilityReference.BearCharge);
+        chargeTarget = target;
+        chargeDirection = target.transform.position - transform.position;
+        charging = true;
+        this.ActOnInterval(chargeEffectInterval, ChargeEffect, chargeEffectInterval, chargeEffectRepetitions);
+    }
+
+    private void ContinueCharge()
+    {
+        Vector3 desiredDirection = chargeTarget.transform.position - transform.position;
+        chargeDirection = Vector3.RotateTowards(
+            chargeDirection,
+            desiredDirection,
+            chargeRotationRate * Time.deltaTime,
+            Mathf.Infinity
+        );
+        MovementManager.Move(chargeDirection, chargeSpeed);
+    }
+
+    private void ChargeEffect(int index)
+    {
+        if (index == chargeEffectRepetitions - 1)
+        {
+            MovementManager.Pause(chargePauseDuration);
+            charging = false;
+        }
+        
+        Quaternion castRotation = Quaternion.LookRotation(transform.forward);
+        
+        AbilityUtils.TemplateCollision(
+            this,
+            CollisionTemplateShape.Wedge90,
+            chargeDamageRange,
+            CollisionTemplateSource,
+            castRotation,
+            actor =>
+            {
+                ChargeKnockBack(actor);
+                actor.HealthManager.ReceiveDamage(chargeDamage, this);
+                CustomCamera.Instance.AddShake(ShakeIntensity.Medium);
+            }
+        );
+
+        SwipeObject.Create(AbilitySource, castRotation);
+    }
+
+    private void ChargeKnockBack(Actor actor)
+    {
+        Vector3 knockBackDirection = actor.transform.position - transform.position;
+        Vector3 knockBackFaceDirection = actor.transform.forward;
+
+        actor.MovementManager.TryLockMovement(
+            MovementLockType.Knockback,
+            chargeKnockBackDuration,
+            chargeKnockBackSpeed,
+            knockBackDirection,
+            knockBackFaceDirection
+        );
     }
 
     public void Maul()
