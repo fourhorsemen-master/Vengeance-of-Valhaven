5519897d77519e325710adadb2b048395a305bde
Andrew Parker <andrewgp94@hotmail.co.uk>
2021-07-15 22:29:10 +0100

    reset ability lookup

--- Danki2/Assets/Scripts/Abilities/AbilityConstructionArgs.cs
+++ Danki2/Assets/Scripts/Abilities/AbilityConstructionArgs.cs
@@ -2,32 +2,32 @@
 {
 	public Actor Owner { get; }
 	public AbilityData AbilityDataObject { get; }
+	public string FmodVocalisationEvent { get; }
 	public string FmodStartEvent { get; }
 	public string FmodEndEvent { get; }
 	public string[] ActiveBonuses { get; }
 	public AbilityAnimationType Animation { get; }
-	public AbilityVocalisationType AbilityVocalisationType { get; }
 	public float ChannelDuration { get; }
 
 	public AbilityConstructionArgs
 	(
 		Actor owner,
 		AbilityData abilityData,
+		string fmodVocalisationEvent,
 		string fmodStartEvent,
 		string fmodEndEvent,
 		string[] activeBonuses,
 		AbilityAnimationType animationType,
-		AbilityVocalisationType abilityVocalisationType,
 		float channelDuration = -1f
 	)
 	{
 		Owner = owner;
 		AbilityDataObject = abilityData;
+		FmodVocalisationEvent = fmodVocalisationEvent;
 		FmodStartEvent = fmodStartEvent;
 		FmodEndEvent = fmodEndEvent;
 		ActiveBonuses = activeBonuses;
 		Animation = animationType;
-		AbilityVocalisationType = abilityVocalisationType;
 		ChannelDuration = channelDuration;
 	}
 }
--- Danki2/Assets/Scripts/Abilities/Lookup/AbilityLookup.cs
+++ Danki2/Assets/Scripts/Abilities/Lookup/AbilityLookup.cs
@@ -19,6 +19,7 @@ public class AbilityLookup : Singleton<AbilityLookup>
     private readonly AbilityMap<bool> playerCanCastLookup = new AbilityMap<bool>();
     private readonly AbilityMap<bool> finisherLookup = new AbilityMap<bool>();
     private readonly AbilityMap<float> channelDurationMap = new AbilityMap<float>();
+    private readonly AbilityMap<string> fmodVocalisationsEventRefs = new AbilityMap<string>();
     private readonly AbilityMap<string> fmodStartEventRefs = new AbilityMap<string>();
     private readonly AbilityMap<string> fmodEndEventRefs = new AbilityMap<string>();
     private readonly AbilityMap<AbilityAnimationType> animationTypes = new AbilityMap<AbilityAnimationType>();
@@ -68,6 +69,7 @@ public class AbilityLookup : Singleton<AbilityLookup>
             AbilityConstructionArgs args = new AbilityConstructionArgs(
                 owner,
                 abilityData,
+                fmodVocalisationsEventRefs[abilityReference],
                 fmodStartEventRefs[abilityReference],
                 fmodEndEventRefs[abilityReference],
                 activeBonuses,
@@ -97,6 +99,7 @@ public class AbilityLookup : Singleton<AbilityLookup>
             AbilityConstructionArgs args = new AbilityConstructionArgs(
                 owner,
                 abilityData,
+                fmodVocalisationsEventRefs[abilityReference],
                 fmodStartEventRefs[abilityReference],
                 fmodEndEventRefs[abilityReference],
                 activeBonuses,
@@ -144,6 +147,7 @@ public class AbilityLookup : Singleton<AbilityLookup>
             SerializableAbilityMetadata serializableAbilityMetadata = serializableMetadataLookup[ability];
 
             displayNameMap[ability] = serializableAbilityMetadata.DisplayName;
+            fmodVocalisationsEventRefs[ability] = serializableAbilityMetadata.FmodVocalisationEventRef;
             fmodStartEventRefs[ability] = serializableAbilityMetadata.FmodStartEventRef;
             fmodEndEventRefs[ability] = serializableAbilityMetadata.FmodEndEventRef;
             baseAbilityDataMap[ability] = serializableAbilityMetadata.BaseAbilityData;
