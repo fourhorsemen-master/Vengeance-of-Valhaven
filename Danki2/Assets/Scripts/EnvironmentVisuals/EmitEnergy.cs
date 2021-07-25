716ce2e9e6db143c40c49f3f6bc0f987bc38b012
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-07-25 16:01:12 +0100

    add EmitEnergy component and test with wolf

---
+++ Danki2/Assets/Scripts/EnvironmentVisuals/EmitEnergy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitEnergy : MonoBehaviour
+{
+    [SerializeField]
+    private List<MeshRenderer> meshes = null;
+
+    [SerializeField]
+    private int intensity = 0;
+
+    private void Start()
+    {
+        Color colour = VisualSettings.Instance.EnergyColour * Mathf.Pow(2, intensity);
+
+        meshes.ForEach(m =>  m.material.SetEmissiveColour(colour));
+    }
+}
