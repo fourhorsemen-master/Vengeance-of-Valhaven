832300d8be211bdbed4b8bcb70e9024ccb103e45
Anthony Murphy <am13571@my.bristol.ac.uk>
2021-07-29 21:45:24 +0100

    Adds some simple colour based vfx

--- Danki2/Assets/Scripts/Abilities2/AbilityService2.cs
+++ Danki2/Assets/Scripts/Abilities2/AbilityService2.cs
@@ -6,12 +6,19 @@ public class AbilityCastInformation
     public AbilityType2 Type { get; }
     public bool HasDealtDamage { get; }
     public List<Empowerment> Empowerments { get; }
+    public Quaternion CastRotation { get; }
 
-    public AbilityCastInformation(AbilityType2 type, bool hasDealtDamage, List<Empowerment> empowerments)
+    public AbilityCastInformation(
+        AbilityType2 type,
+        bool hasDealtDamage,
+        List<Empowerment> empowerments,
+        Quaternion castRotation
+    )
     {
         Type = type;
         HasDealtDamage = hasDealtDamage;
         Empowerments = empowerments;
+        CastRotation = castRotation;
     }
 }
 
@@ -60,7 +67,8 @@ public class AbilityService2
         AbilityCastSubject.Next(new AbilityCastInformation(
             AbilityLookup2.Instance.GetAbilityType(ability),
             hasDealtDamage,
-            empowerments
+            empowerments,
+            castRotation
         ));
     }
 
--- Danki2/Assets/Scripts/Abilities2/Visuals/AbilityVfxManager.cs
+++ Danki2/Assets/Scripts/Abilities2/Visuals/AbilityVfxManager.cs
@@ -5,6 +5,9 @@ public class AbilityVfxManager : MonoBehaviour
     [SerializeField]
     private Player player = null;
 
+    [SerializeField]
+    private float offsetIncrement = 0;
+
     private void Start()
     {
         player.AbilityService.AbilityCastSubject.Subscribe(HandleVfx);
@@ -12,6 +15,17 @@ public class AbilityVfxManager : MonoBehaviour
 
     private void HandleVfx(AbilityCastInformation abilityCastInformation)
     {
+        float offset = 0;
         
+        abilityCastInformation.Empowerments.ForEach(empowerment =>
+        {
+            SlashObject.Create(
+                player.AbilitySource,
+                abilityCastInformation.CastRotation * Quaternion.Euler(0, offset, 0),
+                EmpowermentLookup.Instance.GetColour(empowerment)
+            );
+
+            offset += offsetIncrement;
+        });
     }
 }
