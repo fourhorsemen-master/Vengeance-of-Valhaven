8f49a275cc32fa96b1d92d1bbf46dc019d7d6f3d
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-09-12 21:28:06 +0100

    add event driven footsteps for the player

---
+++ Danki2/Assets/Scripts/Actor/Components/Footsteps.cs
@@ -0,0 +1,25 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    [SerializeField]
+    [EventRef]
+    private string footstepEvent = null;
+
+    [SerializeField]
+    private Actor actor;
+
+    // We don't want to hear footsteps when highly blended with other animations
+    private const float MinWeightThreshold = 0.5f;
+
+    public void Step(AnimationEvent animationEvent)
+    {
+        if (animationEvent.animatorClipInfo.weight < MinWeightThreshold) return;
+
+        EventInstance instance = FmodUtils.CreatePositionedInstance(footstepEvent, actor.transform.position);
+        instance.start();
+        instance.release();
+    }
+}
