8438bc911749c9bb4aad854c79a2f4407e3bd9ec
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-09-23 11:17:58 +0100

    sword trail proof of concept

---
+++ Danki2/Assets/Scripts/Abilities2/Visuals/AbilitySwordTrailManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AbilitySwordTrailManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+
+    [SerializeField]
+    private TrailRenderer swordTrailRenderer;
+
+    private void Start()
+    {
+        player.AbilityAnimationListener.SwingSubject.Subscribe(
+            () => swordTrailRenderer.emitting = true
+        );
+
+        player.AbilityAnimationListener.FinishSubject.Subscribe(
+            () => swordTrailRenderer.emitting = false
+        );
+
+        player.ComboManager.SubscribeToStateEntry(
+            ComboState.Interrupted,
+            () => swordTrailRenderer.emitting = false
+        );
+    }
+}
