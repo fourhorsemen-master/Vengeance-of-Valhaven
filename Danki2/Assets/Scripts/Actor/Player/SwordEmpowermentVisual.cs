eef002fe3abd3f4a5751103d8da4676927e8daac
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-10-06 22:38:57 +0100

    add particle effect to empowerment visual

--- Danki2/Assets/Scripts/Actor/Player/SwordEmpowermentVisual.cs
+++ Danki2/Assets/Scripts/Actor/Player/SwordEmpowermentVisual.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.VFX;
 
 public class SwordEmpowermentVisual : MonoBehaviour
 {
@@ -7,6 +8,10 @@ public class SwordEmpowermentVisual : MonoBehaviour
 
     [SerializeField] private TrailRenderer trail;
 
+    [SerializeField] private VisualEffect vfx;
+
+    private const float TrailColourDamping = 0.6f;
+    private const float VFXEmmissiveIntensity = 5f;
 
     public void Activate(Color colour, Material decalMaterial)
     {
@@ -14,13 +19,17 @@ public class SwordEmpowermentVisual : MonoBehaviour
         decal.material = decalMaterial;
 
         trail.emitting = true;
-        Color trailColour = Color.Lerp(colour, Color.white, 0.6f);
+        Color trailColour = Color.Lerp(colour, Color.white, TrailColourDamping);
         trail.material.SetUnlitColour(trailColour);
+
+        vfx.SetVector4("Colour", colour * VFXEmmissiveIntensity);
+        vfx.Play();
     }
 
     public void Reset()
     {
         decal.fadeFactor = 0;
         trail.emitting = false;
+        vfx.Stop();
     }
 }
