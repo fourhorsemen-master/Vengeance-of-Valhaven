d8357da87d78bc45be7247b244ed81221745f8fa
nick.xue15@gmail.com <nick.xue15@gmail.com>
2021-10-28 23:33:09 +0100

    hightlighting spike

--- Danki2/Assets/Scripts/Actor/Services/EmissiveManager.cs
+++ Danki2/Assets/Scripts/Actor/Services/EmissiveManager.cs
@@ -4,61 +4,109 @@ using UnityEngine;
 
 public class EmissiveManager
 {
-    private Dictionary<Material, Texture> materialsToEmissiveMaps;
-    private Dictionary<Material, Color> materialsToEmissiveColours;
+    private Dictionary<Material, Emissive> materialsToEmissive;
     private readonly Actor actor;
 
+    private const float HighlightIntensity = 0.2f;
     private const float FlashIntensity = 0.3f;
     private const float FlashDuration = 0.1f;
 
     private Coroutine cancelFlashCoroutine = null;
+
+    private bool isFlashing = false;
+    private bool isHighlighted = false;
 
     public EmissiveManager(Actor actor, Subject startSubject, Renderer[] renderers)
     {
         this.actor = actor;
 
         startSubject.Subscribe(() => {
-            materialsToEmissiveMaps = renderers.ToDictionary(
+            materialsToEmissive = renderers.ToDictionary(
                 x => x.material,
-                x => x.material.GetEmissiveMap()
-            );
-            materialsToEmissiveColours = renderers.ToDictionary(
-                x => x.material,
-                x => x.material.GetEmissiveColour()
+                x => Emissive.FromMaterial(x.material)
             );
         });
     }
 
-    public void StartHighlight() { }
+    public void StartHighlight()
+    {
+        isHighlighted = true;
+        UpdateEmissive();
+    }
 
-    public void StopHighlight() { }
+    public void StopHighlight()
+    {
+        isHighlighted = false;
+        UpdateEmissive();
+    }
 
     public void Flash()
     {
         if (cancelFlashCoroutine != null) actor.StopCoroutine(cancelFlashCoroutine);
 
-        SetFlashEmissive();
+        isFlashing = true;
+        UpdateEmissive();
 
-        cancelFlashCoroutine = actor.WaitAndAct(FlashDuration, ResetEmissive);
+        cancelFlashCoroutine = actor.WaitAndAct(FlashDuration, () =>
+        {
+            isFlashing = false;
+            UpdateEmissive();
+        });
     }
 
-    private void SetFlashEmissive()
+    private void UpdateEmissive()
     {
-        materialsToEmissiveMaps.Keys.ToList()
-            .ForEach(x =>
+        materialsToEmissive.Keys
+            .ToList()
+            .ForEach(material =>
             {
-                x.SetEmissiveMap(Texture2D.whiteTexture);
-                x.SetEmissiveColour(Color.white * FlashIntensity);
+                Emissive emissive = materialsToEmissive[material];
+
+                Texture map = isFlashing
+                    ? Texture2D.whiteTexture
+                    : isHighlighted
+                        ? emissive.HighlightedMap
+                        : emissive.BaseMap;
+
+                Color colour = isFlashing
+                    ? Color.white * FlashIntensity
+                    : emissive.Colour;
+
+                material.SetEmissiveMap(map);
+                material.SetEmissiveColour(colour);
             });
     }
 
-    private void ResetEmissive()
+    private static Texture2D BuildHighlightedMap(Texture baseMap)
     {
-        materialsToEmissiveMaps.Keys.ToList()
-            .ForEach(x =>
+        Texture2D highlightedMap;
+
+        if (baseMap == null)
+        {
+            highlightedMap = Texture2D.blackTexture;
+            highlightedMap.SetPixels(highlightedMap.GetPixels().Select(x => Color.Lerp(x, Color.white, HighlightIntensity)).ToArray());
+            return highlightedMap;
+        }
+
+        highlightedMap = new Texture2D(baseMap.width, baseMap.height);
+        highlightedMap.SetPixels(((Texture2D)baseMap).GetPixels().Select(x => Color.Lerp(x, Color.white, HighlightIntensity)).ToArray());
+        highlightedMap.Apply();
+
+        return highlightedMap;
+    }
+
+    private class Emissive
+    {
+        public static Emissive FromMaterial(Material material) =>
+            new Emissive
             {
-                x.SetEmissiveMap(materialsToEmissiveMaps[x]);
-                x.SetEmissiveColour(materialsToEmissiveColours[x]);
-            });
+                BaseMap = material.GetEmissiveMap(),
+                HighlightedMap = BuildHighlightedMap(material.GetEmissiveMap()),
+                Colour = material.GetEmissiveColour()
+            };
+
+        public Texture BaseMap { get; private set; }
+        public Texture HighlightedMap { get; private set; }
+        public Color Colour { get; private set; }
     }
 }
