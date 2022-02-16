ebb13f2a226a1e7634127b59a286c632e0dcb633
nick.xue15@gmail.com <nick.xue15@gmail.com>
2022-02-16 22:34:02 +0000

    define dynamic lighting curves per zone

--- Danki2/Assets/Scripts/Lighting/DynamicLighting.cs
+++ Danki2/Assets/Scripts/Lighting/DynamicLighting.cs
@@ -1,34 +1,40 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
-
-[Serializable]
-public class DynamicLightingData
-{
-    [SerializeField] private Gradient color = null;
-    [SerializeField] private AnimationCurve intensity = null;
-    [SerializeField] private AnimationCurve volumetricsMultiplier = null;
-    [SerializeField] private AnimationCurve xAngle = null;
-    [SerializeField] private AnimationCurve yAngle = null;
-    
-    public Gradient Color => color;
-    public AnimationCurve Intensity => intensity;
-    public AnimationCurve VolumetricsMultiplier => volumetricsMultiplier;
-    public AnimationCurve XAngle => xAngle;
-    public AnimationCurve YAngle => yAngle;
-}
 
 public class DynamicLighting : Singleton<DynamicLighting>
 {
     [SerializeField] private HDAdditionalLightData mainLight = null;
     [SerializeField] private HDAdditionalLightData fillLight = null;
     
-    [SerializeField] private DynamicLightingData mainLightData = null;
-    [SerializeField] private DynamicLightingData fillLightData = null;
-    
+    [SerializeField] private DynamicLightingConfig zone1MainLightConfig = null;
+    [SerializeField] private DynamicLightingConfig zone2MainLightConfig = null;
+    [SerializeField] private DynamicLightingConfig zone3MainLightConfig = null;
+
+    [SerializeField] private DynamicLightingConfig zone1FillLightConfig = null;
+    [SerializeField] private DynamicLightingConfig zone2FillLightConfig = null;
+    [SerializeField] private DynamicLightingConfig zone3FillLightConfig = null;
+
+    private Dictionary<Zone, DynamicLightingConfig> mainLightConfigLookup = null;
+    private Dictionary<Zone, DynamicLightingConfig> fillLightConfigLookup = null;
+
     private void Start()
     {
         RoomNode currentRoomNode = PersistenceManager.Instance.SaveData.CurrentRoomNode;
+
+        mainLightConfigLookup = new Dictionary<Zone, DynamicLightingConfig>
+        {
+            { Zone.Zone1, zone1MainLightConfig },
+            { Zone.Zone2, zone2MainLightConfig },
+            { Zone.Zone3, zone3MainLightConfig }
+        };
+
+        fillLightConfigLookup = new Dictionary<Zone, DynamicLightingConfig>
+        {
+            { Zone.Zone1, zone1FillLightConfig },
+            { Zone.Zone2, zone2FillLightConfig },
+            { Zone.Zone3, zone3FillLightConfig }
+        };
 
         InitialiseLights(
             DepthUtils.GetDepthProportion(currentRoomNode),
@@ -38,13 +44,15 @@ public class DynamicLighting : Singleton<DynamicLighting>
 
     public void InitialiseLights(float depthProportion, Pole pole)
     {
+        Zone zone = PersistenceManager.Instance.SaveData.CurrentRoomNode.Zone;
+
         float yRotation = OrientationUtils.GetYRotation(pole);
 
-        InitialiseLight(mainLight, mainLightData, depthProportion, yRotation);
-        InitialiseLight(fillLight, fillLightData, depthProportion, yRotation);
+        InitialiseLight(mainLight, mainLightConfigLookup[zone], depthProportion, yRotation);
+        InitialiseLight(fillLight, fillLightConfigLookup[zone], depthProportion, yRotation);
     }
     
-    private void InitialiseLight(HDAdditionalLightData light, DynamicLightingData data, float depthProportion, float yRotation)
+    private void InitialiseLight(HDAdditionalLightData light, DynamicLightingConfig data, float depthProportion, float yRotation)
     {
         light.color = data.Color.Evaluate(depthProportion);
         light.intensity = data.Intensity.Evaluate(depthProportion);
---
+++ Danki2/Assets/Scripts/Lighting/DynamicLightingConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DynamicLightingConfig", menuName = "ScriptableObjects/DynamicLightingConfig", order = 1)]
+public class DynamicLightingConfig : ScriptableObject
+
+{
+    [SerializeField] private Gradient color = null;
+    [SerializeField] private AnimationCurve intensity = null;
+    [SerializeField] private AnimationCurve volumetricsMultiplier = null;
+    [SerializeField] private AnimationCurve xAngle = null;
+    [SerializeField] private AnimationCurve yAngle = null;
+
+    public Gradient Color => color;
+    public AnimationCurve Intensity => intensity;
+    public AnimationCurve VolumetricsMultiplier => volumetricsMultiplier;
+    public AnimationCurve XAngle => xAngle;
+    public AnimationCurve YAngle => yAngle;
+}
