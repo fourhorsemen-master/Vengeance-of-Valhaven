0ade38c6a8875318e15a12ec8dd276505f5d4412
nick.xue15@gmail.com <nick.xue15@gmail.com>
2022-02-20 14:24:07 +0000

    create PositionFinder class

--- Danki2/Assets/Scripts/AI/Components/Enemy/Wraith/WraithCastBlink.cs
+++ Danki2/Assets/Scripts/AI/Components/Enemy/Wraith/WraithCastBlink.cs
@@ -1,75 +1,24 @@
 ﻿using UnityEngine;
-using UnityEngine.AI;
 
 public class WraithCastBlink : IStateMachineComponent
 {
-    private const int PositionsToSample = 4;
-    
+    private readonly PositionFinder positionFinder;
     private readonly Wraith wraith;
-    private readonly Actor actorToAvoid;
-    private readonly float minDistance;
-    private readonly float maxDistance;
 
     public WraithCastBlink(Wraith wraith, Actor actorToAvoid, float minDistance, float maxDistance)
     {
         this.wraith = wraith;
-        this.actorToAvoid = actorToAvoid;
-        this.minDistance = minDistance;
-        this.maxDistance = maxDistance;
+
+        positionFinder = new PositionFinder(wraith, actorToAvoid, minDistance, maxDistance);
     }
 
-    public void Enter() => wraith.Blink(GetNewPosition());
+    public void Enter()
+    {
+        Vector3 blinkTo = positionFinder.GetRandomPositionAroundTarget();
+
+        wraith.Blink(blinkTo);
+    }
+
     public void Exit() {}
     public void Update() {}
-
-    /// Samples random positions to find one that is:
-    ///  - A good distance from the player
-    ///  - As far from the edge of the navmesh AND the wraith's current position as possible
-    private Vector3 GetNewPosition()
-    {
-        Vector3[] potentialPositions = GetPotentialPositions();
-
-        Vector3 bestPosition = potentialPositions[0];
-        float bestDistanceProduct = 0;
-
-        // Choose the position with the greatest (distanceFromWraith * distanceFromEdge)
-        foreach (Vector3 position in potentialPositions)
-        {
-            if (!NavMesh.FindClosestEdge(position, out NavMeshHit navMeshHit, NavMesh.AllAreas)) continue;
-
-            float distanceFromWraith = Vector3.Distance(position, wraith.transform.position);
-            float distanceFromEdge = Vector3.Distance(position, navMeshHit.position);
-
-            float distanceProduct = distanceFromWraith * distanceFromEdge;
-
-            if (distanceProduct <= bestDistanceProduct) continue;
-
-            bestPosition = position;
-            bestDistanceProduct = distanceProduct;
-        }
-
-        return bestPosition;
-    }
-
-    /// Gets <see cref="PositionsToSample"/> random points on the navmesh that have distance from player between minDistance and maxDistance
-    private Vector3[] GetPotentialPositions()
-    {
-        Vector3[] potentialPositions = new Vector3[PositionsToSample];
-
-        int positionsFound = 0;
-        while (positionsFound != PositionsToSample)
-        {
-            float distance = Random.Range(minDistance, maxDistance);
-            Vector2 direction2D = Random.insideUnitCircle.normalized;
-            Vector3 direction = new Vector3(direction2D.x, 0f, direction2D.y);
-            Vector3 newPosition = actorToAvoid.transform.position + distance * direction;
-            if (wraith.MovementManager.CanReach(newPosition))
-            {
-                potentialPositions[positionsFound] = newPosition;
-                positionsFound++;
-            }
-        }
-
-        return potentialPositions;
-    }
 }
