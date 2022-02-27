d4de3e615b586fd0a0f730e04cc12bcfb55fbb9b
nick.xue15@gmail.com <nick.xue15@gmail.com>
2022-02-27 22:18:48 +0000

    rename variables and remove usings

--- Danki2/Assets/Scripts/UnityHelpers/LazyFollow.cs
+++ Danki2/Assets/Scripts/UnityHelpers/LazyFollow.cs
@@ -1,35 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LazyFollow : MonoBehaviour
 {
 	[SerializeField]
-	private Transform Parent;
+	private Transform parent;
 
 	[SerializeField, Range(0f, 1f), Tooltip("0 will never move, 1 will follow the parent exactly.")]
-	private float FollowStrength;
+	private float followStrength;
 
 	private Vector3 pos, forward, up;
 
 	private Vector3 velocity = Vector3.zero;
 	void Start()
 	{
-		pos = Parent.transform.InverseTransformPoint(transform.position);
-		forward = Parent.transform.InverseTransformDirection(transform.forward);
-		up = Parent.transform.InverseTransformDirection(transform.up);
+		pos = parent.transform.InverseTransformPoint(transform.position);
+		forward = parent.transform.InverseTransformDirection(transform.forward);
+		up = parent.transform.InverseTransformDirection(transform.up);
 
 		transform.SetParent(null, true);
 	}
 
 	void Update()
 	{
-		Vector3 newpos = Parent.transform.TransformPoint(pos);
-		Vector3 newforward = Parent.transform.TransformDirection(forward);
-		Vector3 newup = Parent.transform.TransformDirection(up);
+		Vector3 newpos = parent.transform.TransformPoint(pos);
+		Vector3 newforward = parent.transform.TransformDirection(forward);
+		Vector3 newup = parent.transform.TransformDirection(up);
 		Quaternion newrot = Quaternion.LookRotation(newforward, newup);
 
-		velocity += Vector3.Distance(newpos, transform.position) * (newpos - transform.position) * FollowStrength * Time.deltaTime;
+		velocity += Vector3.Distance(newpos, transform.position) * (newpos - transform.position) * followStrength * Time.deltaTime;
 
 		velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * 2);
 
